e3edbe397bd570fe8464d2a4c77bd03013323f5e
Evan Travers <evantravers@gmail.com>
2016-12-04 08:44:49 -0600

    Add decrypter
    This one was *really* fun. The trick for the shift of course is the
    modulo operator... that way you can keep the shift for each character
    within the bounds of the alphabet.
    
    Since I already have the section ID and the encrypted message, I
    implemented an alphabet class that returns the shifted character for any
    large shift, using the before-mentioned modulo operator. I searched for
    pole, and voila!

--- day4/room_checksum.rb
+++ day4/room_checksum.rb
@@ -2,21 +2,29 @@ require 'pry'
 
 section_id_totals = 0
 parser            = /([a-z-]+)(\d+)\[([a-z]+)\]/
+answer2           = ""
+
+class Alphabet 
+  Values = ('a'..'z').to_a
+
+  def self.get(char, shift)
+    char_index = Values.index char
+    return Values[(char_index+shift)%26]
+  end
+end
 
 File.foreach('input.txt') do |line|
   frequency_graph = {}
-  parsed_line = line.match parser
+  deciphered_room = ""
+  parsed_line     = line.match parser
 
-  room_code  = parsed_line[1]
-  section_id = parsed_line[2].to_i
-  checksum   = parsed_line[3]
+  room_code       = parsed_line[1]
+  section_id      = parsed_line[2].to_i
+  checksum        = parsed_line[3]
 
   next if checksum.length != 5
 
-  # remove hypens
-  room_code.gsub! "-", ""
-
-  room_code.split("").each do |char|
+  room_code.gsub("-", "").split("").each do |char|
     if not frequency_graph[char].nil?
       frequency_graph[char] += 1
     else
@@ -39,6 +47,18 @@ File.foreach('input.txt') do |line|
   if checksum == computed_checksum
     section_id_totals += section_id
   end
+
+
+  room_code.gsub("-", "").split("").each do |char|
+    deciphered_room += Alphabet.get(char, section_id)
+  end
+
+  if deciphered_room.match /pole/
+    answer2 = "Problem part 2\n#{deciphered_room} is at section ID: #{section_id}"
+  end
 end
 
+puts "Problem part 1:"
 puts section_id_totals
+
+puts answer2
