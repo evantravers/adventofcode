8b5f0618d63e841a4f82f5c54a592bb37f2b60d5
Evan Travers <evantravers@gmail.com>
2016-12-03 21:20:41 -0600

    refactor triangles in prep for part 2

--- day3/triangles.rb
+++ day3/triangles.rb
@@ -1,28 +1,37 @@
 require 'pry'
 
-possible = 0
+possible     = 0
+combinations = []
+
+def check_sides(side1, side2, side3)
+  if (side1 + side2) <= side3
+    return false
+  end
+  if (side1 + side3) <= side2
+    return false
+  end
+  if (side3 + side2) <= side1
+    return false
+  end
+
+  true
+end
 
 File.foreach('input.txt') do |line|
   sides = line.split(' ')
 
   sides.map! { |num| num.to_i }
 
-  sides.sort!
+  combinations << sides
+end
 
-  side_1      = sides[0]
-  side_2      = sides[1]
-  side_3      = sides[2]
+combinations.each do |sides|
+  problem1 = sides.sort
 
-  if (side_1 + side_2) <= side_3
-    next
-  end
-  if (side_1 + side_3) <= side_2
-    next
-  end
-  if (side_3 + side_2) <= side_1
-    next
-  end
+  side_1 = problem1[0]
+  side_2 = problem1[1]
+  side_3 = problem1[2]
 
-  possible += 1
+  possible += 1 if check_sides(side_1, side_2, side_3)
 end
 puts possible
