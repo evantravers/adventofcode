4844506179721ace2e6bf0602b7f9a63dbdb1744
Evan Travers <evantravers@gmail.com>
2016-12-03 19:36:24 -0600

    over-engineered solution, go!

--- day2/bathroom_code.rb
+++ day2/bathroom_code.rb
@@ -1,10 +1,18 @@
 require 'pry'
 
-Keypad = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9]
-]
+solution = ""
+
+class Keypad
+  KeyValues = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ]
+
+  def button x, y
+    return KeyValues[y][x]
+  end
+end
 
 class Pointer
   attr_accessor :x, :y
@@ -41,17 +49,18 @@ class Pointer
   end
 
   def up
-    move_pointer(0, 1)
+    move_pointer(0, -1)
   end
 
   def down
-    move_pointer(0, -1)
+    move_pointer(0, 1)
   end
 end
 
 current_key = Pointer.new
+keypad      = Keypad.new
 
-File.foreach('test.txt') do |line|
+File.foreach('input.txt') do |line|
   line.split('').each do |char|
     if char == "U"
       current_key.up
@@ -59,9 +68,12 @@ File.foreach('test.txt') do |line|
       current_key.down
     elsif char == "L"
       current_key.left
-    else
+    elsif char == "R"
       current_key.right
     end
   end
-  puts Keypad[current_key.x][current_key.y]
+
+  solution += "#{keypad.button(current_key.x, current_key.y)}"
 end
+
+puts solution
