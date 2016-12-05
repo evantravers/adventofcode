45e20d02e4d5e24815fabf15344d258ebfd259c2
Evan Travers <evantravers@gmail.com>
2016-12-05 11:12:53 -0600

    Write up part 2 of day5
    I spent an inordinate amount of time because I had swapped the order of
    the double return from `get_next_char`. Whoops.
    
    I also spent a dumb amount of time because sometimes the position
    character of the hex representation is a letter rather than a number...
    which when `to_i` returns a 0. Duh.
    
    I may spend some time trying to optimize... I feel like I could do this
    in one pass through the count rather than making two decoder classes...
    but eh.

--- day5/doorcode.rb
+++ day5/doorcode.rb
@@ -1,13 +1,18 @@
 require 'digest'
 require 'pry'
 
-Md5       = Digest::MD5.new
-Door_code = ARGV[0]
-password  = ""
+Md5            = Digest::MD5.new
+Door_code      = ARGV[0]
 
 if Door_code.nil?
   puts "You forgot to enter a door ID as an argument."
   exit
+end
+
+class String
+  def number?
+    self.match(/[0-9]/)
+  end
 end
 
 class Decoder
@@ -15,21 +20,51 @@ class Decoder
     @count = 0
   end
 
-  def get_next_char(string)
+  def get_next_char string
     hex_representation = ""
     while hex_representation[0..4] != "00000"
       hex_representation = Md5.hexdigest "#{Door_code}#{@count}"
       @count += 1
     end
-    return hex_representation[5]
+
+    return hex_representation[5], hex_representation[6]
+  end
+
+  def simple string
+    password = ""
+    until password.length > 7
+      password += self.get_next_char(string).first
+    end
+    return password
+  end
+
+  def complex string
+    password = [nil, nil, nil, nil, nil, nil, nil, nil]
+    until !password.member? nil
+      position, value = self.get_next_char(string)
+
+      if position.number?
+        position        = position.to_i
+      else
+        next
+      end
+
+      if position < 8 && password[position].nil?
+        password[position] = value
+      end
+    end
+    return password.join
   end
 end
 
+
 decoder = Decoder.new
+decoder2 = Decoder.new
 
-until password.length > 7
-  password += decoder.get_next_char(Door_code)
-end
+first_password  = decoder.simple(Door_code)
+second_password = decoder2.complex(Door_code)
 
-puts "The answer is:"
-puts password
+puts "The first answer is:"
+puts first_password
+puts "The second answer is:"
+puts second_password
