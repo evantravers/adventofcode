f91477ef24e4129952ada71c98cb54b96cdf1b08
Evan Travers <evantravers@gmail.com>
2016-12-11 19:10:14 -0600

    Working on part II of day 9
    Here's the theory... maintain a count of how high the "multiplier" has
    reached, and count the characters in the file.

--- day9/decompress.rb
+++ day9/decompress.rb
@@ -1,14 +1,15 @@
 require 'pry'
 
 class String
+  Command_pattern = /\((\d+)x(\d+)\)/
+
   def decompress
-    command_pattern = /\((\d+)x(\d+)\)/
     offset          = 0
     string          = ""
 
     while offset < self.length
-      if self.index(command_pattern, offset) == offset
-        str_length, str_times = self[offset..-1].scan(command_pattern).first.map(&:to_i)
+      if self.index(Command_pattern, offset) == offset
+        str_length, str_times = self[offset..-1].scan(Command_pattern).first.map(&:to_i)
         pattern_length = 3 + str_length.to_s.size + str_times.to_s.size
 
         # move the pointer past the command pattern
@@ -27,8 +28,43 @@ class String
 
     return string
   end
+
+  def compute_size
+    offset = 0
+    total  = 0
+
+    multipliers = []
+
+    while offset < self.length
+      if self.index(Command_pattern, offset) == offset
+        str_length, str_times = self[offset..-1].scan(Command_pattern).first.map(&:to_i)
+        pattern_length = 3 + str_length.to_s.size + str_times.to_s.size
+
+        multipliers << {length: str_length, times: str_times}
+
+        # move pointer past command pattern
+        offset += pattern_length
+      else
+        total_multiplier = multipliers.inject(1) { |memo, m| memo * m[:times] }
+        multipliers.each do |mul|
+          mul[:length] = mul[:length] - 1
+          if mul[:length] == 0
+            multipliers.delete(mul)
+          end
+        end
+        total  += total_multiplier
+        offset += 1
+      end
+    end
+
+    return total
+  end
 end
 
 input = File.read('input.txt').strip!
 
+puts "Part 1:"
 puts input.decompress.length
+
+puts "Part 2:"
+puts input.compute_size
