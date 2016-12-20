ffacec8dafff8a544401119dad34f5b41f70cbd6
Evan Travers <evantravers@gmail.com>
2016-12-20 12:14:07 -0600

    Refactor the x and y issues
    I'm having troubles with the right axis...

--- day13/maze.rb
+++ day13/maze.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Maze
   @map = []
 
@@ -9,36 +7,42 @@ class Maze
     generate(dimension)
   end
 
-  def is_wall? x, y
+  def is_space? x, y
     x, y = x.to_i, y.to_i
     secret_formula = x*x + 3*x + 2*x*y + y + y*y
 
     number = secret_formula + @designers_favorite_number
-    return number.to_s(2).scan("1").size.odd?
+    return number.to_s(2).scan("1").size.even?
   end
 
   def generate dimension
-    @map = Array.new
-
-    (0..dimension).each do |y|
-      @map[y] = Array.new
-      (0..dimension).each do |x|
-        @map[y][x] = is_wall?(x, y)
+    @map = Array.new(dimension)
+    (0..dimension).each do |x|
+      @map[x] = Array.new(dimension, false)
+      (0..dimension).each do |y|
+        @map[x][y] = is_space?(x, y)
       end
     end
   end
 
+  def grid
+    return @map.transpose
+  end
+
   def inspect
     map = ""
-    @map.each do |x|
+    grid.each do |x|
       x.each do |y|
-        map += "#" if y
-        map += "." if !y
+        map += "." if y
+        map += "#" if !y
       end
       map += "\n"
     end
     return map
   end
+end
+
+class MazeRunner
 end
 
 test = Maze.new(10, 9)
