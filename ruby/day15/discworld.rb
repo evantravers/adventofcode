eac9cc6e78574afabf0ddf3aaa892333ba41308b
Evan Travers <evantravers@gmail.com>
2017-01-10 13:01:27 -0600

    Solve Day 15 part 1
    After the stupidity of Day 14, I solve Day 15 first try. Bit of a morale
    booster. Nothing too clever here, and I'd like to rewrite `solve` to be
    a little less hacky. The key part is the `open_at` method, which
    correctly handles the rotation of the disk positions by using the ever
    handy modulo operator.

---
+++ ruby/day15/discworld.rb
@@ -0,0 +1,44 @@
+class Disk
+  def initialize description
+    d = description.match(/Disc #(\d+) has (\d+) positions; at time=(\d+), it is at position (\d+)./)
+    @positions      = d[2].to_i
+    @measured_at    = d[3].to_i
+    @start_position = d[4].to_i
+  end
+
+  def open_at time
+    ((@start_position + time) % @positions) == 0
+  end
+end
+
+class Machine
+  def initialize(file)
+    @disks = File.readlines(file).map { |line| Disk.new(line) }
+  end
+
+  def solve
+    (0..30000000).each do |wait_time|
+      capsule_position = wait_time
+      success          = true
+
+      @disks.each do |disk|
+        if success
+          capsule_position += 1
+          if !disk.open_at capsule_position
+            success = false
+          end
+        end
+      end
+
+      return wait_time if success
+    end
+  end
+end
+
+puts "Test Case:"
+test = Machine.new('test.txt')
+puts "Passed" if test.solve == 5
+
+puts "Problem 1:"
+p1 = Machine.new('input.txt')
+puts p1.solve
