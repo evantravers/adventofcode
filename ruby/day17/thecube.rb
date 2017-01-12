1a35c2a2973cf4e7cdd5611576ad46adc3390568
Evan Travers <evantravers@gmail.com>
2017-01-12 15:47:40 -0600

    Trying a little experiment
    I may end up having to memoize this, but I like the idea of only keeping
    track of the history the whole time.

--- ruby/day17/thecube.rb
+++ ruby/day17/thecube.rb
@@ -1,25 +1,87 @@
 require 'minitest/autorun'
 require 'digest'
 require 'set'
+require 'pry'
 
-md5 = Digest::MD5.new
+class Maze
+  def initialize salt
+    @salt = salt
+    @md5 = Digest::MD5.new
+  end
 
-start = [0,0]
+  def solve
+    path = []
 
-def in_bounds x, y
-  (0..3).include?(x) && (0..3).include?(y)
+    until current_position(path) == [3, 3]
+      possible_moves = available_moves(path)
+      # up, down left right
+    end
+
+    return path.join.upcase
+  end
+
+  def open_door? char
+    ['b', 'c', 'd', 'e', 'f'].include? char
+  end
+
+  def available_moves path
+    hash       = @md5.hexdigest(@salt + path.join.upcase)
+    open_doors = hash[0..3].split('').map { |x| open_door? x }
+    available  = []
+
+    [[:U, 0], [:D, 1], [:L, 2], [:R, 3]].each do |direction, indice|
+      if open_doors[indice] && in_bounds(current_position(path << direction))
+        available << direction
+      end
+    end
+    return available
+  end
+
+  def current_position path
+    x, y = 0, 0
+    path.each do |step|
+      case step
+        when :U then y -= 1
+        when :D then y += 1
+        when :L then x -= 1
+        when :R then x += 1
+      end
+    end
+    [x, y]
+  end
+
+  def in_bounds coords
+    x, y = coords
+    (0..3).include?(x) && (0..3).include?(y)
+  end
 end
 
-class MazeTest < Minitest::Test
-  def test_1
-    assert_equal solve('ihgpwlah'), 'DDRRRD'
+class SolutionTests < Minitest::Test
+  def test_doors
+    maze = Maze.new('hijkl')
+    assert_equal maze.available_moves([]), [:D]
+    assert_equal maze.available_moves([:D]), [:U, :R]
   end
 
-  def test_2
-    assert_equal solve('kglvqrro'), 'DDUDRLRRUDRD'
+  def test_current_position
+    maze = Maze.new('hijkl')
+    assert_equal maze.current_position([:D]), [0, 1]
+    assert_equal maze.current_position([:D, :U]), [0, 0]
+    assert_equal maze.current_position([:D, :U, :R, :D, :D]), [1, 2]
   end
 
-  def test_3
-    assert_equal solve('ulqzkmiv'), 'DRURDRUDDLLDLUURRDULRLDUUDDDRR'
-  end
+  # def test_solve_1
+  #   maze = Maze.new('ihgpwlah')
+  #   assert_equal maze.solve, 'DDRRRD'
+  # end
+
+  # def test_solve_2
+  #   maze = Maze.new('kglvqrro')
+  #   assert_equal maze.solve, 'DDUDRLRRUDRD'
+  # end
+
+  # def test_solve_3
+  #   maze.new('ulqzkmiv')
+  #   assert_equal maze.solve, 'DRURDRUDDLLDLUURRDULRLDUUDDDRR'
+  # end
 end
