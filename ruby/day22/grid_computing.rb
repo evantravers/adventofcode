03e1c2e5e685b42c573ecffa06c4e729635c6fd1
Evan Travers <evantravers@gmail.com>
2017-04-07 11:21:09 -0500

    Write a grid class to load in the nodes

--- ruby/day22/grid_computing.rb
+++ ruby/day22/grid_computing.rb
@@ -1,14 +1,28 @@
 require 'minitest/autorun'
 
 class Node
-  NODE_DESCR = /\/dev\/grid\/node-x(?<x>\d+)-y(?<y>\d+)(?: +)(?<size>\d+)T(?: +)(?: +)(?<used>\d+)T(?: +)(?<avail>\d+)T(?: +)(?<use>\d+)%/
+  NODE_DESCR = /\/dev\/grid\/node-x(?<x>\d+)-y(?<y>\d+)(?: +)(?<size>\d+)T(?: +)(?: +)(?<used>\d+)T(?: +)(?<avail>\d+)T(?: +)(?<usage>\d+)%/
 
-  attr_accessor :x, :y, :size, :used, :avail, :use
+  attr_accessor :x, :y, :size, :used, :avail, :usage
 
   def initialize string
     args = string.match(NODE_DESCR)
     args.names.each do |name|
       instance_variable_set("@#{name}", args[name].to_i)
+    end
+  end
+end
+
+class Grid
+  attr_accessor :nodes
+
+  def initialize file
+    @nodes = []
+
+    File.readlines(file).map do |line|
+      if line.match(/\/dev\/grid\/node/)
+        @nodes << Node.new(line)
+      end
     end
   end
 end
@@ -21,7 +35,8 @@ class TestNodeClass < Minitest::Test
     assert_equal 94, n.size
     assert_equal 73, n.used
     assert_equal 21, n.avail
-    assert_equal 77, n.use
+    assert_equal 77, n.usage
   end
 end
 
+grid = Grid.new('input.txt')
