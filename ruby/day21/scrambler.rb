d0487604feb7a7a5ade352806307397d552c0800
Evan Travers <evantravers@gmail.com>
2017-04-06 21:27:08 -0500

    Remove the reverse stuff
    It's not possible to reverse the rotate_position method, not that I can
    think, so I'm going to brute force it.

--- ruby/day21/scrambler.rb
+++ ruby/day21/scrambler.rb
@@ -2,49 +2,43 @@ require 'minitest/autorun'
 require 'pry'
 
 class String
-  def swap_position x, y, reverse: false
+  def swap_position x, y
     x, y = x.to_i, y.to_i
 
     self[x], self[y] = self[y], self[x]
     self
   end
 
-  def swap_letter x, y, reverse: false
+  def swap_letter x, y
     loc_x, loc_y = self.rindex(x), self.rindex(y)
-    self.swap_position(loc_x, loc_y, reverse: reverse)
+    self.swap_position(loc_x, loc_y)
   end
 
-  def rotate_steps dir, x, reverse: false
+  def rotate_steps dir, x
     dir, x = dir.to_sym, x.to_i
 
     x = x * -1 if dir == :right
-    x = x * -1 if reverse
 
     self.split('').rotate(x).join
   end
 
-  def rotate_position x, reverse: false
+  def rotate_position x
     index_of_char = self.rindex(x)
     index_of_char += 1 if index_of_char > 3
     index_of_char += 1
     dir = :right
-    dir = :left if reverse
-    self.rotate_steps dir, index_of_char, reverse: reverse
+    self.rotate_steps dir, index_of_char
   end
 
-  def reverse_positions x, y, reverse: false
+  def reverse_positions x, y
     x, y = x.to_i, y.to_i
 
     self[x..y] = self[x..y].reverse
     self
   end
 
-  def move_position x, y, reverse: false
+  def move_position x, y
     x, y = x.to_i, y.to_i
-
-    if reverse
-      x, y = y, x
-    end
 
     moving = self.slice!(x)
     self.insert(y, moving)
@@ -97,44 +91,35 @@ end
 class TestString < Minitest::Test
   def test_swap_position
     assert_equal 'ebcda', 'abcde'.swap_position(4, 0)
-    assert_equal 'abcde', 'ebcda'.swap_position(4, 0, reverse: true)
     assert_equal 'adcbefg', 'abcdefg'.swap_position(1, 3)
     assert_equal 'gbcdefa', 'abcdefg'.swap_position(0, 6)
   end
 
   def test_swap_letter
     assert_equal 'edcba', 'ebcda'.swap_letter('d', 'b')
-    assert_equal 'ebcda', 'edcba'.swap_letter('d', 'b', reverse: true)
     assert_equal 'ahcdefgb', 'abcdefgh'.swap_letter('b', 'h')
   end
 
   def test_reverse_positions
     assert_equal 'abcde', 'edcba'.reverse_positions(0, 4)
     assert_equal 'aedcbfgh', 'abcdefgh'.reverse_positions(1, 4)
-    assert_equal 'abcdefgh', 'aedcbfgh'.reverse_positions(1, 4, reverse: true)
   end
 
   def test_rotate_steps
     assert_equal 'bcdea', 'abcde'.rotate_steps(:left, 1)
     assert_equal 'habcdefg', 'abcdefgh'.rotate_steps(:right, 1)
-    assert_equal 'abcdefgh', 'habcdefg'.rotate_steps(:right, 1, reverse: true)
   end
 
   def test_rotate_position
     assert_equal 'ecabd', 'abdec'.rotate_position('b')
     assert_equal 'decab', 'ecabd'.rotate_position('d')
-    assert_equal 'ecabd', 'decab'.rotate_position('d', reverse: true)
     assert_equal 'abcdefgh', 'abcdefgh'.rotate_position('g')
-    assert_equal 'abcdefgh', 'abcdefgh'.rotate_position('g', reverse: true)
     assert_equal 'efghabcd', 'abcdefgh'.rotate_position('d')
-    assert_equal 'abcdefgh', 'efghabcd'.rotate_position('d', reverse: true)
   end
 
   def test_move_position
     assert_equal 'bdeac', 'bcdea'.move_position(1, 4)
-    assert_equal 'bcdea', 'bdeac'.move_position(1, 4, reverse: true)
     assert_equal 'dabcefgh', 'abcdefgh'.move_position(3, 0)
-    assert_equal 'abcdefgh', 'dabcefgh'.move_position(3, 0, reverse: true)
   end
 end
 
