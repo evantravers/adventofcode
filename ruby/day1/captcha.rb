d9f05e8e96c2028a27e321381ead4bc82b53da04
Evan Travers <evantravers@gmail.com>
2017-11-30 23:20:39 -0600

    Write a simple solution for day1
    I'm appending the first item to the array so that I have a hacky
    circular array.

---
+++ ruby/day1/captcha.rb
@@ -0,0 +1,36 @@
+require 'minitest/autorun'
+
+def captcha(num)
+  sum = 0
+
+  numbers = num.to_s.split('').map(&:to_i)
+
+  # hacky circular array
+  numbers.push numbers.first
+
+  numbers.each_cons(2) do |a, b|
+    if a == b
+      sum += a
+    end
+  end
+
+  return sum
+end
+
+class CaptchaTest < Minitest::Test
+  def test_1
+    assert_equal 3, captcha(1122)
+  end
+
+  def test_2
+    assert_equal 4, captcha(1111)
+  end
+
+  def test_3
+    assert_equal 0, captcha(1234)
+  end
+
+  def test_4
+    assert_equal 9, captcha(91212129)
+  end
+end
