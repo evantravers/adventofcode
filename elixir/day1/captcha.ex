ee2e54aa6bde554cdaf1e0ab8fa35e480de30cf6
Evan Travers <evantravers@gmail.com>
2017-12-02 14:31:23 -0600

    Stupid solution to day2
    I kind of would like to DRY this up, and pass the step as a function to
    a new captcha/2, but hey. Winning.

--- elixir/day1/captcha.ex
+++ elixir/day1/captcha.ex
@@ -15,6 +15,23 @@ captcha = fn (array_of_numbers) ->
     end )
 end
 
+captcha2 = fn (array_of_numbers) ->
+  size = Enum.count(array_of_numbers)
+
+  array_of_numbers
+    |> Enum.with_index
+    |> Enum.reduce(0, fn ({num, index}, acc) ->
+
+      next_index = rem(index+(div(size,2)), size)
+      next       = Enum.at(array_of_numbers, next_index)
+
+      acc = case num == next do
+        true  -> acc = acc + num
+        false -> acc
+      end
+    end )
+end
+
 {:ok, file} = File.open "input.txt"
 
 # it'd be much better to read in by character, but hey
@@ -25,3 +42,4 @@ numbers = IO.read(file, :all)
           |> Enum.map(fn(x) -> String.to_integer(x) end)
 
 IO.puts captcha.(numbers)
+IO.puts captcha2.(numbers)
