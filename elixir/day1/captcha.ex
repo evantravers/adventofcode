8c05fe819cd20a382aac5a7a452aa7b394fde692
Evan Travers <evantravers@gmail.com>
2017-12-02 19:00:25 -0600

    Trying to follow the style guide
    https://github.com/christopheradams/elixir_style_guide

--- elixir/day1/captcha.ex
+++ elixir/day1/captcha.ex
@@ -2,43 +2,44 @@ captcha = fn (array_of_numbers) ->
   size = Enum.count(array_of_numbers)
 
   array_of_numbers
-    |> Enum.with_index
-    |> Enum.reduce(0, fn ({num, index}, acc) ->
+  |> Enum.with_index
+  |> Enum.reduce(0, fn ({num, index}, acc) ->
 
-      next_index = rem(index+1, size)
-      next       = Enum.at(array_of_numbers, next_index)
+    next_index = rem(index+1, size)
+    next       = Enum.at(array_of_numbers, next_index)
 
-      case num == next do
-        true  -> acc + num
-        false -> acc
-      end
-    end )
+    case num == next do
+      true  -> acc + num
+      false -> acc
+    end
+  end)
 end
 
 captcha2 = fn (array_of_numbers) ->
   size = Enum.count(array_of_numbers)
 
   array_of_numbers
-    |> Enum.with_index
-    |> Enum.reduce(0, fn ({num, index}, acc) ->
+  |> Enum.with_index
+  |> Enum.reduce(0, fn ({num, index}, acc) ->
 
-      next_index = rem(index+(div(size,2)), size)
-      next       = Enum.at(array_of_numbers, next_index)
+    next_index = rem(index+(div(size,2)), size)
+    next       = Enum.at(array_of_numbers, next_index)
 
-      case num == next do
-        true  -> acc + num
-        false -> acc
-      end
-    end )
+    case num == next do
+      true  -> acc + num
+      false -> acc
+    end
+  end)
 end
 
 {:ok, file} = File.open "input.txt"
 
 # it'd be much better to read in by character, but hey
-numbers = IO.read(file, :all)
-          |> String.trim
-          |> String.split("", [trim: true ])
-          |> Enum.map(fn(x) -> String.to_integer(x) end)
+numbers =
+  IO.read(file, :all)
+  |> String.trim
+  |> String.split("", [trim: true ])
+  |> Enum.map(fn(x) -> String.to_integer(x) end)
 
 IO.puts captcha.(numbers)
 IO.puts captcha2.(numbers)
