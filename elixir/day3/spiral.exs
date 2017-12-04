5d2887bbc05a67601e1fa8058ed1309e27792168
Evan Travers <evantravers@gmail.com>
2017-12-04 10:55:11 -0600

    WIP Spiral
    So I'm beginning to be more pleased with this, but I've got to trace
    through and find my bug.

--- elixir/day3/spiral.exs
+++ elixir/day3/spiral.exs
@@ -6,46 +6,53 @@ defmodule Advent2017 do
   `spiral(<target>, :right, 1, 2, %{{0, 0} => 1})`
 
   Notes:
-  - I think distance increases by 1 every two iterations: 1, 1, 2, 2, 3, 3...
+  - I think distance increases by 1 every three iterations: 1, 2, 2, 2, 3, 3,
+    3...
   - The structure should look like:
     %{{0, 0} => 1, {1, 0} => 2, {1, 1} => 3 ... }
-  - Maybe I should use `get_in` and `put_in` instead...
+  - Maybe I should use `get_in` and `put_in` instead... except I need negative
+    values. Rats.
   """
 
-  def spiral(target) do: spiral(target, :right, 1, 2, %{})
+  def next_space({x, y}, direction, value) do
+    {adjust_x, adjust_y} = 
+      case direction do
+        :right -> {1, 0}
+        :up    -> {0, 1}
+        :left  -> {-1, 0}
+        :down  -> {0, -1}
+      end
+    %{{x+adjust_x, y+adjust_y} => value}
+  end
+
+  def spiral(target), do: spiral(target, :right, 1, 1, %{{0, 0} => 1})
 
   def spiral(target, direction, distance, duration, map) do
-    {coords, last} = Enum.at(map, -1)
-    if last == target do
+    IO.puts("dir: #{direction}, dist: #{distance}, dur: #{duration}")
+    IO.inspect(map)
+
+    {coords, last_value} = Enum.at(map, -1)
+    IO.inspect coords
+    IO.inspect last_value
+    if last_value == target do
       coords
       |> Tuple.to_list
       |> Enum.map(&(abs(&1)))
       |> Enum.sum
     else
-      if duration > 0 do
-        spiral(target, direction, distance, duration-1, map)
-      else
-        case direction do
-          :right ->
-            next_space = {1, 0}
-            # ...
-            spiral(target, :up, distance, map)
-          :up ->
-            next_space = {0, 1}
-            # ...
-            spiral(target, :left, distance, map)
-          :left ->
-            next_space = {-1, 0}
-            # ...
-            spiral(target, :down, distance, map)
-          :down ->
-            next_space = {0, -1}
-            # ...
-            spiral(target, :right, distance, map)
-        end
+      if duration == 0 do
+        duration = 2
+        direction = case direction do
+                      :right -> :up
+                      :up -> :left
+                      :left -> :down
+                      :down -> :right
+                    end
       end
+      map = Map.merge(map, next_space(coords, direction, last_value + 1))
+      spiral(target, direction, distance, duration-1, map)
     end
   end
 end
 
-Advent2017.spiral(12)
+IO.puts "Part 1: #{Advent2017.spiral(9)}"
