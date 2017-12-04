a65aab29162499d96fdf1141b6b9a3f653b1e306
Evan Travers <evantravers@gmail.com>
2017-12-03 23:06:27 -0600

    WIP starting to feel out a possible implementation

--- elixir/day3/spiral.exs
+++ elixir/day3/spiral.exs
@@ -1,14 +1,38 @@
 defmodule Day3 do
-  def spiral(direction \\ :right, distance, map) do
+  @doc """
+  Going to attempt to use streams here.
+
+  I think the first call is:
+  `spiral(<target>, :right, 1, %{{0, 0} => 1})`
+
+  Notes:
+  - I think distance increases by 1 every two iterations: 1, 1, 2, 2, 3, 3...
+  """
+
+  def spiral(target, direction, distance, map) do
+    {coords, last} = Enum.at(map, -1)
+    if last == target
+      coords
+      |> Tuple.to_list
+      |> Enum.map(&(abs(&1)))
+      |> Enum.sum
     case direction do
       :right ->
         {1, 0}
+        # ...
+        spiral(target, :up, distance, map)
       :up ->
         {0, 1}
+        # ...
+        spiral(target, :left, distance, map)
       :left ->
         {-1, 0}
+        # ...
+        spiral(target, :down, distance, map)
       :down ->
         {0, -1}
+        # ...
+        spiral(target, :right, distance, map)
     end
   end
 end
