f69c83ed3dad010d31762637b8d969b291b44be3
Evan Travers <evantravers@gmail.com>
2017-12-04 09:42:32 -0600

    Wrote part 2 to day 4
    Stuff like this really makes me like functional programming

--- elixir/day4/passphrase.exs
+++ elixir/day4/passphrase.exs
@@ -8,6 +8,21 @@ defmodule Advent2017 do
     |> Enum.filter(fn (x) -> Enum.count(x) == Enum.count(Enum.uniq(x)) end)
     |> Enum.count
   end
+
+  def day4b do
+    {:ok, file} = File.read("input.txt")
+
+    file
+    |> String.split("\n", [trim: true])
+    |> Enum.map(fn (line) ->
+                  line
+                  |> String.split(" ", [trim: true])
+                  |> Enum.map(&Enum.sort(to_charlist(&1)))
+                end)
+    |> Enum.filter(fn (x) -> Enum.count(x) == Enum.count(Enum.uniq(x)) end)
+    |> Enum.count
+  end
 end
 
 IO.puts "Part 1: #{Advent2017.day4a}"
+IO.puts "Part 2: #{Advent2017.day4b}"
