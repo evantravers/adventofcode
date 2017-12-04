ca25ddf661f6f9182b66bd00624d8b4864caa48a
Evan Travers <evantravers@gmail.com>
2017-12-04 12:18:10 -0600

    Add some ghetto debugging tools
    Nothing like a visualization

--- elixir/day3/spiral.exs
+++ elixir/day3/spiral.exs
@@ -6,16 +6,17 @@ defmodule Advent2017 do
   `spiral(<target>, :right, 1, 2, %{{0, 0} => 1})`
 
   Notes:
-  - I think distance increases by 1 every three iterations: 1, 2, 2, 2, 3, 3,
-    3...
+  - I think interval increases by 1 every two iterations: 1, 2, 2, 3, 3...
   - The structure should look like:
     %{{0, 0} => 1, {1, 0} => 2, {1, 1} => 3 ... }
   - Maybe I should use `get_in` and `put_in` instead... except I need negative
     values. Rats.
+
+  Inteval = length of arm
   """
 
   def next_space({x, y}, direction, value) do
-    {adjust_x, adjust_y} = 
+    {adjust_x, adjust_y} =
       case direction do
         :right -> {1, 0}
         :up    -> {0, 1}
@@ -25,34 +26,54 @@ defmodule Advent2017 do
     %{{x+adjust_x, y+adjust_y} => value}
   end
 
-  def spiral(target), do: spiral(target, :right, 1, 1, %{{0, 0} => 1})
+  def next_direction(direction) do
+    case direction do
+      :right -> :up
+      :up -> :left
+      :left -> :down
+      :down -> :right
+    end
+  end
 
-  def spiral(target, direction, distance, duration, map) do
-    IO.puts("dir: #{direction}, dist: #{distance}, dur: #{duration}")
-    IO.inspect(map)
+  def view_spiral(map) do
+    map
+    Enum.map((-3..3), fn (x) ->
+      Enum.map((-3..3), fn(y) ->
+        val = if is_nil(map[{x, y}]), do: " ", else: map[{x, y}]
+        IO.binwrite "[#{val}]"
+      end)
+      IO.puts "\n"
+    end)
+    IO.puts "\n"
+  end
+
+  def spiral(target), do: spiral(target, :right, 1, 0, 1, %{{0, 0} => 1})
+
+  def spiral(target, direction, interval, traveled, turns, map) do
 
     {coords, last_value} = Enum.at(map, -1)
-    IO.inspect coords
-    IO.inspect last_value
+    view_spiral(map)
     if last_value == target do
       coords
       |> Tuple.to_list
       |> Enum.map(&(abs(&1)))
       |> Enum.sum
     else
-      if duration == 0 do
-        duration = 2
-        direction = case direction do
-                      :right -> :up
-                      :up -> :left
-                      :left -> :down
-                      :down -> :right
-                    end
+      if interval == traveled do
+        direction = next_direction(direction)
+        turns = turns + 1
+        # after we've turned two times, increment the interval
+        unless turns > 3 do
+          interval = interval + 1
+          turns    = 0
+        end
       end
+
       map = Map.merge(map, next_space(coords, direction, last_value + 1))
-      spiral(target, direction, distance, duration-1, map)
+
+      spiral(target, direction, interval, traveled+1, turns, map)
     end
   end
 end
 
-IO.puts "Part 1: #{Advent2017.spiral(9)}"
+IO.puts "Part 1: #{Advent2017.spiral(7)}"
