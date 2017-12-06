5fe3c0b4e8af0c52f5053b6ad0c71b9ebf78750f
Evan Travers <evantravers@gmail.com>
2017-12-06 10:08:56 -0600

    Setup tests for Day6

---
+++ advent2017/lib/day6/memorybank.ex
@@ -0,0 +1,13 @@
+defmodule Advent2017.Day6 do
+  @doc ~S"""
+  Finds the most populated bank in `list`, and redistrubutes it across the
+  other banks.
+
+      iex> Advent2017.Day6.reallocate([0, 2, 7, 0])
+      [2, 4, 1, 2]
+  """
+  @spec reallocate(list) :: list
+  def reallocate(list) do
+    list
+  end
+end
---
+++ advent2017/test/day6_test.exs
@@ -0,0 +1,4 @@
+defmodule Advent2017Test do
+  use ExUnit.Case
+  doctest Advent2017.Day6
+end
