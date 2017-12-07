02e497c818809424a759dffbdac62dd9fef95788
Evan Travers <evantravers@gmail.com>
2017-12-07 14:05:52 -0600

    WIP
    I think I've lost my way... *hangs head*

--- advent2017/lib/day7/tower.ex
+++ advent2017/lib/day7/tower.ex
@@ -1,6 +1,9 @@
 defmodule Advent2017.Day7 do
   defmodule Disc do
-    defstruct name: "", weight: 0, children: MapSet.new
+    defstruct name: "",
+              weight: 0,
+              missing_children: MapSet.new,
+              children: MapSet.new
 
     defp w(weight) do
       weight
@@ -21,32 +24,45 @@ defmodule Advent2017.Day7 do
 
     def new([name_and_weight, children]) do
       [name, weight] = String.split(name_and_weight, " ")
-      %Disc{name: name, weight: w(weight), children: c(children)}
+      %Disc{name: name, weight: w(weight), missing_children: c(children)}
+    end
+
+    def add_child(parent, child) do
+      parent
     end
   end
 
-  def p1 do
-    load_file_into_nodes("input.txt")
-    |> build_tower
-    |> Enum.at(0)
-  end
-  def p2, do: nil
+  def build_tower([child | remaining_nodes]) do
+    # I have a list of nodes that haven't been placed yet.
+    # child
+    #   if List.empty(remaining_nodes)
+    #   check to see if it belongs in remaining_nodes
+    parent =
+      Enum.find(remaining_nodes, &(MapSet.member?(&1.missing_children, child)))
 
-  def build_tower(nodes) do
-    nodes
-    |> Enum.reduce(MapSet.new, fn(n, tree) ->
-      
-    end)
+    cond do
+      Enum.empty? remaining_nodes -> # win condition
+        MapSet.new([child])
+      parent ->
+        parent = Disc.add_child(parent, child)
+    end
   end
 
   def load_file_into_nodes(file_name) do
     {:ok, file} = File.read("lib/day7/#{file_name}")
 
-    MapSet.new(
-      file
-      |> String.split("\n", [trim: true])
-      |> Enum.map(&(String.split(&1, " -> ", [trim: true])))
-      |> Enum.map(&(Disc.new &1))
-    )
+    file
+    |> String.split("\n", [trim: true])
+    |> Enum.map(&(String.split(&1, " -> ", [trim: true])))
+    |> Enum.map(&(Disc.new &1))
   end
+
+  def p1 do
+    load_file_into_nodes("test.txt")
+    |> build_tower
+    |> List.first
+    |> Map.fetch(:name)
+  end
+
+  def p2, do: nil
 end
