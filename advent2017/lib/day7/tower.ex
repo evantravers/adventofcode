063bcc853c59d12615a5c439141f956bef3f0525
Evan Travers <evantravers@gmail.com>
2017-12-07 07:37:29 -0600

    Use modules properly
    I'm really beginning to like this language

--- advent2017/lib/day7/tower.ex
+++ advent2017/lib/day7/tower.ex
@@ -1,31 +1,31 @@
-defmodule Advent2017.Day7.Disc do
-  defstruct name: "", weight: 0, children: MapSet.new
-
-  defp w(weight) do
-    weight
-    |> String.replace(~r/\(|\)/, "")
-    |> String.to_integer
-  end
-
-  defp c(children) do
-    children
-    |> String.split(", ", [trim: true])
-    |> MapSet.new
-  end
-
-  def new([name_and_weight]) do
-    [name, weight] = String.split(name_and_weight, " ")
-    %Advent2017.Day7.Disc{name: name, weight: w(weight)}
-  end
-
-  def new([name_and_weight, children]) do
-    [name, weight] = String.split(name_and_weight, " ")
-    %Advent2017.Day7.Disc{name: name, weight: w(weight), children: c(children)}
-  end
-end
-
 defmodule Advent2017.Day7 do
-  def p1, do
+  defmodule Disc do
+    defstruct name: "", weight: 0, children: MapSet.new
+
+    defp w(weight) do
+      weight
+      |> String.replace(~r/\(|\)/, "")
+      |> String.to_integer
+    end
+
+    defp c(children) do
+      children
+      |> String.split(", ", [trim: true])
+      |> MapSet.new
+    end
+
+    def new([name_and_weight]) do
+      [name, weight] = String.split(name_and_weight, " ")
+      %Disc{name: name, weight: w(weight)}
+    end
+
+    def new([name_and_weight, children]) do
+      [name, weight] = String.split(name_and_weight, " ")
+      %Disc{name: name, weight: w(weight), children: c(children)}
+    end
+  end
+
+  def p1 do
     load_file_into_nodes("input.txt")
     |> build_tower
     |> Enum.at(0)
@@ -46,7 +46,7 @@ defmodule Advent2017.Day7 do
       file
       |> String.split("\n", [trim: true])
       |> Enum.map(&(String.split(&1, " -> ", [trim: true])))
-      |> Enum.map(&(Advent2017.Day7.Disc.new &1))
+      |> Enum.map(&(Disc.new &1))
     )
   end
 end
