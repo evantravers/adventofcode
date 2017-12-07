7aa51eae9869d72d56d7950e032cae0325387ab0
Evan Travers <evantravers@gmail.com>
2017-12-07 07:32:46 -0600

    WIP working on structure
    Looking at this now... this probably would have been easier with a
    keyword map of keyword maps... but heck. I like it. It looks good.

--- advent2017/lib/day7/tower.ex
+++ advent2017/lib/day7/tower.ex
@@ -7,41 +7,40 @@ defmodule Advent2017.Day7.Disc do
     |> String.to_integer
   end
 
-  defp c(children, nodes) do
+  defp c(children) do
     children
     |> String.split(", ", [trim: true])
     |> MapSet.new
   end
 
-  def new([name_and_weight], nodes) do
+  def new([name_and_weight]) do
     [name, weight] = String.split(name_and_weight, " ")
     %Advent2017.Day7.Disc{name: name, weight: w(weight)}
   end
 
-  def new([name_and_weight, children], nodes) do
+  def new([name_and_weight, children]) do
     [name, weight] = String.split(name_and_weight, " ")
     %Advent2017.Day7.Disc{name: name, weight: w(weight), children: c(children)}
   end
 end
 
 defmodule Advent2017.Day7 do
-  def p1, do: nil
+  def p1, do
+    load_file_into_nodes("input.txt")
+    |> build_tower
+    |> Enum.at(0)
+  end
   def p2, do: nil
 
-  @doc ~S"""
-      iex> Advent2017.Day7.load_file_into_nodes("test.txt")
-      ...> |> Advent2017.Day7.build_tower
-      ...> |> Advent2017.Day7.base
-      "tknk"
-  """
-  def build_tower(file_name) do
-    file_name
+  def build_tower(nodes) do
+    nodes
+    |> Enum.reduce(MapSet.new, fn(n, tree) ->
+      
+    end)
   end
 
   def load_file_into_nodes(file_name) do
     {:ok, file} = File.read("lib/day7/#{file_name}")
-
-    IO.inspect file
 
     MapSet.new(
       file
