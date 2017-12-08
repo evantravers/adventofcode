10cb89f82d7c0349a81840cd63f0dc957d8f23b2
Evan Travers <evantravers@gmail.com>
2017-12-08 16:35:11 -0600

    More tests

--- advent2017/lib/day6/memorybank.ex
+++ advent2017/lib/day6/memorybank.ex
@@ -29,6 +29,11 @@ defmodule Advent2017.Day6 do
     |> sort_banks
   end
 
+  @doc ~S"""
+      iex> Advent2017.Day6.redistribute([{1, 0}, {2, 1}, {3, 2}, {0, 3}], 3)
+      ...> |> Advent2017.Day6.sort_banks
+      [2, 3, 4, 0]
+  """
   def redistribute(list, 0), do: list
 
   def redistribute([h|t], spare_blocks) do
@@ -48,6 +53,14 @@ defmodule Advent2017.Day6 do
     Enum.slice(list, index..-1) ++ Enum.slice(list, 0..index-1)
   end
 
+  @doc ~S"""
+  Sorts a list of tuples (from `Enum.with_index`) back into it's original state
+
+      iex> Advent2017.Day6.sort_banks([{1, 0}, {2, 1}, {3, 2}])
+      [1, 2, 3]
+      iex> Advent2017.Day6.sort_banks([{1, 1}, {2, 2}, {3, 0}])
+      [3, 1, 2]
+  """
   def sort_banks(list) do
     list
     |> Enum.sort(fn({_, i1}, {_, i2}) -> i1 <= i2 end)
@@ -63,7 +76,7 @@ defmodule Advent2017.Day6 do
       iex> Advent2017.Day6.largest_bank([{2,0}, {2, 1}, {1,2}])
       {2,0}
       iex> Advent2017.Day6.largest_bank([{1, 5}, {5, 0}, {2, 3}, {5, 1}])
-      {5, 0}
+      {5,0}
   """
   def largest_bank(list) do
     # because the list is of tuples where I'm tracking their original location
@@ -90,7 +103,7 @@ defmodule Advent2017.Day6 do
   end
 
   def load_file do
-    {:ok, file} = File.read("lib/day6/test.txt")
+    {:ok, file} = File.read("lib/day6/input.txt")
 
     file
     |> String.split("\t", [trim: true])
