f7f2dc8ac526a157a32c80ba30af753c68a1a9e4
Evan Travers <evantravers@gmail.com>
2017-12-09 17:43:36 -0600

    WIP working on rewriting tower
    I'm getting frustrated and stymied, going to skip a day

--- advent2017/lib/day7/tower.ex
+++ advent2017/lib/day7/tower.ex
@@ -1,26 +1,32 @@
+require IEx
+
 defmodule Advent2017.Day7 do
-  def build_tower(file_name) do {:ok, file} = File.read("lib/day7/#{file_name}")
+  def read_input(file_name) do
+    {:ok, file} = File.read("lib/day7/#{file_name}")
     disc = ~r/(?<name>.+) \((?<weight>\d+)\)( -> (?<children>.+))*/
 
-    nodes =
-      file
-      |> String.split("\n", [trim: true])
-      |> Enum.map(fn (pattern) ->
-        Regex.named_captures(disc, pattern)
-        |> Map.update("children", [], &(String.split(&1, ", ", [trim: true])))
-      end)
-      |> Enum.sort # put the leaf nodes first, branch nodes last
-
-    IO.inspect nodes
-
-    nodes
-    |> Enum.reduce(fn (n, tree) ->
-      nil
+    file
+    |> String.split("\n", [trim: true])
+    |> Enum.map(fn (pattern) ->
+      Regex.named_captures(disc, pattern)
+      |> Map.update("children", [], &(String.split(&1, ", ", [trim: true])))
     end)
+    |> Enum.sort
+  end
+
+  # take from remaining, put into correct place in tree
+  def build_tower(remaining), do: build_tower([], remaining)
+  def build_tower(tree, remaining) do
+    IO.inspect remaining
+  end
+
+  def test do
+    read_input("test.txt")
+    |> build_tower
   end
 
   def p1 do
-    build_tower("test.txt")
+    read_input("input.txt")
   end
 
   def p2, do: nil
