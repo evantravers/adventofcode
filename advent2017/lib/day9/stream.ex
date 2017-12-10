0532629b7fb29a9ff393c754c2972d7fc91dacfc
Evan Travers <evantravers@gmail.com>
2017-12-10 00:26:16 -0600

    Set up some notes for tomorrow

--- advent2017/lib/day9/stream.ex
+++ advent2017/lib/day9/stream.ex
@@ -1,5 +1,14 @@
 defmodule Advent2017.Day9 do
   @doc ~S"""
+  `process` needs to go char by char, and fire off recursive functions based on
+  the character.
+
+  - "<" starts a garbage string, which listens for the very next ">" character,
+    ignoring all "<".
+  - "!" cancels the next character.
+  - "{" starts a group, which listens for the next "}" to close it. Each level
+    gets score+1.
+
     iex> Advent2017.Day9.process("{}")
     1
     iex> Advent2017.Day9.process("{{{}}}")
@@ -17,7 +26,15 @@ defmodule Advent2017.Day9 do
     iex> Advent2017.Day9.process("{{<a!>},{<a!>},{<a!>},{<ab>}}")
     3
   """
-  def process(input) do
-    nil
+  def process([char|input]) do
+    # by character
+    cond do
+      "!" ->
+        process(input, state)
+      "{" ->
+        group(input, state)
+      "<" ->
+        garbage(input, state)
+    end
   end
 end
