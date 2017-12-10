f43b3fbefe1f5710e1459bb124e14208f64fea2d
Evan Travers <evantravers@gmail.com>
2017-12-10 14:12:59 -0600

    Finish Day10 P2
    It took me a while digging and playing w/ Elixir's charlists to realize
    they were internally represented as a list of integers which are ASCII
    characters. Not totally clear, and took some getting used to, but cool.

--- elixir/lib/day10/knot.ex
+++ elixir/lib/day10/knot.ex
@@ -1,4 +1,8 @@
+require IEx
+
 defmodule Advent2017.Day10 do
+  use Bitwise
+
   @doc ~S"""
   For each length:
   - Reverse the order of that length of elements in the list, starting with the
@@ -30,6 +34,49 @@ defmodule Advent2017.Day10 do
 
   def unrotate(list, index), do: rotate(list, index * -1)
 
+  def hash(list, _, _, _, 0), do: list
+  def hash(list, position, skip, lengths, amount) do
+    {{position, skip}, list} = knot(list, position, skip, lengths) # reuse opts
+    hash(list, position, skip, lengths, amount-1)
+  end
+
+  @doc ~S"""
+      iex> Advent2017.Day10.prep_lengths("1,2,3")
+      [49,44,50,44,51,17,31,73,47,23]
+  """
+  def prep_lengths(input) do
+    input
+    |> String.trim
+    |> String.to_charlist
+    |> Enum.concat([17, 31, 73, 47, 23]) # magic chars
+  end
+
+  @doc ~S"""
+      iex> Advent2017.Day10.dense_hash("")
+      "a2582a3a0e66e6e86e3812dcb672a272"
+      iex> Advent2017.Day10.dense_hash("AoC 2017")
+      "33efeb34ea91902bb2f59c9920caa6cd"
+      iex> Advent2017.Day10.dense_hash("1,2,3")
+      "3efbe78a8d82f29979031a4aa0b16a9d"
+      iex> Advent2017.Day10.dense_hash("1,2,4")
+      "63960835bcdc130f0b66d7ff4f6a5a8e"
+  """
+  def dense_hash(input) do
+    lengths = prep_lengths(input)
+
+    Enum.to_list(0..255)
+    |> hash(0, 0, lengths, 64)
+    |> Enum.chunk_every(16)
+    |> Enum.map(fn(chunk) -> 
+      chunk
+      |> Enum.reduce(&(&1 ^^^ &2))
+      |> Integer.to_string(16)
+      |> String.pad_leading(2, "0") # hex representation w/ leading 0
+    end)
+    |> Enum.join
+    |> String.downcase
+  end
+
   def p1 do
     {:ok, file} = File.read("./lib/day10/input.txt")
 
@@ -44,6 +91,8 @@ defmodule Advent2017.Day10 do
   end
 
   def p2 do
-    # {:ok, file} = File.read("./lib/day10/input.txt")
+    {:ok, file} = File.read("./lib/day10/input.txt")
+
+    dense_hash(file)
   end
 end
