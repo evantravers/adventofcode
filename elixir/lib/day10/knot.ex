83b664115334806eb69ecce91c3e45245692c453
Evan Travers <evantravers@gmail.com>
2017-12-10 12:38:29 -0600

    Write Day10 p1
    I didn't want to fool with implementing a circular linked list... so I
    used the same rotate trick I did in Day 6, except this time I'm adding
    in unrotate function so that I don't have to keep track of the indexes.
    Much cleaner.
    
    I don't like how the match in the function calls gets a little heavy,
    but I'm pretty happy with the result.

--- elixir/lib/day10/knot.ex
+++ elixir/lib/day10/knot.ex
@@ -1,8 +1,42 @@
 defmodule Advent2017.Day10 do
   @doc ~S"""
-      iex> knot(foo)
-      bar
+  For each length:
+  - Reverse the order of that length of elements in the list, starting with the
+    element at the current position.
+  - Move the current position forward by that length plus the skip size.
+  - Increase the skip size by one.
+
+      iex> Enum.to_list(0..4)
+      ...> |> Advent2017.Day10.knot([3, 4, 1, 5])
+      [3, 4, 2, 1, 0]
   """
-  def knot(list, current_position, skip_size, lengths) do
+  def knot(list, current_position \\ 0, skip_size \\ 0, lengths) # defaults
+  def knot(list, _, _, []), do: list
+  def knot(list, current_position, skip_size, [l|next_length]) do
+    list
+    |> rotate(rem(current_position, length(list)))
+    |> Enum.reverse_slice(0, l)
+    |> unrotate(rem(current_position, length(list)))
+    |> knot(current_position+l+skip_size, skip_size+1, next_length)
+  end
+
+  def rotate(list, 0), do: list
+  def rotate(list, index) do
+    Enum.slice(list, index..-1) ++ Enum.slice(list, 0..index-1)
+  end
+
+  def unrotate(list, index), do: rotate(list, index * -1)
+
+  def p1 do
+    {:ok, file} = File.read("./lib/day10/input.txt")
+
+    lengths =
+      file
+      |> String.replace("\n", "")
+      |> String.split(",", [trim: true])
+      |> Enum.map(&(String.to_integer(&1)))
+
+    result = knot(Enum.to_list(0..255), lengths)
+    Enum.at(result, 0) * Enum.at(result, 1)
   end
 end
