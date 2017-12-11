2aff51f24939aee740dcd472ec5137aadcd38327
Evan Travers <evantravers@gmail.com>
2017-12-11 15:59:54 -0600

    Store all the states visited

--- elixir/lib/day11/hexgrid.ex
+++ elixir/lib/day11/hexgrid.ex
@@ -3,12 +3,20 @@ require IEx
 defmodule Advent2017.Day11 do
   @doc ~S"""
       iex> Advent2017.Day11.track("ne,ne,ne")
+      ...> |> List.first
+      ...> |> Advent2017.Day11.distance
       3
       iex> Advent2017.Day11.track("ne,ne,sw,sw")
+      ...> |> List.first
+      ...> |> Advent2017.Day11.distance
       0
       iex> Advent2017.Day11.track("ne,ne,s,s")
+      ...> |> List.first
+      ...> |> Advent2017.Day11.distance
       2
       iex> Advent2017.Day11.track("se,sw,se,sw,sw")
+      ...> |> List.first
+      ...> |> Advent2017.Day11.distance
       3
   """
   def track(steps) do
@@ -16,8 +24,7 @@ defmodule Advent2017.Day11 do
     |> String.trim
     |> String.split(",", [trim: true])
     |> Enum.map(&(String.to_atom(&1)))
-    |> track(%{x: 0, y: 0, z: 0})
-    |> distance
+    |> track([%{x: 0, y: 0, z: 0}])
   end
 
   def step(:n,  c), do: %{c | y: c[:y]+1, z: c[:z]-1}
@@ -27,9 +34,9 @@ defmodule Advent2017.Day11 do
   def step(:sw, c), do: %{c | z: c[:z]+1, x: c[:x]-1}
   def step(:nw, c), do: %{c | y: c[:y]+1, x: c[:x]-1}
 
-  def track([], state), do: state
-  def track([dir|future], state) do
-    track(future, step(dir, state))
+  def track([], states), do: states
+  def track([dir|future], states) do
+    track(future, [step(dir, hd(states))|states])
   end
 
   def distance(state) do
@@ -43,7 +50,15 @@ defmodule Advent2017.Day11 do
     {:ok, file} = File.read("lib/day11/input.txt")
     file
     |> track
+    |> List.first
+    |> distance
   end
 
-  def p2, do: nil
+  def p2 do
+    {:ok, file} = File.read("lib/day11/input.txt")
+    file
+    |> track
+    |> Enum.map(&distance &1)
+    |> Enum.max
+  end
 end
