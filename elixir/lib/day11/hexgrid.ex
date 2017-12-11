efae10e743bdd0fed7f09c4b779489ca3081d908
Evan Travers <evantravers@gmail.com>
2017-12-11 15:17:06 -0600

    Do some reading, implement hex grid as cubes
    Thanks to Jason Pike (@jrpdrummer), I decided to stop guessing and start
    googling. He provided this *amazing* link:
    https://www.redblobgames.com/grids/hexagons/#distances
    
    So I implemented the cube distance method.

--- elixir/lib/day11/hexgrid.ex
+++ elixir/lib/day11/hexgrid.ex
@@ -16,32 +16,27 @@ defmodule Advent2017.Day11 do
     |> String.trim
     |> String.split(",", [trim: true])
     |> Enum.map(&(String.to_atom(&1)))
-    |> track(%{n: 0, ne: 0, se: 0, s: 0, sw: 0, nw: 0})
+    |> track(%{x: 0, y: 0, z: 0})
     |> distance
   end
-  def track([], state), do: state
-  def track([step|future], state) do
-    track(future, %{state | step => state[step]+1})
-  end
 
-  def eliminate_steps([d1, middle, d2]) do
-    diff = Enum.min([d1, d2])
-    {d1-diff, middle+diff, d2-diff}
+  def step(:n,  c), do: %{c | y: c[:y]+1, z: c[:z]-1}
+  def step(:ne, c), do: %{c | x: c[:x]+1, z: c[:z]-1}
+  def step(:se, c), do: %{c | x: c[:x]+1, y: c[:y]-1}
+  def step(:s,  c), do: %{c | z: c[:z]+1, y: c[:y]-1}
+  def step(:sw, c), do: %{c | z: c[:z]+1, x: c[:x]-1}
+  def step(:nw, c), do: %{c | y: c[:y]+1, x: c[:x]-1}
+
+  def track([], state), do: state
+  def track([dir|future], state) do
+    track(future, step(dir, state))
   end
 
   def distance(state) do
-    [n, ne, nw, s, se, sw] = Map.values(state)
-
-    # in a hexgrid, two steps in directions (seperated by one) direction == 1
-    {n, ne, se} = eliminate_steps([n, ne, se])
-    {ne, se, s} = eliminate_steps([ne, se, s])
-    {se, s, sw} = eliminate_steps([se, s, sw])
-    {s, sw, nw} = eliminate_steps([s, sw, nw])
-    {sw, nw, n} = eliminate_steps([sw, nw, n])
-    {nw, n, ne} = eliminate_steps([nw, n, ne])
-
-    # in a hexgrid, opposite directions cancel each other out
-    abs(n-s)+abs(ne-sw)+abs(nw-se)
+    Map.values(state)
+    |> Enum.map(&abs(&1))
+    |> Enum.sum
+    |> div(2)
   end
 
   def p1 do
