28e62787ac9fc60281578f8b6fe66f6a3cd7d13f
Evan Travers <evantravers@gmail.com>
2017-12-12 14:05:36 -0600

    This algorithm works... it just doesn't return
    This traversal works perfectly, the trouble is that `build_graph`
    returns an empty list at the bottom, where I need it to return and
    collect a list of each node it touches during the traversal.

--- elixir/lib/day12/plumber.ex
+++ elixir/lib/day12/plumber.ex
@@ -1,3 +1,4 @@
+require IEx
 defmodule Advent2017.Day12 do
   @doc ~S"""
   Returns a list of lists, where the index in the first list is the id of the
@@ -12,16 +13,27 @@ defmodule Advent2017.Day12 do
       Regex.scan(~r/\d+/, pattern)
       |> List.flatten
       |> Enum.map(&String.to_integer &1)
-      |> tl
+      |> (fn(n) -> %{name: hd(n), pipes: MapSet.new(tl(n))} end).()
     end)
   end
 
-  def build_graph(list_of_nodes, visited \\ []) do
+  def build_graph(nodes, target, visited \\ MapSet.new)
+  def build_graph(nodes, target, visited) do
+    Enum.find(nodes, fn(n) -> n[:name] == target end)
+    |> Map.fetch!(:pipes)
+    |> MapSet.difference(visited) # remove visited
+    |> Enum.map(fn (link) ->
+      build_graph(nodes, link, MapSet.put(visited, link))
+    end)
+  end
 
+  def test do
+    load_nodes("test.txt")
+    |> build_graph(0)
   end
 
   def p1 do
     load_nodes("input.txt")
-    |> build_graph
+    |> build_graph(0)
   end
 end
