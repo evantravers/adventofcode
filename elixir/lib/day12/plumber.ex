6d72949890fb64bc5528df5469a2792977d2ef94
Evan Travers <evantravers@gmail.com>
2017-12-12 15:12:03 -0600

    Finally write a BFS that doesn't stink

--- elixir/lib/day12/plumber.ex
+++ elixir/lib/day12/plumber.ex
@@ -17,23 +17,27 @@ defmodule Advent2017.Day12 do
     end)
   end
 
-  def build_graph(nodes, target, visited \\ MapSet.new)
-  def build_graph(nodes, target, visited) do
-    Enum.find(nodes, fn(n) -> n[:name] == target end)
-    |> Map.fetch!(:pipes)
-    |> MapSet.difference(visited) # remove visited
-    |> Enum.map(fn (link) ->
-      build_graph(nodes, link, MapSet.put(visited, link))
-    end)
+  def build_graph(nodes, todos \\ [0], visited \\ MapSet.new)
+  def build_graph(nodes, [], visited), do: visited # win condition
+  def build_graph(nodes, [todo|todos], visited) do
+    unvisited =
+      Enum.find(nodes, & todo == &1[:name])
+      |> Map.fetch!(:pipes)
+      |> MapSet.difference(visited) # list of unvisited
+      |> MapSet.to_list
+
+    build_graph(nodes, todos ++ unvisited, MapSet.put(visited, todo))
   end
 
   def test do
     load_nodes("test.txt")
-    |> build_graph(0)
+    |> build_graph
+    |> MapSet.size
   end
 
   def p1 do
     load_nodes("input.txt")
-    |> build_graph(0)
+    |> build_graph
+    |> MapSet.size
   end
 end
