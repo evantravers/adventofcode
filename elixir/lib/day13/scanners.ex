085f3b066e302ed2e48e78feba455dc3e6892fe4
Evan Travers <evantravers@gmail.com>
2017-12-13 13:13:12 -0600

    Function for firewall's period

--- elixir/lib/day13/scanners.ex
+++ elixir/lib/day13/scanners.ex
@@ -8,7 +8,20 @@ defmodule Advent2017.Day13 do
       false
       iex> Advent2017.Day13.firewall_active?(2, {0, 2})
       true
+
+      iex> Advent2017.Day13.firewall_active?(0, {0, 3})
+      true
+      iex> Advent2017.Day13.firewall_active?(1, {0, 3})
+      false
+      iex> Advent2017.Day13.firewall_active?(2, {0, 3})
+      false
+      iex> Advent2017.Day13.firewall_active?(3, {0, 3})
+      false
+      iex> Advent2017.Day13.firewall_active?(4, {0, 3})
+      true
   """
-  def firewall_active?(clock, {depth, range}) do
+  def firewall_active?(clock, {_, range}) do
+    rem(clock, (range-1)*2) == 0
   end
 end
+
