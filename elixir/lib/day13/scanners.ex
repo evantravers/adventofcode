fdc63bbddaf0bd28a2c2e5aa7e1a67e3e682ff08
Evan Travers <evantravers@gmail.com>
2017-12-13 13:26:57 -0600

    Load input into tuples of firewall ranges

--- elixir/lib/day13/scanners.ex
+++ elixir/lib/day13/scanners.ex
@@ -23,5 +23,15 @@ defmodule Advent2017.Day13 do
   def firewall_active?(clock, {_, range}) do
     rem(clock, (range-1)*2) == 0
   end
+
+  def load_config(filename) do
+    {:ok, file} = File.read("lib/day13/#{filename}")
+
+    Regex.scan(~r/\d+/, file)
+    |> List.flatten
+    |> Enum.map(&String.to_integer &1)
+    |> Enum.chunk_every(2)
+    |> Enum.map(&List.to_tuple &1)
+  end
 end
 
