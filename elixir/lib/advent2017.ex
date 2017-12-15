dc5bac1f2c635b5593c1f4ba0ba6ac1e148bdec4
Evan Travers <evantravers@gmail.com>
2017-12-15 10:42:11 -0600

    Add Day 15's solution

--- elixir/lib/advent2017.ex
+++ elixir/lib/advent2017.ex
@@ -20,7 +20,8 @@ defmodule Advent2017 do
       Advent2017.Day11,
       Advent2017.Day12,
       Advent2017.Day13,
-      Advent2017.Day14
+      Advent2017.Day14,
+      Advent2017.Day15
     ]
     |> Enum.map(fn(day) ->
       IO.puts "#{day}\n" <>
