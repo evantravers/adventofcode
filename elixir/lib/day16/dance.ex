61e89a33beb42daf3702ea377dccb71d4c6f8c8b
Evan Travers <evantravers@gmail.com>
2017-12-16 00:00:03 -0600

    Pass all the tests

--- elixir/lib/day16/dance.ex
+++ elixir/lib/day16/dance.ex
@@ -6,7 +6,19 @@ defmodule Advent2017.Day16 do
       iex> Advent2017.Day16.s("abcde", 3)
       "cdeab"
   """
+  def s(string, 0), do: string
   def s(string, count) do
+    [last|string] =
+      string
+      |> String.graphemes
+      |> Enum.reverse
+
+    string =
+      string ++ [last]
+      |> Enum.reverse
+      |> Enum.join
+
+    s(string, count-1)
   end
 
   @doc """
@@ -16,6 +28,11 @@ defmodule Advent2017.Day16 do
       "cbade"
   """
   def x(string, pos1, pos2) do
+    string
+    |> String.graphemes
+    |> List.replace_at(pos1, String.at(string, pos2))
+    |> List.replace_at(pos2, String.at(string, pos1))
+    |> Enum.join
   end
 
   @doc """
@@ -25,5 +42,36 @@ defmodule Advent2017.Day16 do
       "adcbe"
   """
   def p(string, a, b) do
+    string
+    |> String.graphemes
+    |> Enum.map(fn x ->
+      cond do
+        x == a -> b
+        x == b -> a
+        true   -> x
+      end
+    end)
+    |> Enum.join
+  end
+
+  def dance([], dancers), do: dancers
+  def dance([instruction|instructions], dancers) do
+    case instruction do
+      "s" ->
+        nil
+      "x" ->
+        nil
+      "p" ->
+        nil
+    end
+    dance(instructions, dancers)
+  end
+
+  def p1 do
+    {:ok, file} = File.read("lib/day16/input.txt")
+
+    file
+    |> String.split(",")
+    |> dance("abcdefghijklmnop")
   end
 end
