f0ac451bb227811fbbf57f58376645c39a13ceb3
Evan Travers <evantravers@gmail.com>
2017-12-17 01:13:29 -0600

    Refactor Day17 to head into part 2 tomorrow

--- elixir/lib/day17/spinlock.ex
+++ elixir/lib/day17/spinlock.ex
@@ -1,15 +1,18 @@
 defmodule Advent2017.Day17 do
-  def spinlock(steps), do: spinlock({0}, 0, steps)
-  def spinlock(buffer, _, _) when tuple_size(buffer)>2017, do: buffer
-  def spinlock(buffer, position, steps) do
-    position = rem(position + steps, tuple_size(buffer))+1
-    value    = tuple_size(buffer)
-    spinlock(Tuple.insert_at(buffer, position, value), position, steps)
+  def spinlock(stop), do: spinlock({0}, 0, stop)
+  def spinlock(buffer, position, stop) do
+    cond do
+      tuple_size(buffer) > stop -> buffer
+      true ->
+        position = rem(position + 394, tuple_size(buffer))+1
+        value    = tuple_size(buffer)
+        spinlock(Tuple.insert_at(buffer, position, value), position, stop)
+    end
   end
 
   def p1 do
     buffer =
-      spinlock(394)
+      spinlock(2017)
 
     elem(buffer, Enum.find_index(Tuple.to_list(buffer), & &1 == 2017)+1)
   end
