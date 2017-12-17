62b544ed101ea917bf510780d17988c9d752ceff
Evan Travers <evantravers@gmail.com>
2017-12-17 15:17:42 -0600

    Totally rip off @efrees amazing solution for p2
    All credit to @efrees, that was wicked smart.
    https://github.com/efrees/adventofcode2017/blob/master/AdventOfCode2017/Solvers/Day17Solver.cs
    
    My only cleverness was realizing that his `length` variable is
    meaningless, it's always the value of i.

--- elixir/lib/day17/spinlock.ex
+++ elixir/lib/day17/spinlock.ex
@@ -1,11 +1,11 @@
 defmodule Advent2017.Day17 do
   def spinlock(stop), do: spinlock({0}, 0, stop)
   def spinlock(buffer, position, stop) do
+    value = tuple_size(buffer)
     cond do
-      tuple_size(buffer) > stop -> buffer
+      value > stop -> buffer
       true ->
-        position = rem(position + 394, tuple_size(buffer))+1
-        value    = tuple_size(buffer)
+        position = rem(position + 394, value)+1
         spinlock(Tuple.insert_at(buffer, position, value), position, stop)
     end
   end
@@ -15,5 +15,17 @@ defmodule Advent2017.Day17 do
       spinlock(2017)
 
     elem(buffer, Enum.find_index(Tuple.to_list(buffer), & &1 == 2017)+1)
+  end
+
+  def p2 do
+    elem(
+      Enum.reduce(1..50_000_000, {0, 0}, fn i, {secondElement, index} ->
+        countedTo = rem((index + 394), i)
+
+        secondElement = if countedTo == 0, do: i, else: secondElement
+
+        {secondElement, countedTo + 1}
+      end),
+      0)
   end
 end
