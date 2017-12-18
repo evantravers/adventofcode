c7b399a9d93c4174036ef2fc6cf43482834b85f3
Evan Travers <evantravers@gmail.com>
2017-12-18 07:41:50 -0600

    Write task to run a specific day

--- elixir/lib/advent2017.ex
+++ elixir/lib/advent2017.ex
@@ -23,7 +23,8 @@ defmodule Advent2017 do
       Advent2017.Day13,
       Advent2017.Day15,
       Advent2017.Day16,
-      Advent2017.Day17
+      Advent2017.Day17,
+      Advent2017.Day18
     ]
     |> Enum.map(fn(day) ->
       IO.puts "#{day}\n" <>
@@ -39,5 +40,16 @@ defmodule Mix.Tasks.RunAll do
   @shortdoc "Simply runs all the problems from 2017"
   def run(_) do
     Advent2017.run
+  end
+end
+
+defmodule Mix.Tasks.Day do
+  use Mix.Task
+
+  @shortdoc "Run a specific day from the problem set"
+  def run(day) do
+    IO.puts "Day #{day}: \n" <>
+    "Part 1:\n#{apply(String.to_existing_atom("Elixir.Advent2017.Day#{day}"), :p1, [])}\n" <>
+    "Part 2:\n#{apply(String.to_existing_atom("Elixir.Advent2017.Day#{day}"), :p2, [])}"
   end
 end
