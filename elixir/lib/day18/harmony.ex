8cf50947aa0f54dbe7a89ea5a7cd1a92436a1a52
Evan Travers <evantravers@gmail.com>
2017-12-18 05:09:45 -0600

    Update state w/ a pointer
    This is nice, and I like it. This way, every command returns a new state.

--- elixir/lib/day18/harmony.ex
+++ elixir/lib/day18/harmony.ex
@@ -2,45 +2,49 @@ defmodule Advent2017.Day18 do
   @doc ~S"""
   snd X plays a sound with a frequency equal to the value of X.
 
-      iex> Advent2017.Day18.snd(%{}, "12")
-      %{snd: 12}
+      iex> Advent2017.Day18.snd(%{pointer: 0}, "12")
+      %{snd: 12, pointer: 1}
   """
   def snd(state, x) do
     Map.put(state, :snd, e(state, x))
+    |> next
   end
 
   @doc ~S"""
   set X Y sets register X to the value of Y.
 
-      iex> Advent2017.Day18.set(%{f: 0}, "f", "3")
-      %{f: 3}
+      iex> Advent2017.Day18.set(%{pointer: 0, f: 0}, "f", "3")
+      %{f: 3, pointer: 1}
 
-      iex> Advent2017.Day18.set(%{f: 3}, "x", "f")
-      %{f: 3, x: 3}
+      iex> Advent2017.Day18.set(%{pointer: 0, f: 3}, "x", "f")
+      %{f: 3, x: 3, pointer: 1}
   """
   def set(state, x, y) do
     Map.put(state, a(x), e(state, y))
+    |> next
   end
 
   @doc ~S"""
   add X Y increases register X by the value of Y.
 
-      iex> Advent2017.Day18.add(%{f: 3}, "f", "f")
-      %{f: 6}
+      iex> Advent2017.Day18.add(%{pointer: 0, f: 3}, "f", "f")
+      %{f: 6, pointer: 1}
   """
   def add(state, x, y) do
     Map.put(state, a(x), e(state, x) + e(state, y))
+    |> next
   end
 
   @doc ~S"""
   mul X Y sets register X to the result of multiplying the value contained in
   register X by the value of Y.
 
-      iex> Advent2017.Day18.mul(%{f: 3}, "f", "f")
-      %{f: 9}
+      iex> Advent2017.Day18.mul(%{pointer: 0, f: 3}, "f", "f")
+      %{f: 9, pointer: 1}
   """
   def mul(state, x, y) do
     Map.put(state, a(x), e(state, x) * e(state, y))
+    |> next
   end
 
   @doc ~S"""
@@ -48,18 +52,19 @@ defmodule Advent2017.Day18 do
   register X by the value of Y (that is, it sets X to the result of X modulo
   Y).
 
-      iex> Advent2017.Day18.mod(%{f: 3}, "f", "2")
-      %{f: 1}
+      iex> Advent2017.Day18.mod(%{pointer: 0, f: 3}, "f", "2")
+      %{f: 1, pointer: 1}
   """
   def mod(state, x, y) do
     Map.put(state, a(x), rem(e(state, x), e(state,y)))
+    |> next
   end
 
   @doc ~S"""
   rcv X recovers the frequency of the last sound played, but only when the
   value of X is not zero. (If it is zero, the command does nothing.)
 
-      iex> Advent2017.Day18.rcv(%{f: 3, snd: 12}, "f")
+      iex> Advent2017.Day18.rcv(%{pointer: 0, f: 3, snd: 12}, "f")
       12
   """
   def rcv(state, x) do
@@ -73,19 +78,20 @@ defmodule Advent2017.Day18 do
   is greater than zero. (An offset of 2 skips the next instruction, an offset
   of -1 jumps to the previous instruction, and so on.)
 
-      iex> Advent2017.Day18.jgz(%{f: 3}, "3", "2")
-      2
-      iex> Advent2017.Day18.jgz(%{f: 3}, "-3", "2")
-      0
+      iex> Advent2017.Day18.jgz(%{pointer: 0, f: 3}, "3", "2")
+      %{pointer: 2, f: 3}
+      iex> Advent2017.Day18.jgz(%{pointer: 0, f: 3}, "-3", "2")
+      %{pointer: 1, f: 3}
   """
   def jgz(state, x, y) do
     case e(state, x) > 0 do
-      true -> e(state, y)
-      false -> 0
+      true  -> Map.update!(state, :pointer, &(&1 + e(state, y)))
+      false -> next(state)
     end
   end
 
   def a(str), do: String.to_atom(str)
+  def next(state), do: Map.update!(state, :pointer, &(&1+1))
 
   @doc ~S"""
   e evaluates a variable by the stack. If there's a register, it returns that.
@@ -120,6 +126,7 @@ defmodule Advent2017.Day18 do
 
     file
     |> String.split("\n")
-    |> run(debug: true)
+    |> run(%{pointer: 0})
+    |> Map.fetch!(:snd)
   end
 end
