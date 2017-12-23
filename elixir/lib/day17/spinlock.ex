f64301b8e2254cff311d4be82cf6571836e3b32d
Evan Travers <evantravers@gmail.com>
2017-12-23 13:32:42 -0600

    Credo refactors

--- elixir/lib/day17/spinlock.ex
+++ elixir/lib/day17/spinlock.ex
@@ -2,11 +2,11 @@ defmodule Advent2017.Day17 do
   def spinlock(stop), do: spinlock({0}, 0, stop)
   def spinlock(buffer, position, stop) do
     value = tuple_size(buffer)
-    cond do
-      value > stop -> buffer
-      true ->
-        position = rem(position + 394, value)+1
-        spinlock(Tuple.insert_at(buffer, position, value), position, stop)
+    if value > stop do
+      buffer
+    else
+      position = rem(position + 394, value) + 1
+      spinlock(Tuple.insert_at(buffer, position, value), position, stop)
     end
   end
 
@@ -14,16 +14,16 @@ defmodule Advent2017.Day17 do
     buffer =
       spinlock(2017)
 
-    elem(buffer, Enum.find_index(Tuple.to_list(buffer), & &1 == 2017)+1)
+    elem(buffer, Enum.find_index(Tuple.to_list(buffer), & &1 == 2017) + 1)
   end
 
   def p2 do
     elem(
-      Enum.reduce(1..50_000_000, {0, 0}, fn i, {secondElement, index} ->
-        countedTo     = rem((index + 394), i)
-        secondElement = if countedTo == 0, do: i, else: secondElement
+      Enum.reduce(1..50_000_000, {0, 0}, fn i, {second_element, index} ->
+        counted_to     = rem((index + 394), i)
+        second_element = if counted_to == 0, do: i, else: second_element
 
-        {secondElement, countedTo + 1}
+        {second_element, counted_to + 1}
       end),
       0)
   end
