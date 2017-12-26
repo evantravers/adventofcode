17047f31e3660235af64f283bba5ab5ed4310ff3
Evan Travers <evantravers@gmail.com>
2017-12-26 17:37:52 -0600

    Write Day24 P1 and P2
    The crux of the problem is that a port could match either side of the
    component, but if you rotate it you need to pay attention to it's
    rotation for both removing it from the remaining possible pieces for
    what can connect to it.

--- elixir/lib/day24/moat.ex
+++ elixir/lib/day24/moat.ex
@@ -1,3 +1,5 @@
+require IEx
+
 defmodule Advent2017.Day24 do
   @moduledoc """
   Each component has two ports, one on each end. The ports come in all
@@ -16,20 +18,76 @@ defmodule Advent2017.Day24 do
   example, if your bridge is made of components 0/3, 3/7, and 7/4, your bridge
   has a strength of 0+3 + 3+7 + 7+4 = 24.
   """
-  def build_graph(filename) do
+  def load_components(filename) do
     {:ok, file} = File.read(__DIR__ <> "/#{filename}")
 
-    edges =
-      file
-      |> String.split("\n", trim: true)
-      |> Enum.map(fn component ->
-        [v1, v2] = component
-                   |> String.split("/")
-                   |> Enum.map(&String.to_integer &1)
+    file
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn comp ->
+      comp
+      |> String.split("/")
+      |> Enum.map(&String.to_integer &1)
+    end)
+    |> Enum.with_index
+  end
 
-        Graph.Edge.new(v1, v2, weight: v1 + v2)
+  def combinations(components, {[_, port], _}, path \\ []) do
+    connectable =
+      components
+      |> Enum.filter(fn {[p1, p2], _} -> p1 == port or p2 == port end)
+      |> Enum.map(fn {[p1, p2], id} ->
+        if p1 == port do
+          {[p1, p2], id}
+        else
+          {[p2, p1], id}
+        end
       end)
 
-    Graph.add_edges(Graph.new, edges)
+    if Enum.empty? connectable do
+      [path]
+    else
+      connectable
+      |> Enum.map(fn next ->
+        components
+        |> remove_used(next)
+        |> combinations(next, [next|path])
+      end)
+      |> Enum.concat
+    end
+  end
+  defp remove_used(components, {[_, _], id}) do
+    List.keydelete(components, id, 1)
+  end
+
+  def weigh(list) do
+    Enum.reduce(list, 0, fn {[p1, p2], _}, weight -> weight + p1 + p2 end)
+  end
+
+  def p1 do
+    "input.txt"
+    |> load_components
+    |> combinations({[0, 0], nil})
+    |> Enum.map(&weigh &1)
+    |> Enum.max
+  end
+
+  def p2 do
+    paths =
+      "input.txt"
+      |> load_components
+      |> combinations({[0, 0], nil})
+
+    longest =
+      paths
+      |> Enum.map(fn path -> {path, length(path)} end)
+      |> List.keysort(1)
+      |> Enum.reverse
+      |> List.first
+      |> (fn {_, size} -> size end).()
+
+    paths
+    |> Enum.filter(fn path -> length(path) == longest end)
+    |> Enum.map(&weigh &1)
+    |> Enum.max
   end
 end
