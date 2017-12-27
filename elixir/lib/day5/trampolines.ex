22e25c4bd98ae37c2fc4a0e790ac916799c96dbd
Evan Travers <evantravers@gmail.com>
2017-12-27 14:54:33 -0600

    Credo fixes

--- elixir/lib/day5/trampolines.ex
+++ elixir/lib/day5/trampolines.ex
@@ -1,4 +1,19 @@
 defmodule Advent2017.Day5 do
+  @moduledoc """
+  An urgent interrupt arrives from the CPU: it's trapped in a maze of jump
+  instructions, and it would like assistance from any programs with spare
+  cycles to help find the exit.
+
+  The message includes a list of the offsets for each jump. Jumps are relative:
+  -1 moves to the previous instruction, and 2 skips the next one. Start at the
+  first instruction in the list. The goal is to follow the jumps until one
+  leads outside the list.
+
+  In addition, these instructions are a little strange; after each jump, the
+  offset of that instruction increases by 1. So, if you come across an offset
+  of 3, you would move three instructions forward, but change it to a 4 for the
+  next time it is encountered.
+  """
   def trampoline(list, index, jumps, inc) do
     cond do
       index < 0 ->
@@ -11,7 +26,7 @@ defmodule Advent2017.Day5 do
 
         list
         |> put_elem(index, inc.(value))
-        |> trampoline(offset, jumps+1, inc)
+        |> trampoline(offset, jumps + 1, inc)
     end
   end
 
