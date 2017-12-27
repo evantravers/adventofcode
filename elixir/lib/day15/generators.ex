232eea43c5f330a932c6b5e1ddd398595e259303
Evan Travers <evantravers@gmail.com>
2017-12-27 11:23:04 -0600

    Fixing tests
    You can alias modules in doctests! Yay!

--- elixir/lib/day15/generators.ex
+++ elixir/lib/day15/generators.ex
@@ -28,9 +28,9 @@ defmodule Advent2017.Day15 do
   def first_sixteen(string), do: String.slice(string, -16..-1)
 
   @doc ~S"""
-      iex> Advent2017.Day15.judge(1181022009, 1233683848)
+      iex> judge(1181022009, 1233683848)
       false
-      iex> Advent2017.Day15.judge(245556042, 1431495498)
+      iex> judge(245556042, 1431495498)
       true
    """
   def judge(genA, genB) do
@@ -48,31 +48,31 @@ defmodule Advent2017.Day15 do
   end
 
   @doc ~S"""
-      iex> Advent2017.Day15.judge_bitwise(1181022009, 1233683848)
+      iex> judge_bitwise(1181022009, 1233683848)
       false
-      iex> Advent2017.Day15.judge_bitwise(245556042, 1431495498)
+      iex> judge_bitwise(245556042, 1431495498)
       true
    """
   def judge_bitwise(genA, genB) do
     '0000000000000000' ==
       genA
-      |> Bitwise.^^^ genB
+      |> Bitwise.^^^(genB)
       |> Integer.to_charlist(2)
       |> Enum.slice(-16..-1)
   end
 
 
   @doc ~S"""
-      iex> Advent2017.Day15.generate(65, 16807)
+      iex> generate(65, 16807)
       1092455
-      iex> Advent2017.Day15.generate(8921, 48271)
-      ...> |> Advent2017.Day15.generate(48271)
+      iex> generate(8921, 48271)
+      ...> |> generate(48271)
       1233683848
 
-      iex> Advent2017.Day15.generate(65, 16807, &rem(&1, 4)==0)
+      iex> generate(65, 16807, &rem(&1, 4)==0)
       1352636452
-      iex> Advent2017.Day15.generate(8921, 48271, &rem(&1, 8)==0)
-      ...> |> Advent2017.Day15.generate(48271, &rem(&1, 8)==0)
+      iex> generate(8921, 48271, &rem(&1, 8)==0)
+      ...> |> generate(48271, &rem(&1, 8)==0)
       862516352
   """
   def generate(previous, factor, comparison \\ fn (_) -> true end) do
--- elixir/test/day_test.exs
+++ elixir/test/day_test.exs
@@ -5,10 +5,11 @@ defmodule Advent2017Test do
   doctest Advent2017.Day10
   doctest Advent2017.Day11
   doctest Advent2017.Day13
-  doctest Advent2017.Day14
-  doctest Advent2017.Day15
+  doctest Advent2017.Day14, import: true
+  doctest Advent2017.Day15, import: true
   doctest Advent2017.Day16
-  doctest Advent2017.Day18
+  doctest Advent2017.Day18, import: true
+  doctest Advent2017.Day18.Machine, import: true
   doctest Advent2017.Day20
   doctest Advent2017.Day20.Particle
   doctest Advent2017.Day21
