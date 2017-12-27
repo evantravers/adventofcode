1c73b0995895f72dc7164ab97f80807732f965c0
Evan Travers <evantravers@gmail.com>
2017-12-26 21:27:02 -0600

    Add moduledoc for credo

--- elixir/lib/day14/defrag.ex
+++ elixir/lib/day14/defrag.ex
@@ -1,6 +1,34 @@
-require IEx
-
 defmodule Advent2017.Day14 do
+  @moduledoc """
+  Suddenly, a scheduled job activates the system's disk defragmenter. Were the
+  situation different, you might sit and watch it for a while, but today, you
+  just don't have that kind of time. It's soaking up valuable system resources
+  that are needed elsewhere, and so the only option is to help it finish its
+  task as soon as possible.
+
+  The disk in question consists of a 128x128 grid; each square of the grid is
+  either free or used. On this disk, the state of the grid is tracked by the
+  bits in a sequence of knot hashes.
+
+  A total of 128 knot hashes are calculated, each corresponding to a single row
+  in the grid; each hash contains 128 bits which correspond to individual grid
+  squares. Each bit of a hash indicates whether that square is free (0) or used
+  (1).
+
+  The hash inputs are a key string (your puzzle input), a dash, and a number
+  from 0 to 127 corresponding to the row. For example, if your key string were
+  flqrgnkx, then the first row would be given by the bits of the knot hash of
+  flqrgnkx-0, the second row from the bits of the knot hash of flqrgnkx-1, and
+  so on until the last row, flqrgnkx-127.
+
+  The output of a knot hash is traditionally represented by 32 hexadecimal
+  digits; each of these digits correspond to 4 bits, for a total of 4 * 32 =
+  128 bits. To convert to bits, turn each hexadecimal digit to its equivalent
+  binary value, high-bit first: 0 becomes 0000, 1 becomes 0001, e becomes 1110,
+  f becomes 1111, and so on; a hash that begins with a0c2017... in hexadecimal
+  would begin with 10100000110000100000000101110000...  in binary.
+  """
+
   alias Advent2017.Day10, as: Knot
 
   @spec hex_to_bin(String) :: String
