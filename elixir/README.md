bcddbb4ad3552254581f7443d244ad32ca69ca65
Evan Travers <evantravers@gmail.com>
2017-12-29 13:06:16 -0600

    Update mix command

--- elixir/README.md
+++ elixir/README.md
@@ -1,7 +1,7 @@
 # Advent 2017 in Elixir
 
 This mix application is just for testing and such. If you want to run all the
-solutions, run `mix run_all`. I've been developing and testing by running `iex
+solutions, run `mix day all`. I've been developing and testing by running `iex
 -S mix` and running solutions in iex by typing `Advent2017.Day<num>.p1`. You
 can also run a specific day's solution w/ `mix day <num>`.
 
--- elixir/lib/advent2017.ex
+++ elixir/lib/advent2017.ex
@@ -5,48 +5,6 @@ defmodule Advent2017 do
   I started out writing it in random .exs scripts, but now I'm putting it into
   mix so I can use tests.
   """
-
-  def run do
-    [
-      Advent2017.Day1,
-      Advent2017.Day2,
-      Advent2017.Day3,
-      Advent2017.Day4,
-      Advent2017.Day5,
-      Advent2017.Day6,
-      Advent2017.Day7,
-      Advent2017.Day8,
-      Advent2017.Day9,
-      Advent2017.Day10,
-      Advent2017.Day11,
-      Advent2017.Day12,
-      Advent2017.Day13,
-      Advent2017.Day14,
-      Advent2017.Day15,
-      Advent2017.Day16,
-      Advent2017.Day17,
-      Advent2017.Day18,
-      Advent2017.Day19,
-      Advent2017.Day21,
-      Advent2017.Day22,
-      Advent2017.Day24,
-      Advent2017.Day25
-    ]
-    |> Enum.map(fn(day) ->
-      IO.puts "#{day}\n" <>
-      "Part 1:\n#{day.p1()}\n" <>
-      "Part 2:\n#{day.p2()}\n\n"
-    end)
-  end
-end
-
-defmodule Mix.Tasks.RunAll do
-  use Mix.Task
-
-  @shortdoc "Simply runs all the problems from 2017"
-  def run(_) do
-    Advent2017.run
-  end
 end
 
 defmodule Mix.Tasks.Day do
@@ -54,8 +12,15 @@ defmodule Mix.Tasks.Day do
 
   @shortdoc "Run a specific day from the problem set"
   def run(day) do
-    IO.puts "Day #{day}: \n" <>
-    "Part 1:\n#{apply(String.to_existing_atom("Elixir.Advent2017.Day#{day}"), :p1, [])}\n" <>
-    "Part 2:\n#{apply(String.to_existing_atom("Elixir.Advent2017.Day#{day}"), :p2, [])}"
+    if day == ["all"] do
+      Enum.each(1..25, fn d ->
+        run d
+        IO.puts "\n"
+      end)
+    else
+      IO.puts "Day #{day}: \n" <>
+      "Part 1:\n#{apply(String.to_existing_atom("Elixir.Advent2017.Day#{day}"), :p1, [])}\n" <>
+      "Part 2:\n#{apply(String.to_existing_atom("Elixir.Advent2017.Day#{day}"), :p2, [])}"
+    end
   end
 end
