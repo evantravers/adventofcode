c4d1fd77f98a8a500653f8b1670df2bb48179387
Evan Travers <evantravers@gmail.com>
2018-03-08 17:38:05 -0600

    Still working on Day1
    Getting an answer, it's just the wrong answer.

--- 2016/elixir/lib/day01/taxicab.ex
+++ 2016/elixir/lib/day01/taxicab.ex
@@ -19,20 +19,45 @@ defmodule Advent2016.Day1 do
   def turn(state = %{compass: "S"}, "R"), do: %{state | compass: "W"}
   def turn(state = %{compass: "E"}, "R"), do: %{state | compass: "N"}
 
-  def step, do: nil
+  def step(state, 0), do: state
+  def step(state, distance) do
+    {x, y} = hd(state.visited)
 
-  def walk(state) do
+    next_position =
+      case state.compass do
+        "N" -> {x, y + 1}
+        "E" -> {x + 1, y}
+        "S" -> {x, y - 1}
+        "W" -> {x - 1, y}
+      end
+
+    %{state | visited: [next_position | state.visited]}
+    |> step(distance - 1)
+  end
+
+  def walk(state, []), do: state
+  def walk(state, [instruction|instructions]) do
     # take an instruction
-    [direction, distance] = state.instructions
-                            |> hd
-                            |> String.split("", trim: true)
-    # turn the direction that we need to go
-    # recursively step that direction
+    [_, direction, distance] = Regex.run(~r/([L|R])(.*)/, instruction)
+
+    distance = String.to_integer(distance)
+
+    state
+    |> turn(direction)
+    |> step(distance)
+    |> walk(instructions)
   end
 
   def p1 do
-    %{compass: "N", visited: [{0, 0}], instructions: load_instructions()}
-    |> walk
+    the_long_walk =
+      %{compass: "N", visited: [{0, 0}]}
+      |> walk(load_instructions())
+
+    the_long_walk.visited
+    |> hd
+    |> Tuple.to_list
+    |> Enum.map(&abs &1)
+    |> Enum.sum
   end
 
   def p2, do: nil
