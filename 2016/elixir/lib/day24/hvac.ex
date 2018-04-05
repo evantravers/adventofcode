335ea2e6a195c0f17e698828d07ebef5dc2a6ad2
Evan Travers <evantravers@gmail.com>
2018-04-05 10:08:01 -0500

    WIP Day24

--- 2016/elixir/lib/day24/hvac.ex
+++ 2016/elixir/lib/day24/hvac.ex
@@ -1,3 +1,5 @@
+alias Graph.Edge
+
 defmodule Advent2016.Day24 do
   @moduledoc """
   You extract the duct layout for this area from some blueprints you acquired
@@ -15,9 +17,33 @@ defmodule Advent2016.Day24 do
       |> Enum.map(&String.split(&1, "", trim: true))
 
     for {row, y} <- Enum.with_index(coords),
-        {val, x} <- Enum.with_index(row)
+        {val, x} <- Enum.with_index(row),
+        val != "#"
     do
       {{x, y}, val}
+    end
+  end
+
+  def build_graph(list, edges \\ [])
+  def build_graph([], edges) do
+    Graph.new
+    |> Graph.add_edges(edges)
+  end
+  def build_graph([{coord, val}|coords], edges) do
+    adjacent =
+      coord
+      |> valid_moves(coords)
+      |> Enum.map(&Edge.new({coord, val}, &1))
+
+    build_graph(coords, adjacent ++ edges)
+  end
+
+  def valid_moves({x, y}, list) do
+    for target_x <- (x - 1)..(x + 1),
+        target_y <- (y - 1)..(y + 1),
+        !is_nil(List.keyfind(list, {target_x, target_y}, 0))
+    do
+      {target_x, target_y}
     end
   end
 
@@ -25,9 +51,21 @@ defmodule Advent2016.Day24 do
     {:ok, file} = File.read("#{__DIR__}/#{file}")
 
     file
-    |> load_list_of_coords
   end
 
-  def p1, do: nil
+  def test do
+    "test.txt"
+    |> load_input
+    |> load_list_of_coords
+    |> build_graph
+  end
+
+  def p1 do
+    "input.txt"
+    |> load_input
+    |> load_list_of_coords
+    |> build_graph
+  end
+
   def p2, do: nil
 end
