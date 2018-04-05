ccf7812d22aa018b9e4faa65e1ae52c9f2519b6f
Evan Travers <evantravers@gmail.com>
2018-04-05 09:52:22 -0500

    Add doctest

--- 2016/elixir/test/advent2016_test.exs
+++ 2016/elixir/test/advent2016_test.exs
@@ -3,5 +3,7 @@ defmodule Advent2016Test do
 
   doctest Advent2016.Day2, import: true
   doctest Advent2016.Day3, import: true
+  doctest Advent2016.Day4, import: true
+  doctest Advent2016.Day5, import: true
   doctest Advent2016.Day19, import: true
 end
