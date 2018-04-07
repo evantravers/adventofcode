10ad7cac3afd5fd0401a72d7f6d011199888fc31
Evan Travers <evantravers@gmail.com>
2018-04-07 16:42:15 -0500

    Solve part 2016.5.2
    I'm not crazy about this solution, there's some *ugly* elixir in here,
    but I get results, so that's nice.
    
    I refactored `decode/3` to take another parameter (thus becoming
    `decode/4`), this new param is a function that fills the password hash
    (which used to be a list) based on the logic for the part of the puzzle.
    
    If I *really* wanted to refactor for speed, I'd probably have to figure out
    a way to go through the numbers only once.

--- 2016/elixir/lib/day05/password_hash.ex
+++ 2016/elixir/lib/day05/password_hash.ex
@@ -14,29 +14,60 @@ defmodule Advent2016.Day5 do
     binary_part(encrypt(input, number), 0, 3) <= <<0, 0, 15>>
   end
 
-  def sixth_char(binary) do
-    binary
-    |> Base.encode16
-    |> String.graphemes
-    |> Enum.at(5)
+  def sixth_char(binary, password) do
+    index =
+      if map_size(password) == 0 do
+        0
+      else
+        (password |> Map.keys |> Enum.max) + 1
+      end
+
+    Map.put(password, index,
+      binary
+        |> Base.encode16
+        |> String.graphemes
+        |> Enum.at(5)
+    )
   end
 
-  def decode(input, number \\ 0, password \\ [])
-  def decode(_, _, password) when length(password) == 8 do
-    password |> Enum.reverse |> Enum.join
-  end
-  def decode(input, number, password) do
-    if matches(input, number) do
-      decode(input, number + 1, [sixth_char(encrypt(input, number)) | password])
+  @doc """
+  The sixth character is the position of the character in the seventh place.
+  """
+  def magic_char(binary, password) do
+    string = binary |> Base.encode16 |> String.graphemes()
+
+    position = Enum.at(string, 5)
+    char     = Enum.at(string, 6)
+
+    if Enum.member?(?0..?7, hd(String.to_charlist(position))) do
+      Map.put_new(password, position, char)
     else
-      decode(input, number + 1, password)
+      password
+    end
+  end
+
+  def decode(input, next_char, number \\ 0, password \\ %{})
+  def decode(_, _, _, password) when map_size(password) == 8 do
+    with {_, chars} <- password |> Enum.sort |> Enum.unzip
+    do
+      chars
+      |> Enum.join
+      |> String.downcase
+    end
+  end
+  def decode(input, next_char, number, password) do
+    if matches(input, number) do
+      decode(input, next_char, number + 1, next_char.(encrypt(input, number), password))
+    else
+      decode(input, next_char, number + 1, password)
     end
   end
 
   def p1 do
-    decode(@input)
+    decode(@input, &sixth_char/2)
   end
 
   def p2 do
+    decode(@input, &magic_char/2)
   end
 end
