c05290fa2786545e77254da3850efdd8bf0d4e07
Evan Travers <evantravers@gmail.com>
2018-04-09 13:06:29 -0500

    Code golfing. ⛳

--- 2017/ruby/day12/digital_plumber.rb
+++ 2017/ruby/day12/digital_plumber.rb
@@ -1,18 +1,4 @@
 require 'tsort'
-
-class Hash
-  include TSort
-  alias tsort_each_node each_key
-  def tsort_each_child(node, &block)
-    fetch(node).each(&block)
-  end
-end
-
-tree =
-  File.read("./input.txt")
-    .split("\n")
-    .map{|v| n, v = v.split(" <-> "); [n, v.split(', ')]}
-    .to_h
-
-puts "p1: #{tree.strongly_connected_components.first.count}"
-puts "p2: #{tree.strongly_connected_components.count}"
+class Hash include TSort; alias tsort_each_node each_key; def tsort_each_child(node, &block) fetch(node).each(&block) end;end
+t=File.read("./input.txt").split("\n").map{|v|n,v=v.split(" <-> ");[n,v.split(', ')]}.to_h
+puts "p1: #{t.strongly_connected_components[0].size}\np2: #{t.strongly_connected_components.size}"
