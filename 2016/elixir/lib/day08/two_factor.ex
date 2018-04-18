234a5effdd99318b2a2b4ab4ead8d1feacdaafac
Evan Travers <evantravers@gmail.com>
2018-04-17 22:10:59 -0500

    REWRITE. I DEMAND A REWRITE.
    I went through a couple iterations, I was convinced I needed a struct or
    a MapSet at some point, I think I'm just going to give this a whirl. I
    think a list is probably a slow implementation for now, and a MapSet
    would be *faster*, yet it adds a lot of cruft, and I like how simple
    this is so far.

--- 2016/elixir/lib/day08/two_factor.ex
+++ 2016/elixir/lib/day08/two_factor.ex
@@ -3,64 +3,33 @@ defmodule Advent2016.Day8 do
   http://adventofcode.com/2016/day/8
   """
 
-  @doc """
-  rect AxB turns on all of the pixels in a rectangle at the top-left of the
-  screen which is A wide and B tall.
-  """
-  def rect(screen, a, b) do
-    for y <- 0..a-1 do
-      for x <- 0..b-1 do
+  def rect(screen, x, y) do
+    for x <- 0..x-1 do
+      for y <- 0..y-1 do
         {x, y}
       end
     end
     |> List.flatten
-    |> Enum.reduce(screen, fn ({x, y}, s) ->
-      put_in(s, [y, x], true)
-    end)
   end
 
-  @doc """
-  rotate row y=A by B shifts all of the pixels in row A (0 is the top row)
-  right by B pixels. Pixels that would fall off the right end appear at the
-  left end of the row.
-  """
-  def rotate(screen, :row, y, b) do
+  def rotate(screen, :row, y, offset) do
+    {row, remainder} =
+      screen
+      |> Enum.split_with(fn({_, target}) -> target = y end)
+
+    row
+    |> Enum.map(fn({x, y}) -> {x, y + rem(offset, 50)} end)
+    |> Kernel.++(remainder)
   end
 
-  @doc """
-  rotate column x=A by B shifts all of the pixels in column A (0 is the left
-  column) down by B pixels. Pixels that would fall off the bottom appear at the
-  top of the column.
-  """
-  def rotate(screen, :column, x, b) do
-  end
+  def rotate(screen, :column, x, offset) do
+    {column, remainder} =
+      screen
+      |> Enum.split_with(fn({_, target}) -> target = x end)
 
-  def display(screen) do
-    for y <- 0..screen.y-1 do
-      for x <- 0..screen.x-1 do
-        case get_in(screen, [y, x]) do
-          true  -> "#"
-          false -> "."
-        end
-      end
-      |> Enum.join
-      |> Kernel.<> "\n"
-    end
-    |> Enum.join
-  end
-
-  @doc "builds a map of maps of size x, y"
-  def screen(x, y) do
-    for y <- 0..y-1, into: %{} do
-      {y, (for x <- 0..x-1, into: %{}, do: {x, false})}
-    end
-    |> Map.put(:x, x)
-    |> Map.put(:y, y)
-  end
-
-  def test do
-    screen(5, 5)
-    |> rect(2, 3)
+    row
+    |> Enum.map(fn({x, y}) -> {x + rem(offset, 6), y} end)
+    |> Kernel.++(remainder)
   end
 
   def p1, do: nil
