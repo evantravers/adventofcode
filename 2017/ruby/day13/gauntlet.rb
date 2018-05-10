a275087222bb91d169a990982e961b1b71db4fd2
Evan Travers <evantravers@gmail.com>
2018-05-10 17:46:28 -0500

    Solve 2017.13.2 in ruby
    I knew this would work... I'm kind of sad Barret beat me to it, but I
    think I'm more proud of him...

--- 2017/ruby/day13/gauntlet.rb
+++ 2017/ruby/day13/gauntlet.rb
@@ -1,5 +1,4 @@
 # WITH MATHS!
-require 'set'
 require 'pry'
 require 'minitest/autorun'
 
@@ -19,47 +18,16 @@ def firewall_inactive?(depth, range)
   !firewall_active?(depth, range)
 end
 
-def period(delay, range)
-  ((range - 1) * 2)
-end
-
-# FIXME this is broken
-# for a failure, delete all options in possible where
-# (n*period_of_gate)+delay
-# after writing functional code, this seems awful
-def filter(possible, failure, depth_and_delay)
-  (0..possible.max/2).each do |n|
-    possible.delete((n * period( * failure)) + depth_and_delay)
-  end
-  possible
-end
-
-# returns either nil or an array of [delay, range], man this is making miss
-# functional programming
-def failed_the_gauntlet?(delay, gauntlet)
-  gauntlet.map{|(d, r)| [d+delay, r]}
-          .find{|(depth, range)| firewall_inactive?(depth, range)}
-end
-
-def find_delay(gauntlet, max=4_000_000)
-  possible_delays = (2..max).to_a
-
-  until possible_delays.empty?
-    delay = possible_delays.shift
-
-    failure = failed_the_gauntlet?(delay, gauntlet)
-    unless failure
-      return delay
-    else
-      possible_delays = filter(possible_delays, failure, delay)
-    end
-  end
-end
-
 def p2
   gauntlet = load_input
 
-  find_delay(gauntlet)
+  options = (0..5_000_000).to_a
+
+  gauntlet.each do |(depth, range)|
+    options.reject!{|delay| firewall_active?(delay+depth, range)}
+  end
+
+  options.min
 end
 
 class FirewallTest < Minitest::Test
@@ -70,15 +38,4 @@ class FirewallTest < Minitest::Test
   end
 end
 
-class GauntletTest < Minitest::Test
-  def test_gauntlet
-    assert !failed_the_gauntlet?(9, [[0, 3], [1, 2], [4, 4], [6, 4]]).nil?
-    assert_equal nil, failed_the_gauntlet?(10, [[0, 3], [1, 2], [4, 4], [6, 4]])
-  end
-
-  def test_find_delay
-    assert_equal 10, find_delay([[0, 3], [1, 2], [4, 4], [6, 4]], 15)
-  end
-end
-
-# puts p2
+puts p2
