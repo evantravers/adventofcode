dad538795e377548d1c358fade00b278387d4521
Evan Travers <evantravers@gmail.com>
2018-06-11 16:46:52 -0500

    Solve 2016.9.2 in elixir
    I have rewritten this thing like... five times. Mistakes I made:
    
    - Trying to do this as a sort of pseudo-reduce function
    - Trying to avoid putting the whole state into a single map
    - Having a different "state" for the `marker` functions than the `count`
    - Forgetting to call `decrement_multipliers` on every character advance
    - Rejecting empty multipliers at the wrong place in
      `decrement_multipliers` (still not sure why that broke it, just know
      that it passes tests now!)
    
    I still don't like how ugly the `marker` is, I think it could be broken
    up or something. I mean... this *is* what regexes was made for, but for
    some reason I'm being stubborn.

--- 2016/elixir/lib/day09/crypto_bomb.ex
+++ 2016/elixir/lib/day09/crypto_bomb.ex
@@ -15,78 +15,98 @@ defmodule Advent2016.Day9 do
   """
 
   @doc ~S"""
-      iex> decompress("ADVENT")
-      6
-      iex> decompress("A(1x5)BC")
-      7
       iex> decompress("(3x3)XYZ")
       9
-      iex> decompress("A(2x2)BCD(2x2)EFG")
-      11
-      iex> decompress("(6x1)(1x3)A")
-      6
+
       iex> decompress("X(8x2)(3x3)ABCY")
-      18
+      20
+
+      iex> decompress("(27x12)(20x12)(13x14)(7x10)(1x12)A")
+      241920
+
+      iex> decompress("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN")
+      445
   """
   def decompress(string) do
-    string
-    |> String.graphemes
-    |> decrypt
-  end
-
-  def current_multiplier(multipliers) do
-    multipliers
-    |> Enum.reduce(1, fn({_, multiplier}, total) -> total * multiplier end)
-  end
-
-  def decrement(multipliers) do
-    multipliers
-    |> Enum.map(fn({len, multiplier}) -> {len-1, multiplier} end)
-  end
-
-  def decrypt(string, score \\ 0, multipliers \\ [])
-  def decrypt([], score, _), do: score
-  def decrypt([char|chars], score, multipliers) do
-    case char do
-      "(" -> # it's a control marker!
-        {len, multiplier} = marker(chars)
-        marker_length = byte_size("#{len}x#{multiplier})") # skip ahead
-
-        decrypt(Enum.drop(chars, marker_length), score, [{len, multiplier}|multipliers])
-      _   -> nil
-        decrypt(chars, score + current_multiplier(multipliers), decrement(multipliers))
-    end
-  end
-
-  @doc ~S"""
-  This might be the most offensive function I've ever written. It's very
-  opaque:
-
-  1) It goes through the marker, looking for an "x".
-  2) Once it finds an "x", it stops, converts that string to an integer, and
-     runs itself on the next part of the string.
-  3) Once the second copy finds a ")", it stops and returns its integer.
-  4) It returns back to the caller which returns the tuple.
-
-      iex> marker(String.graphemes("3x5)"))
-      {3, 5}
-  """
-  def marker(chars, number \\ "")
-  def marker([char|chars], number) do
-    case char do
-      "x" ->
-        {String.to_integer(number), marker(chars, "")}
-      ")" ->
-        String.to_integer(number)
-      _   ->
-        marker(chars, number <> char)
-    end
+    string |> String.graphemes |> count
   end
 
   def load_input do
     with {:ok, file} <- File.read("#{__DIR__}/input") do
       file
       |> String.replace(~r/\s/, "")
+    end
+  end
+
+  def decrement_multipliers(state) do
+    state
+    |> Map.update(:mul, [], fn(list_of_multipliers) ->
+      list_of_multipliers
+      |> Enum.reject(fn({l, _}) -> l == 0 end)
+      |> Enum.map(fn({length, mul}) ->
+        {length - 1, mul}
+      end)
+    end)
+  end
+
+  def multiplier(state) do
+    state
+    |> Map.get(:mul)
+    |> Enum.map(fn({_, mul}) -> mul end)
+    |> Enum.reduce(1, fn(num, acc) -> num * acc end)
+  end
+
+  def increase_score(state) do
+    state |> Map.update(:score, 0, & &1 + multiplier(state))
+  end
+
+  def marker(state = %{chars: [char|chars]}, length \\ nil, acc \\ "") do
+    case char do
+      "x" ->
+        state
+        |> decrement_multipliers
+        |> Map.put(:chars, chars)
+        |> marker(String.to_integer(acc), "")
+      ")" ->
+        state
+        |> decrement_multipliers
+        |> Map.put(:chars, chars)
+        |> Map.update(:mul, [], fn(mul) ->
+          [{length, String.to_integer(acc)}|mul]
+        end)
+        |> count
+      _   ->
+        state
+        |> decrement_multipliers
+        |> Map.put(:chars, chars)
+        |> marker(length, acc <> char)
+    end
+  end
+
+  @doc """
+  # Model:
+  - chars: current characters to iterate through
+  - score: final score for the end of p2
+  - mul: currently active multipliers
+  """
+  def count(list_of_characters) when is_list(list_of_characters) do
+    count(%{chars: list_of_characters, score: 0, mul: []})
+  end
+
+  def count(state = %{chars: []}), do: state.score
+  def count(state = %{chars: [char|chars]}) do
+    case char do
+      "(" ->
+        state
+        |> decrement_multipliers
+        |> Map.put(:chars, chars)
+        |> marker
+      _ ->
+        state
+        |> decrement_multipliers
+        |> increase_score
+        |> Map.put(:chars, chars) # essentially continue the reduce
+        |> count
     end
   end
 
