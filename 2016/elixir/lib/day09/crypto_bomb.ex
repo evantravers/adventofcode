55549c6033bed6513d3303a5724de8f4190c52c0
Evan Travers <evantravers@gmail.com>
2018-06-11 18:18:32 -0500

    Solve 2016.9.1
    It's a little ugly, but it works and is relatively clean. Both
    solutions only travel through the input once, which is nice.

--- 2016/elixir/lib/day09/crypto_bomb.ex
+++ 2016/elixir/lib/day09/crypto_bomb.ex
@@ -2,6 +2,7 @@ defmodule Advent2016.Day9 do
   @moduledoc """
   http://adventofcode.com/2016/day/9
 
+  P1:
   The format compresses a sequence of characters. Whitespace is ignored. To
   indicate that some sequence should be repeated, a marker is added to the
   file, like (10x2). To decompress this marker, take the subsequent 10
@@ -12,6 +13,14 @@ defmodule Advent2016.Day9 do
   If parentheses or other characters appear within the data referenced by a
   marker, that's okay - treat it like normal data, not a marker, and then
   resume looking for markers after the decompressed section.
+
+  P2:
+  Apparently, the file actually uses version two of the format.
+
+  In version two, the only difference is that markers within decompressed data
+  are decompressed. This, the documentation explains, provides much more
+  substantial compression capabilities, allowing many-gigabyte files to be
+  stored in only a few kilobytes.
   """
 
   @doc ~S"""
@@ -27,8 +36,8 @@ defmodule Advent2016.Day9 do
       iex> decompress("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN")
       445
   """
-  def decompress(string) do
-    string |> String.graphemes |> count
+  def decompress(string, opts \\ []) do
+    string |> String.graphemes |> count(opts)
   end
 
   def load_input do
@@ -67,22 +76,28 @@ defmodule Advent2016.Day9 do
   end
 
   def marker(state = %{chars: [char|chars]}, length \\ nil, acc \\ "") do
-    case char do
-      "x" ->
-        state
-        |> advance_to_next_char
-        |> marker(String.to_integer(acc), "")
-      ")" ->
-        state
-        |> advance_to_next_char
-        |> Map.update(:mul, [], fn(mul) ->
-          [{length, String.to_integer(acc)}|mul]
-        end)
-        |> count
-      _   ->
-        state
-        |> advance_to_next_char
-        |> marker(length, acc <> char)
+    if Map.has_key?(state, :simple) && !Enum.empty?(state.mul) do
+      state
+      |> increase_score
+      |> count
+    else
+      case char do
+        "x" ->
+          state
+          |> advance_to_next_char
+          |> marker(String.to_integer(acc), "")
+        ")" ->
+          state
+          |> advance_to_next_char
+          |> Map.update(:mul, [], fn(mul) ->
+            [{length, String.to_integer(acc)}|mul]
+          end)
+          |> count
+        _   ->
+          state
+          |> advance_to_next_char
+          |> marker(length, acc <> char)
+      end
     end
   end
 
@@ -92,8 +107,10 @@ defmodule Advent2016.Day9 do
   - score: final score for the end of p2
   - mul: currently active multipliers
   """
-  def count(list_of_characters) when is_list(list_of_characters) do
-    count(%{chars: list_of_characters, score: 0, mul: []})
+  def count(list_of_characters, opts) when is_list(list_of_characters) do
+    opts
+    |> Enum.into(%{chars: list_of_characters, score: 0, mul: []})
+    |> count()
   end
 
   def count(state = %{chars: []}), do: state.score
@@ -111,7 +128,11 @@ defmodule Advent2016.Day9 do
     end
   end
 
-  def p1, do: nil
+  def p1 do
+    load_input()
+    |> decompress(simple: true)
+  end
+
   def p2 do
     load_input()
     |> decompress
