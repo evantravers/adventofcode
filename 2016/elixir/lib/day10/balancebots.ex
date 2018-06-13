f58336cab20708b7bc6a55353c2f50adf65f88b6
Evan Travers <evantravers@gmail.com>
2018-06-13 14:59:04 -0500

    Refactor 2016.10
    I'm trying to make this less horrid. I still don't have a good way to
    solve part 1.

--- 2016/elixir/lib/day10/balancebots.ex
+++ 2016/elixir/lib/day10/balancebots.ex
@@ -11,14 +11,20 @@ defmodule Advent2016.Day10 do
   end
 
   @doc """
-  `setup_sim` loads in the input, and sets up a map of bots
+  `setup_sim` loads in the input, and sets up a map of bots, with their inputs
+  and rules.
   """
   def setup_sim do
     load_input()
     |> Enum.reduce(%{}, &read_instruction/2)
   end
 
-  @doc "Pulls the arguments out of the string"
+  @doc """
+  Pulls the arguments out of the string.
+  Takes the "target id" string and transforms it into a list:
+  `[:atom, integer]`. We use this as part of the path to identify bots and
+  outputs in the sim.
+  """
   def extract_arguments(string) do
     ~r/\w+ \d+/
     |> Regex.scan(string)
@@ -30,11 +36,10 @@ defmodule Advent2016.Day10 do
   end
 
   @doc """
-  `read_instruction/2` is designed to be used as part of a reduce... it goes
-  through the input and if the input is a setup instruction, it puts it in the
-  first list in the `acc`, otherwise the second.
+  `read_instruction/2` is designed to be used as the function in a reduce.
 
-  %{bot_id => %{rules: {high_target, low_target}, cargo: []}}
+  For a string in the input, it evaluates it, pulls out the rules or input and
+  puts it in the `sim` map.
   """
   def read_instruction(match = "value" <> _, sim) do # input
     [[_, value], [_, bot_id]] = extract_arguments(match)
@@ -48,11 +53,11 @@ defmodule Advent2016.Day10 do
     [[:bot, bot_id], low, high] = extract_arguments(match)
 
     sim
-    |> put_in(build_as_go([:bot, bot_id, :low]), low)
-    |> put_in(build_as_go([:bot, bot_id, :high]), high)
+    |> put_in(build_path([:bot, bot_id, :low]), low)
+    |> put_in(build_path([:bot, bot_id, :high]), high)
   end
 
-  def unresolved_bots(sim) do
+  def find_unresolved_bots(sim) do
     sim
     |> Map.get(:bot)
     |> Enum.filter(fn({_, att}) ->
@@ -61,33 +66,57 @@ defmodule Advent2016.Day10 do
   end
 
   @doc """
-  Find a bot w/ two cargo, resolve, repeat until done.
+  Takes in a list of unresolved bots (bots w/ two chips) and the sim, and puts
+  out a sim where their chips have been evaluated.
   """
-  def run(sim) do
-    if Enum.empty?(unresolved_bots(sim)) do
+  def redistribute_chips(unresolved_bots, sim) do
+    unresolved_bots
+    |> Enum.reduce(sim, fn({id, attr}, sim) ->
+      sim
+      |> distribute_chip(attr.high, Enum.max(attr.cargo))
+      |> distribute_chip(attr.low, Enum.min(attr.cargo))
+      |> clear_cargo(id)
+    end)
+  end
+
+  @doc """
+  Find bots w/ two cargo, resolve, repeat until done.
+  """
+  def run(sim, target \\ [-1, -1]) do
+    if Enum.empty?(find_unresolved_bots(sim)) do
       sim
     else
       sim
-      |> unresolved_bots
-      |> Enum.reduce(sim, fn({id, attr}, sim) ->
-        sim
-        |> update_in(build_as_go(attr.high) ++ [:cargo], & [Enum.max(attr.cargo)|List.wrap(&1)])
-        |> update_in(build_as_go(attr.low) ++ [:cargo], & [Enum.min(attr.cargo)|List.wrap(&1)])
-        |> pop_in([:bot, id, :cargo])
-        |> elem(1) # throw away the popped value
-      end)
-      |> run
+      |> find_unresolved_bots
+      |> redistribute_chips(sim)
+      |> run(target)
     end
   end
 
-  def build_as_go(list_of_keys) do
+  def distribute_chip(sim, target, chip) do
+    update_in(sim, build_path(target) ++ [:cargo], & [chip|List.wrap(&1)])
+  end
+
+  def clear_cargo(sim, bot_id) do
+    sim
+    |> pop_in([:bot, bot_id, :cargo])
+    |> elem(1) # throw away the popped value
+  end
+
+  def build_path(list_of_keys) do
     list_of_keys
     |> Enum.map(&Access.key(&1, %{}))
   end
 
+  def get_output(sim, num) do
+    sim
+    |> get_in([:output, num, :cargo])
+    |> hd
+  end
+
   def p1 do
     setup_sim()
-    |> run
+    |> run([17, 61])
   end
 
   def p2 do
@@ -95,8 +124,6 @@ defmodule Advent2016.Day10 do
       setup_sim()
       |> run
 
-    hd(get_in(result, [:output, 0, :cargo]))
-    * hd(get_in(result, [:output, 1, :cargo]))
-    * hd(get_in(result, [:output, 2, :cargo]))
+    get_output(result, 0) * get_output(result, 1) * get_output(result, 2)
   end
 end
