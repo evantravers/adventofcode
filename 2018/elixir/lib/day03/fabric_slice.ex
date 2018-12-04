07821a190944cb99e2b0bbf11576b845cba5d0e6
Evan Travers <evantravers@gmail.com>
2018-12-04 13:45:51 -0600

    Solve 2018.3
    I solved 2018.3.1 pretty quickly... but got stuck trying to solve .2 for
    a while... I was trying to be too clever. Finished a working brute force
    in around 5 minutes. ¯\(°_o)/¯

--- 2018/elixir/lib/day03/fabric_slice.ex
+++ 2018/elixir/lib/day03/fabric_slice.ex
@@ -1,6 +1,9 @@
 defmodule Advent2018.Day3 do
   @moduledoc """
-  Each Elf has made a claim about which area of fabric would be ideal for Santa's suit. All claims have an ID and consist of a single rectangle with edges parallel to the edges of the fabric. Each claim's rectangle is defined as follows:
+  Each Elf has made a claim about which area of fabric would be ideal for
+  Santa's suit. All claims have an ID and consist of a single rectangle with
+  edges parallel to the edges of the fabric. Each claim's rectangle is defined
+  as follows:
 
   - The number of inches between the left edge of the fabric and the left edge
     of the rectangle.
@@ -24,22 +27,47 @@ defmodule Advent2018.Day3 do
     end
   end
 
+  @doc """
+  results in an id and a description of where the values start and stop.
+  """
   def read_line(str) do
-    [[_, id, x, y, width, height]] =
+    [id, x, y, width, height] =
       ~r/#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/
       |> Regex.scan(str)
+      |> List.flatten
+      |> tl
+      |> Enum.map(&String.to_integer/1)
 
     %{id: id,
-      offset: {String.to_integer(x), String.to_integer(y)},
-      size: {String.to_integer(width), String.to_integer(height)}}
+      x_range: (x+1)..(x+width),
+      y_range: (y+1)..(y+height)}
   end
 
-  def compute_coords(%{offset: {x_offset, y_offset}, size: {width, height}}) do
-    for x <- x_offset..(x_offset + width)-1, y <- y_offset..(y_offset + height)-1 do
-      {x, y}
-    end
+  def compute_coords(%{x_range: x_range, y_range: y_range}) do
+    for x <- x_range, y <- y_range, do: {x, y}
   end
 
+  @doc ~S"""
+  Current not used, doesn't work. I think it'd be a lot faster though.
+
+  FIXME: Figure out what off by one is making this fail
+  """
+  def overlap?(%{x_range: x_range, y_range: y_range}, target) do
+    x_min = Enum.min(target.x_range)
+    x_max = Enum.max(target.x_range)
+
+    y_min = Enum.min(target.y_range)
+    y_max = Enum.max(target.y_range)
+
+    (Enum.member?(x_range, x_min) || Enum.member?(x_range, x_max))
+    &&
+    (Enum.member?(y_range, y_min) || Enum.member?(y_range, y_max))
+  end
+
+  @doc """
+  If the Elves all proceed with their own plans, none of them will have enough
+  fabric. How many square inches of fabric are within two or more claims?
+  """
   def p1 do
     load_input()
     |> Enum.map(&compute_coords/1)
@@ -51,5 +79,26 @@ defmodule Advent2018.Day3 do
     |> Enum.count
   end
 
-  def p2, do: nil
+  @doc """
+  What is the ID of the only claim that doesn't overlap?
+
+  I could do this with MapSet.disjoint?/2...
+
+  Or I could try to be clever and ask if it's overlapping based on ranges.
+  """
+  def p2 do
+    claims =
+      load_input()
+      |> Enum.map(fn(claim) ->
+        Map.put(claim, :coords, MapSet.new(compute_coords(claim)))
+      end)
+
+    claims
+    |> Enum.find(fn(%{coords: coords} = claim) ->
+      Enum.all?(List.delete(claims, claim), fn(%{coords: other_coords}) ->
+        MapSet.disjoint?(coords, other_coords)
+      end)
+    end)
+    |> Map.get(:id)
+  end
 end
--- 2018/elixir/test/day_test.exs
+++ 2018/elixir/test/day_test.exs
@@ -1,17 +1,4 @@
 defmodule Advent2018Test do
   use ExUnit.Case
-  doctest Advent2018.Day6, import: true
-  doctest Advent2018.Day9
-  doctest Advent2018.Day10
-  doctest Advent2018.Day11, import: true
-  doctest Advent2018.Day13
-  doctest Advent2018.Day14, import: true
-  doctest Advent2018.Day15, import: true
-  doctest Advent2018.Day16, import: true
-  doctest Advent2018.Day18, import: true
-  doctest Advent2018.Day18.Machine, import: true
-  doctest Advent2018.Day20
-  doctest Advent2018.Day20.Particle
-  doctest Advent2018.Day23
-  doctest Advent2018.Day25
+  doctest Advent2018.Day3, import: true
 end
