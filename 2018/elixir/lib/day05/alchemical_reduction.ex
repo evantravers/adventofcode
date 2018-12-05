0c190f111464d60b82ca6ed21edebb58151ac7e6
Evan Travers <evantravers@gmail.com>
2018-12-05 14:22:57 -0600

    Setup failing tests
    TDD y'all

--- 2018/elixir/lib/day05/alchemical_reduction.ex
+++ 2018/elixir/lib/day05/alchemical_reduction.ex
@@ -1,6 +1,14 @@
 defmodule Advent2018.Day5 do
   @moduledoc "https://adventofcode.com/2018/day/5"
 
+  @doc """
+      iex> react("dabAcCaCBAcCcaDA") |> String.length
+      10
+  """
+  def react(str) do
+    str
+  end
+
   def p1, do: nil
   def p2, do: nil
 end
--- 2018/elixir/test/day_test.exs
+++ 2018/elixir/test/day_test.exs
@@ -2,4 +2,5 @@ defmodule Advent2018Test do
   use ExUnit.Case
   doctest Advent2018.Day3, import: true
   doctest Advent2018.Day4, import: true
+  doctest Advent2018.Day5, import: true
 end
