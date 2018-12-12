c6d31a649d7a0c92baaf3b6852e09e299446a618
Evan Travers <evantravers@gmail.com>
2018-12-12 16:29:43 -0600

    Solve 2018.8.2
    This took me entirely too long, I thought I had the algorithm all
    wrong... and I did. The first time I was iterating over children, which
    didn't allow for double counting if there were two indexes in data.
    
    It turned out I needed to run `Enum.reverse/1` on the
    children list since I had appended to head each time. Doh.

--- 2018/elixir/lib/day08/memory_maneuver.ex
+++ 2018/elixir/lib/day08/memory_maneuver.ex
@@ -30,17 +30,60 @@ defmodule Advent2018.Day8 do
     |> metadata(num - 1)
   end
 
-  def summarize_metadata(%{children: [], metadata: data}), do: Enum.sum(data)
-  def summarize_metadata(%{children: children, metadata: data}) do
-    Enum.sum(data) + Enum.sum(Enum.map(children, &summarize_metadata/1))
+  def add_metadata(%{children: [], metadata: data}), do: Enum.sum(data)
+  def add_metadata(%{children: children, metadata: data}) do
+    Enum.sum(data) + Enum.sum(Enum.map(children, &add_metadata/1))
+  end
+
+  @doc """
+  If a node has no child nodes, its value is the sum of its metadata entries.
+  So, the value of node B is 10+11+12=33, and the value of node D is 99.
+
+  However, if a node does have child nodes, the metadata entries become indexes
+  which refer to those child nodes. A metadata entry of 1 refers to the first
+  child node, 2 to the second, 3 to the third, and so on. The value of this
+  node is the sum of the values of the child nodes referenced by the metadata
+  entries.  If a referenced child node does not exist, that reference is
+  skipped. A child node can be referenced multiple time and counts each time it
+  is referenced. A metadata entry of 0 does not refer to any child node.
+
+  Node A:
+
+      iex> [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]
+      ...> |> build_tree()
+      ...> |> elem(1)
+      ...> |> compute_value
+      66
+
+  Node B:
+
+      iex> [0, 3, 10, 11, 12]
+      ...> |> build_tree()
+      ...> |> elem(1)
+      ...> |> compute_value
+      33
+  """
+  def compute_value(nil), do: 0
+  def compute_value(%{children: [], metadata: data}), do: Enum.sum(data)
+  def compute_value(%{children: children, metadata: data}) do
+    data
+    |> Enum.map(fn(data_index) ->
+      compute_value(Enum.at(Enum.reverse(children), data_index - 1))
+    end)
+    |> Enum.sum
   end
 
   def p1 do
     load_input()
     |> build_tree
     |> elem(1)
-    |> summarize_metadata
+    |> add_metadata
   end
 
-  def p2, do: nil
+  def p2 do
+    load_input()
+    |> build_tree
+    |> elem(1)
+    |> compute_value
+  end
 end
