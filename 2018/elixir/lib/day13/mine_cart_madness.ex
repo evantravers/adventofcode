2a9a27bd432c078a504244379ba135df2705ee36
Evan Travers <evantravers@gmail.com>
2018-12-23 14:30:21 -0600

    Moar cleanup and debug

--- 2018/elixir/lib/day13/mine_cart_madness.ex
+++ 2018/elixir/lib/day13/mine_cart_madness.ex
@@ -9,6 +9,22 @@ defmodule Advent2018.Day13 do
     end
   end
 
+  def load_input(input \\ "input.txt") do
+    input
+    |> load_file
+    |> load_track
+    |> load_cars
+  end
+
+  def load_track(list_of_lists) do
+    for {row, y} <- Enum.with_index(list_of_lists),
+        {character, x} <- Enum.with_index(row),
+        character != " ",
+        into: %{} do
+      {{x, y}, character}
+    end
+  end
+
   def load_cars(map) do
     cars =
       map
@@ -19,13 +35,18 @@ defmodule Advent2018.Day13 do
   end
 
   def initialize_car({{x, y}, character}) do
-    %{pos: {x, y}, direction: north_or_south(character), next_turn: :left}
+    %{pos: {x, y}, direction: char_to_dir(character), next_turn: :left}
   end
 
-  def north_or_south("v"), do: :south
-  def north_or_south("^"), do: :north
-  def north_or_south(">"), do: :east
-  def north_or_south("<"), do: :west
+  def char_to_dir("v"), do: :south
+  def char_to_dir("^"), do: :north
+  def char_to_dir(">"), do: :east
+  def char_to_dir("<"), do: :west
+
+  def dir_to_char(:south), do: "v"
+  def dir_to_char(:north), do: "^"
+  def dir_to_char(:east), do: ">"
+  def dir_to_char(:west), do: "<"
 
   def replace_cars_with_track({coord, "^"}), do: {coord, "|"}
   def replace_cars_with_track({coord, "v"}), do: {coord, "|"}
@@ -47,8 +68,9 @@ defmodule Advent2018.Day13 do
 
     for y <- 0..max_y do
       for x <- 0..max_x do
-        if Enum.find(cars, fn(%{pos: pos}) -> pos == {x, y} end) do
-          "O"
+        car = Enum.find(cars, fn(%{pos: pos}) -> pos == {x, y} end)
+        if car do
+          dir_to_char(car.direction)
         else
           Map.get(track, {x, y}, " ")
         end
@@ -59,24 +81,8 @@ defmodule Advent2018.Day13 do
     |> Enum.join
   end
 
-  def load_track(list_of_lists) do
-    for {row, y} <- Enum.with_index(list_of_lists),
-        {character, x} <- Enum.with_index(row),
-        character != " ",
-        into: %{} do
-      {{x, y}, character}
-    end
-  end
-
-  def load_input(input \\ "input.txt") do
-    input
-    |> load_file
-    |> load_track
-    |> load_cars
-  end
-
   def p1 do
-    {cars, track} = load_input()
+    load_input()
   end
 
   def p2, do: nil
