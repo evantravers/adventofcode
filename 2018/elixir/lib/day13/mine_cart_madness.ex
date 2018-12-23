89600c7a962c09c5765ac30c5a06a6ea971063be
Evan Travers <evantravers@gmail.com>
2018-12-23 11:00:25 -0600

    Starting on Day13
    I think I'm going to store the "map" in a map... should help handle
    edges.

--- 2018/elixir/lib/day13/mine_cart_madness.ex
+++ 2018/elixir/lib/day13/mine_cart_madness.ex
@@ -1,12 +1,27 @@
 defmodule Advent2018.Day13 do
   @moduledoc "https://adventofcode.com/2018/day/13"
 
-  def load_track(input \\ "input.txt") do
+  def load_file(input) do
     with {:ok, file} <- File.read("#{__DIR__}/#{input}") do
       file
       |> String.split("\n", trim: true)
       |> Enum.map(&String.graphemes/1)
     end
+  end
+
+  def load_track(list_of_lists) do
+    for {row, y} <- Enum.with_index(list_of_lists),
+        {character, x} <- Enum.with_index(row),
+        character != " ",
+        into: %{} do
+      {{x, y}, character}
+    end
+  end
+
+  def load_input(input \\ "input.txt") do
+    input
+    |> load_file
+    |> load_track
   end
 
   def p1, do: nil
