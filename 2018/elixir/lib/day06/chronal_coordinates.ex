aeff974d18ea07b9b43a6d9051beeefcaf3c0f17
Evan Travers <evantravers@gmail.com>
2019-01-20 09:27:10 -0600

    Solve 2018.6.1
    I'm not sure what happened, this is the same strategy I was gunning for
    the first attempt, but this one works.
    
    I think precomputing the bounds as a set of coordinates helped, I was
    also probably doing something nonperformant with lists or something

--- 2018/elixir/lib/day06/chronal_coordinates.ex
+++ 2018/elixir/lib/day06/chronal_coordinates.ex
@@ -2,17 +2,12 @@ defmodule Advent2018.Day6 do
   @moduledoc """
   https://adventofcode.com/2018/day/6
 
-  My theory:
-  - "grow" each start point until I've computed the manhattan distance for each
-    point from each point... or I guess I don't need to do that, I can just do
-    the math.
-  - for each point within bounds, reject all but the lowest distance. If it's
-    tied, reject both and it's nil
-  - purge anyone on the edges
-  - find the largest resulting space
-  """
+  I watched a little bit of jose valim's video, but now I'm going to try and
+  start from here.
 
-  @bound 500
+  P1 is find the largest bounded space... I believe that everyone who touches
+  the bounding box is infinite. (Now that sounds philosophical.)
+  """
 
   @doc ~S"""
       iex> string_to_coord("2, 3")
@@ -38,79 +33,83 @@ defmodule Advent2018.Day6 do
   end
 
   @doc """
-  Given a list of target starting coords, return a map of
-  {x, y} => %{group_id => distance}
+  This is an ugly attempt to come up with a set of coords that represent the
+  bounds of the map.
   """
-  def compute_distances(target_coords) do
-    for x <- 0..@bound, y <- 0..@bound do
-      {{x, y},
-        target_coords
-        |> Enum.with_index # FIXME: this will be deterministic, but wasteful
-        |> Enum.map(fn({coord, group_number}) ->
-          {group_number, manhattan_distance(coord, {x, y})}
-        end)
-      }
-    end
+  def bounds(list_of_coords) do
+    {x_range, y_range} = bounding_box(list_of_coords)
+
+    Enum.map(y_range, &{Enum.min(x_range), &1}) ++
+    Enum.map(y_range, &{Enum.max(x_range), &1}) ++
+    Enum.map(x_range, &{&1, Enum.min(y_range)}) ++
+    Enum.map(x_range, &{&1, Enum.max(y_range)})
+    |> Enum.into(MapSet.new)
   end
 
-  def find_closest_group(distances) do
-    possible_winners =
-      distances
-      |> Enum.sort_by(&elem(&1, 1)) # sort by distance
-      |> Enum.chunk_by(&elem(&1, 1)) # chunk by distance
-      |> List.first
+  def bounding_box(list_of_coords) do
+    {{min_x, _}, {max_x, _}} = Enum.min_max_by(list_of_coords, &elem(&1, 0))
+    {{_, min_y}, {_, max_y}} = Enum.min_max_by(list_of_coords, &elem(&1, 1))
 
-    if length(possible_winners) > 1 do
+    {min_x..max_x, min_y..max_y}
+  end
+
+  def find_group(point, coordinates) do
+    groups =
+      coordinates
+      |> Enum.map(fn(coord) ->
+        {coord, manhattan_distance(point, coord)}
+      end)
+      |> Enum.sort_by(&elem(&1, 1))
+      |> Enum.chunk_by(&elem(&1, 1))
+      |> hd
+
+    if Enum.count(groups) > 1 do
       nil
     else
-      possible_winners
+      groups
       |> hd
-      |> elem(0) # group number
+      |> elem(0)
     end
   end
 
-  def resolve_groups(world) do
-    world
-    |> Enum.map(fn({coord, distances}) ->
-      closest_group = find_closest_group(distances)
-      {coord, closest_group}
+  def grid(coordinates) do
+    {x_range, y_range} = bounding_box(coordinates)
+
+    for x <- x_range,
+        y <- y_range,
+        point = {x, y},
+        do: {point, find_group(point, coordinates)}
+  end
+
+  def grid_to_groups(list_of_grouped_coords) do
+    list_of_grouped_coords
+    |> Enum.reduce(%{}, fn({coord, group}, groups) ->
+      Map.update(groups, group, [coord], & [coord|&1])
+    end)
+    |> Enum.map(fn({key, val}) -> {key, MapSet.new(val)} end)
+    |> Enum.into(%{})
+  end
+
+  def remove_infinite(map_of_groups, coordinates) do
+    Enum.filter(map_of_groups, fn({group, coords}) ->
+      MapSet.disjoint?(bounds(coordinates), coords)
     end)
   end
 
-  @doc """
-  returns a list of group_ids who are on the bounds
-  """
-  def groups_on_bounds(world) do
-    world
-    |> Enum.filter(fn({{x, y}, _}) ->
-      x == 0 || y == 0 || x == @bound || y == @bound
-    end)
-    |> Enum.map(&elem(&1, 1))
-    |> Enum.uniq
-  end
-
-  def reject_groups_on_bounds(world) do
-    bad_groups = groups_on_bounds(world)
-
-    world
-    |> Enum.reject(fn({_, id}) ->
-      Enum.member?(bad_groups, id)
-    end)
-  end
-
-  def largest_group_remaining(world) do
-    world
-    |> Enum.chunk_by(fn({_, id}) -> id end)
-    |> Enum.max_by(&Enum.count/1)
-    |> Enum.count
+  def find_largest(list_of_groups) do
+    Enum.max_by(list_of_groups, & Enum.count(elem(&1, 1)))
   end
 
   def p1 do
-    load_input()
-    |> compute_distances
-    |> resolve_groups
-    |> reject_groups_on_bounds
-    |> largest_group_remaining
+    coordinates = load_input()
+
+    coordinates
+    |> grid
+    |> grid_to_groups
+    |> remove_infinite(coordinates)
+    |> find_largest
+    |> elem(1)
+    |> Enum.count
   end
 
   def p2 do
