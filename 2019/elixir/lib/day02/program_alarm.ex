65c9bd0c0e4238f148cf27a97ff402a03c979757
Evan Travers <evantravers@gmail.com>
2019-12-02 11:20:10 -0600

    Solve part 2
    First time using throw/catch to stop execution in elixir... that went a
    lot faster than I thought.

--- 2019/elixir/lib/day02/program_alarm.ex
+++ 2019/elixir/lib/day02/program_alarm.ex
@@ -50,6 +50,26 @@ defmodule Advent2019.Day2 do
     |> run
   end
 
+  @doc """
+  Find the input noun and verb that cause the program to produce the output
+  19690720. What is 100 * noun + verb? (For example, if noun=12 and verb=2, the
+  answer would be 1202.)
+  """
   def p2(input) do
+    try do
+      for noun <- 0..99, verb <- 0..99 do
+        result =
+          input
+          |> Map.put(1, noun)
+          |> Map.put(2, verb)
+          |> run
+
+        if result == 19690720 do
+          throw({:break, (100 * noun + verb)})
+        end
+      end
+    catch
+      {:break, result} -> result
+    end
   end
 end
