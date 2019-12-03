14ed64ba175be1664bcd70e5d433b339b1f3a2d4
Evan Travers <evantravers@gmail.com>
2019-12-03 09:26:40 -0600

    Remove tests while I try and figure out setup

--- 2019/elixir/lib/day03/crossed_wires.ex
+++ 2019/elixir/lib/day03/crossed_wires.ex
@@ -2,6 +2,7 @@ defmodule Advent2019.Day3 do
   @moduledoc "https://adventofcode.com/2019/day/3"
   @behaviour Advent
 
+  @spec setup() :: {list(), list()}
   def setup do
     with {:ok, file} <- File.read("#{__DIR__}/input.txt") do
       [wire1, wire2] =
@@ -52,12 +53,6 @@ defmodule Advent2019.Day3 do
     follow_wire([{dir, distance - 1}|tail], [next_coord(history, dir)|history])
   end
 
-  @doc ~S"""
-      iex> "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
-      iex> |> read_wires
-      iex> |> p1
-      135
-  """
   def p1({wire1, wire2}) do
     wire1
     |> MapSet.new
