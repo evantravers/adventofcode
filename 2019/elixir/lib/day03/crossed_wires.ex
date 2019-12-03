ea2bc0c65eeb969352f528e40e0fee69657a6e94
Evan Travers <evantravers@gmail.com>
2019-12-03 09:03:10 -0600

    Fix tests

--- 2019/elixir/lib/day03/crossed_wires.ex
+++ 2019/elixir/lib/day03/crossed_wires.ex
@@ -49,10 +49,10 @@ defmodule Advent2019.Day3 do
   end
 
   @doc ~S"""
-      iex> "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83"
+      iex> "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
       iex> |> read_wires
       iex> |> p1
-      159
+      135
   """
   def p1(wires) do
     [wire1, wire2] =
@@ -63,8 +63,8 @@ defmodule Advent2019.Day3 do
     |> MapSet.new
     |> MapSet.intersection(MapSet.new(wire2))
     |> MapSet.delete({0, 0})
-    |> Enum.min_by(fn({x, y}) -> {abs(x), abs(y)} end)
-    |> (fn({x, y}) -> x + y end).()
+    |> Enum.min_by(fn({x, y}) -> abs(x) + abs(y) end)
+    |> (fn({x, y}) -> abs(x) + abs(y) end).()
   end
 
   def p2(input) do
