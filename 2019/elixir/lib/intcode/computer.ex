a5f5e8ac990f3c133ac9042ce67ffbf2a151afe9
Evan Travers <evantravers@gmail.com>
2019-12-05 10:01:28 -0600

    Day2 passes!
    I had key/value swapped in `Map.put/3`. :(

--- 2019/elixir/lib/intcode/computer.ex
+++ 2019/elixir/lib/intcode/computer.ex
@@ -19,21 +19,30 @@ defmodule Intcode do
 
   - if the parameter is 50, its value is simply 50.
 
-      iex> parameter_mode(%{0 => 10, 1 => 20}, {0, 0})
+      iex> parameter_mode(%{0 => 10, 1 => 20}, {:pos, 0})
       10
 
-      iex> parameter_mode(%{0 => 10, 1 => 20}, {1, 0})
+      iex> parameter_mode(%{0 => 10, 1 => 20}, {:imm, 0})
       0
   """
-  def parameter_mode(tape, {0, target}), do: Map.get(tape, target)
-  def parameter_mode(tape, {1, value}), do: value
+  def parameter_mode(tape, {:pos, target}), do: Map.get(tape, target)
+  def parameter_mode(_, {:imm, value}), do: value
 
+  @doc """
+  Opcode 1 adds together numbers read from two positions and stores the result
+  in a third position.
+  """
   def add(tape, arg1, arg2, target) do
-    Map.put(tape, parameter_mode(tape, arg1) + parameter_mode(tape, arg2), target)
+    tape
+    |> Map.put(target, parameter_mode(tape, arg1) + parameter_mode(tape, arg2))
   end
 
+  @doc """
+  Opcode 2 works exactly like opcode 1, except it multiplies the two inputs
+  """
   def mul(tape, arg1, arg2, target) do
-    Map.put(tape, parameter_mode(tape, arg1) * parameter_mode(tape, arg2), target)
+    tape
+    |> Map.put(target, parameter_mode(tape, arg1) * parameter_mode(tape, arg2))
   end
 
   @doc """
@@ -42,6 +51,7 @@ defmodule Intcode do
   value and store it at address 50.
   """
   def inp(tape, target) do
+    Map.put(tape, target, 1) # Part 1 only takes a 1!
   end
 
   @doc """
@@ -49,18 +59,28 @@ defmodule Intcode do
   instruction 4,50 would output the value at address 50.
   """
   def out(tape, arg1) do
+    tape
+    |> Map.get(parameter_mode(tape, arg1))
+    |> IO.puts
   end
 
   def load_input(file_path) do
     with {:ok, file} <- File.read(file_path) do
-      file
-      |> String.split(~r/,|\n/, trim: true)
-      |> Enum.map(&String.to_integer/1)
-      |> Enum.with_index
-      |> Enum.map(fn({a, b}) -> {b, a} end)
-      |> Enum.into(%{})
+      string_to_tape(file)
     end
   end
+
+  def string_to_tape(string) do
+    string
+    |> String.split(~r/,|\n/, trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.with_index
+    |> Enum.map(fn({a, b}) -> {b, a} end)
+    |> Enum.into(%{})
+  end
+
+  defp convert_to_mode(0), do: :pos
+  defp convert_to_mode(1), do: :imm
 
   def run(tape, position) do
     instruction =
@@ -68,29 +88,33 @@ defmodule Intcode do
       |> Map.get(position)
       |> Integer.digits
 
-    opcode    = instruction
-                |> Enum.reverse_slice(0, 2)
-                |> Integer.undigits
+    opcode = instruction
+             |> Enum.reverse_slice(0, 2)
+             |> Integer.undigits
 
     # set parameter mode, default to 0 (position)
-    arg1_mode = Enum.at(instruction, -3, 0)
-    arg2_mode = Enum.at(instruction, -4, 0)
+    arg1_mode = instruction
+                |> Enum.at(-3, 0)
+                |> convert_to_mode
+    arg2_mode = instruction
+                |> Enum.at(-4, 0)
+                |> convert_to_mode
 
-    arg1      = Map.get(tape, position + 1)
-    arg2      = Map.get(tape, position + 2)
-    arg3      = Map.get(tape, position + 3)
+    arg1 = Map.get(tape, position + 1)
+    arg2 = Map.get(tape, position + 2)
+    arg3 = Map.get(tape, position + 3)
 
     case opcode do
-      1  ->
+      1 ->
         tape
-        |> add({arg1_mode, arg1}, {arg2_mode, arg2}, arg3)
+        |> add({:pos, arg1}, {:pos, arg2}, arg3)
         |> run(position + 4)
-      2  ->
+      2 ->
         tape
-        |> mul({arg1_mode, arg1}, {arg2_mode, arg2}, arg3)
+        |> mul({:pos, arg1}, {:pos, arg2}, arg3)
         |> run(position + 4)
-      3  -> run(inp(tape, arg1), position + 2)
-      4  -> run(out(tape, arg1), position + 2)
+      3 -> run(inp(tape, arg1), position + 2)
+      4 -> run(out(tape, {arg1_mode, arg1}), position + 2)
       99 -> Map.get(tape, 0)
       _  -> throw("Unrecognized opcode: #{Map.get(tape, position)}")
     end
