c1027121ecd9a444214082fe9a868793105bd3c7
Evan Travers <evantravers@gmail.com>
2019-12-06 21:53:27 -0600

    Solve 6.2
    There's probably a much faster way to solve this

--- 2019/elixir/lib/day06/universal_orbit_map.ex
+++ 2019/elixir/lib/day06/universal_orbit_map.ex
@@ -48,12 +48,60 @@ defmodule Advent2019.Day6 do
         graph
         |> Graph.get_shortest_path("COM", v)
         |> Enum.count
+        |> Kernel.-(1) # don't count yourself
       end
     end)
     |> Enum.sum
   end
 
+  @doc """
+  What is the total number of direct and indirect orbits in your map data?
+
+      iex> "COM)B
+      ...>B)C
+      ...>C)D
+      ...>D)E
+      ...>E)F
+      ...>B)G
+      ...>G)H
+      ...>D)I
+      ...>E)J
+      ...>J)K
+      ...>K)L
+      ...>K)YOU
+      ...>I)SAN"
+      ...> |> string_to_graph
+      ...> |> p2
+      4
+  """
   def p2(graph) do
-    nil
+    santas_orbital_path =
+      graph
+      |> Graph.get_shortest_path("COM", "SAN")
+      |> Enum.reverse
+
+    your_orbital_path =
+      graph
+      |> Graph.get_shortest_path("COM", "YOU")
+      |> Enum.reverse
+
+    shared_root =
+      Enum.find(santas_orbital_path, fn(node) ->
+        Enum.any?(your_orbital_path, & node == &1)
+      end)
+
+    santa_jumps =
+      graph
+      |> Graph.get_shortest_path(shared_root, "SAN")
+      |> Enum.count
+      |> Kernel.-(2)
+
+    you_jumps =
+      graph
+      |> Graph.get_shortest_path(shared_root, "YOU")
+      |> Enum.count
+      |> Kernel.-(2)
+
+    santa_jumps + you_jumps
   end
 end
