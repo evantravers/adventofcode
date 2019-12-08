5b85d6491b603f9b6045538cbfa2098591e5e65e
Evan Travers <evantravers@gmail.com>
2019-12-08 14:40:27 -0600

    Solve 8.2
    Simple and easy... I really liked my solution for today. I'm sure there
    are small optimizations to be made, but it's clean and simple.

--- 2019/elixir/lib/day08/space_image_format.ex
+++ 2019/elixir/lib/day08/space_image_format.ex
@@ -2,6 +2,9 @@ defmodule Advent2019.Day8 do
   @moduledoc "https://adventofcode.com/2019/day/8"
   @behaviour Advent
 
+  @width  25
+  @height 6
+
   def setup do
     with {:ok, file} <- File.read("#{__DIR__}/input.txt") do
       file
@@ -14,7 +17,7 @@ defmodule Advent2019.Day8 do
   def p1(list_of_integers) do
     least_zeroes =
       list_of_integers
-      |> Enum.chunk_every(25*6)
+      |> Enum.chunk_every(@width*@height)
       |> Enum.min_by(fn(layer) ->
         layer
         |> Enum.filter(& &1 == 0)
@@ -33,6 +36,22 @@ defmodule Advent2019.Day8 do
     ones * twos
   end
 
-  def p2(i) do
+  def pixel(0), do: " "
+  def pixel(1), do: "█"
+
+  def p2(list_of_integers) do
+    list_of_integers
+    |> Enum.chunk_every(@width*@height)
+    |> Enum.zip
+    |> Enum.map(fn(pixel_stack) ->
+      pixel_stack
+      |> Tuple.to_list
+      |> Enum.reject(fn(pixel) -> pixel == 2 end)
+      |> hd
+      |> pixel
+    end)
+    |> Enum.chunk_every(@width)
+    |> Enum.map(&Enum.join(&1))
+    |> Enum.reduce("", fn(string, acc) -> "#{acc}\n#{string}" end)
   end
 end
