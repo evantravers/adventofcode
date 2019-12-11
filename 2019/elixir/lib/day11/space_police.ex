08a0d65c149a4fbda2fc170980ca8f0893ac6f51
Evan Travers <evantravers@gmail.com>
2019-12-11 09:35:34 -0600

    Stub out Day11
    I'm not sure I should proceed on this till I finish the previous Intcode
    puzzles.

---
+++ 2019/elixir/lib/day11/space_police.ex
@@ -0,0 +1,15 @@
+defmodule Advent2019.Day11 do
+  @moduledoc "https://adventofcode.com/2019/day/11"
+  @behaviour Advent
+
+  def setup do
+    Intcode.load_file("#{__DIR__}/input.txt")
+  end
+
+  def p1(machine) do
+  end
+
+  def p2(machine) do
+  end
+end
+
