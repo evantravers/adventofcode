6922de37f8c45415400f595b6e92ce6c323c79c2
Evan Travers <evantravers@gmail.com>
2019-12-10 20:34:20 -0600

    Continuing to think through day 10

--- 2019/elixir/lib/day10/monitoring_station.ex
+++ 2019/elixir/lib/day10/monitoring_station.ex
@@ -43,8 +43,37 @@ defmodule Advent2019.Day10 do
   def find_station(map) do
   end
 
-  def p1(i) do
-    i
+  @doc """
+  An asteroid is blocked by another asteroid if there's another asteroid
+  directly in between them.
+
+  The classic is on the same x/y access.
+
+      iex> [{0,0}, {0, 2}, {0, 4}]
+      ...> |> MapSet.new
+      ...> |> blocked({0, 0}, {0, 4})
+      true
+
+  I should note, this is a two-way relationship... probably could speed up the
+  search by memoizing "searched" positions in a two way fashion.
+  """
+  def blocked(map, {x1, y1} = a1, {x2, y2} = a2) do
+    x_distance = x1 - x2
+    y_distance = y1 - y2
+
+    map
+    |> MapSet.delete(a1)
+    |> MapSet.delete(a2)
+    |> Enum.any?(fn({x3, y3}) ->
+      # FIXME There must be a math function... I think it's something to do
+      # with the x1-x2 / y1-y2 ratio.
+    end)
+  end
+
+  def p1(asteroid_map) do
+    asteroid_map
+    |> find_station
+    |> elem(1)
   end
 
   def p2(i) do
