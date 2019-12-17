8d7843b00c297de2f9403ca66f0b1e85b284bb9e
Evan Travers <evantravers@gmail.com>
2019-12-17 09:27:35 -0600

    input is now FILO instead of FIFO

--- 2019/elixir/lib/day07/amplification_circuit.ex
+++ 2019/elixir/lib/day07/amplification_circuit.ex
@@ -25,7 +25,7 @@ defmodule Advent2019.Day7 do
   end
 
   def put_input(computer = %{input: input}, new_input) do
-    %{computer | input: [new_input|input]}
+    %{computer | input: List.insert_at(input, -1, new_input)}
   end
   def get_output(%{output: [o|_]}), do: o
 
@@ -41,8 +41,8 @@ defmodule Advent2019.Day7 do
     |> Enum.reduce(0, fn({amp, phase_setting}, previous_output) ->
 
       amp
+      |> put_input(phase_setting) # FILO
       |> put_input(previous_output)
-      |> put_input(phase_setting) # FIFO
       |> Intcode.run
       |> get_output
     end)
