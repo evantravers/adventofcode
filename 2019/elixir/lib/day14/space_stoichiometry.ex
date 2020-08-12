73ea28182fc2ae7586038d3e422b586c60821d5a
Evan Travers <evantravers@gmail.com>
2020-08-11 22:28:42 -0500

    Continue to figure all this out...
    I'm going to have to shift from this to a graph... I kind of want to
    build the graph as I go, but I know it's a _little_ wasteful to rewrite
    this when it works.

--- 2019/elixir/lib/day14/space_stoichiometry.ex
+++ 2019/elixir/lib/day14/space_stoichiometry.ex
@@ -3,17 +3,21 @@ defmodule Advent2019.Day14 do
   https://adventofcode.com/2019/day/14
 
   I'm still reading the instructions, but I'm kind of feeling that this a
-  weighted graph problem?
+  weighted graph problem? Yes, it's a dependency graph.
 
   It's basically a factorio problem, hah!
   """
 
   def setup do
     with {:ok, string} <- File.read("#{__DIR__}/input.txt") do
-      string
-      |> String.split("\n", trim: true)
-      |> Enum.map(&string_to_reaction/1)
+      setup_map(string)
     end
+  end
+
+  def setup_map(string) do
+    string
+    |> String.split("\n", trim: true)
+    |> Enum.map(&string_to_reaction/1)
   end
 
   def string_to_reaction(str) do
@@ -29,11 +33,23 @@ defmodule Advent2019.Day14 do
       s
       |> String.split(" ", trim: true)
       |> (fn([amount, ingredient]) ->
-        [String.to_integer(amount), ingredient]
+        [String.to_integer(amount), String.to_atom(ingredient)]
       end).()
     end)
   end
 
+  @doc """
+      iex> "9 ORE => 2 A
+      ...> 8 ORE => 3 B
+      ...> 7 ORE => 5 C
+      ...> 3 A, 4 B => 1 AB
+      ...> 5 B, 7 C => 1 BC
+      ...> 4 C, 1 A => 1 CA
+      ...> 2 AB, 3 BC, 4 CA => 1 FUEL"
+      ...> |> setup_map
+      ...> |> p1
+      165
+  """
   def p1(input) do
     input
   end
