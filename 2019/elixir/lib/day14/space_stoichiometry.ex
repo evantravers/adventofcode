8661a17274c5174cef7b79cbc6141e536c5bca2f
Evan Travers <evantravers@gmail.com>
2020-08-12 22:37:07 -0500

    Start writing the reducer

--- 2019/elixir/lib/day14/space_stoichiometry.ex
+++ 2019/elixir/lib/day14/space_stoichiometry.ex
@@ -61,6 +61,25 @@ defmodule Advent2019.Day14 do
   end
 
   @doc """
+  Compute cost of a system from FUEL -> ORE.
+  """
+  def compute_cost(graph, node, score \\ %{total: 1, mul: 1}) do
+    reaction = Map.get(graph, node)
+
+    next_paths =
+      reaction
+      |> Map.get(:ingredients)
+      |> Enum.map(&elem(&, 1))
+
+    total
+  end
+
+  @doc """
+  Compute ORE cost to make 1 FUEL.
+
+  Starting from FUEL, recursively search backwards to determine the total cost
+  in ORE.
+
       iex> "9 ORE => 2 A
       ...> 8 ORE => 3 B
       ...> 7 ORE => 5 C
@@ -72,8 +91,9 @@ defmodule Advent2019.Day14 do
       ...> |> p1
       165
   """
-  def p1(input) do
-    input
+  def p1(graph) do
+    compute_cost(graph, :FUEL)
   end
-  def p2(input), do: nil
+
+  def p2(_input), do: nil
 end
