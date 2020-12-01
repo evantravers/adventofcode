4ad11e175fdbbfa71f41fb5649faab172fb2f5eb
Evan Travers <evantravers@gmail.com>
2020-12-01 10:30:04 -0600

    2020.1.2
    Same solution... higher run time.
    
    This is way too slow.

--- 2020/elixir/lib/day01/expense_report.ex
+++ 2020/elixir/lib/day01/expense_report.ex
@@ -25,5 +25,21 @@ defmodule Advent2020.Day1 do
 
     x * y
   end
-  def p2(_input), do: nil
+
+  @doc """
+      iex> p2([1721, 979, 366, 299, 675, 1456])
+      241861950
+  """
+  def p2(list_of_ints) do
+    [x, y, z] =
+      for int1 <- list_of_ints,
+          int2 <- Enum.reject(list_of_ints, & &1 == int1),
+          int3 <- Enum.reject(list_of_ints, & &1 == int1 || &1 == int2) do
+        Enum.sort([int1, int2, int3])
+      end
+      |> Enum.uniq # remove duplicates the cheapo way
+      |> Enum.find(fn([int1, int2, int3]) -> int1 + int2 + int3 == 2020 end)
+
+    x * y * z
+  end
 end
