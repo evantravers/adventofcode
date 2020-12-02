9ba8d7bae6d5d9c07f54bf74c509dec7172f1815
Evan Travers <evantravers@gmail.com>
2020-12-02 10:52:23 -0600

    2020.2.2
    Quick refactor.
    
    Not terribly lovely.

--- 2020/elixir/lib/day02/password_philosophy.ex
+++ 2020/elixir/lib/day02/password_philosophy.ex
@@ -9,19 +9,15 @@ defmodule Advent2020.Day2 do
     end
   end
 
-  def parse_line(line) do
-    [[_str, low, high, char, password]] =
+  def parse_line(line, func) do
+    [[_str, first, second, char, password]] =
       Regex.scan(~r/(\d+)-(\d+) (\w): (\w+)/, line)
 
-    low = String.to_integer(low)
-    high = String.to_integer(high)
+    password = String.graphemes(password)
+    first    = String.to_integer(first)
+    second   = String.to_integer(second)
 
-    count =
-      password
-      |> String.graphemes
-      |> Enum.count(& &1 == char)
-
-    count >= low && count <= high
+    func.(first, second, char, password)
   end
 
   @doc """
@@ -29,6 +25,31 @@ defmodule Advent2020.Day2 do
       ...> |> p1
       2
   """
-  def p1(input), do: Enum.count(input, &parse_line/1)
-  def p2(_input), do: nil
+  def p1(input) do
+    Enum.count(input, fn(line) ->
+      parse_line(line, fn(first, second, char, password) ->
+        count = Enum.count(password, & &1 == char)
+
+        count >= first && count <= second
+      end)
+    end)
+  end
+
+  @doc """
+      iex> ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
+      ...> |> p2
+      1
+  """
+  def p2(input) do
+    Enum.count(input, fn(line) ->
+      parse_line(line, fn(first, second, char, password) ->
+        [
+          Enum.at(password, first-1) == char,
+          Enum.at(password, second-1) == char
+        ]
+        |> Enum.count(& &1)
+        |> Kernel.==(1)
+      end)
+    end)
+  end
 end
