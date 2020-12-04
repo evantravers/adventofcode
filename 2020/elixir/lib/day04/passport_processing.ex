12446ccca16d9a33d39ccf2af310d52b8ec292ba
Evan Travers <evantravers@gmail.com>
2020-12-04 09:36:21 -0600

    Setup

---
+++ 2020/elixir/lib/day04/passport_processing.ex
@@ -0,0 +1,12 @@
+defmodule Advent2020.Day4 do
+  @moduledoc "https://adventofcode.com/2020/day/4"
+
+  def setup do
+    with {:ok, file} <- File.read(__DIR__ + '/input.txt/') do
+      file
+    end
+  end
+
+  def p1(_i), do: nil
+  def p2(_i), do: nil
+end
