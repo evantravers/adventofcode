57b73f0d6d17635974c7e4c082db5b7d5ef303d6
Evan Travers <evantravers@gmail.com>
2020-12-06 15:30:03 -0600

    2020.6.1
    Quick and dirty win

--- 2020/elixir/lib/day06/custom_customs.ex
+++ 2020/elixir/lib/day06/custom_customs.ex
@@ -2,8 +2,29 @@ defmodule Advent2020.Day6 do
   @moduledoc "https://adventofcode.com/2020/day/6"
 
   def setup do
+    with {:ok, file} <- File.read("#{__DIR__}/input.txt") do
+      file
+      |> String.split("\n\n", trim: true)
+      |> Enum.map(&String.split(&1, "\n", trim: true))
+    end
   end
 
-  def p1(_i), do: nil
+  def count_freq(group) do
+    group
+    |> Enum.reduce(MapSet.new, fn(str, set) ->
+      str
+      |> String.graphemes
+      |> MapSet.new
+      |> MapSet.union(set)
+    end)
+    |> Enum.count
+  end
+
+  def p1(groups) do
+    groups
+    |> Enum.map(&count_freq/1)
+    |> Enum.sum
+  end
+
   def p2(_i), do: nil
 end
