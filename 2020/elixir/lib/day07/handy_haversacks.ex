48c9e2a226411fd782087526d690b136af36722a
Evan Travers <evantravers@gmail.com>
2020-12-07 17:02:24 -0600

    Passes tests, doesn't fix the problem
    I lost a lot of time because I forgot to update the regex with the final
    `?`

--- 2020/elixir/lib/day07/handy_haversacks.ex
+++ 2020/elixir/lib/day07/handy_haversacks.ex
@@ -2,42 +2,68 @@ defmodule Advent2020.Day7 do
   @moduledoc "https://adventofcode.com/2020/day/7"
 
   def setup do
-    with {:ok, file} <- File.read("#{__DIR__}/input.txt") do
-      file
-      |> String.split("\n", trim: true)
-      |> Enum.reduce(Graph.new, &parse_sentence/2)
-    end
+    with {:ok, file} <- File.read("#{__DIR__}/input.txt"), do: setup_str(file)
+  end
+
+  def setup_str(str) do
+    str
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(Graph.new, &parse_sentence/2)
   end
 
   @doc """
       iex> parse_bag("2 shiny purple bags")
-      %{"number" => 2, "adjective" => "shiny", "color" => "purple"}
+      %{"adjective" => "shiny", "color" => "purple"}
   """
   def parse_bag(str) do
-    ~r/(\d+ )?(?<adjective>\w+) (?<color>\w+) bags/
+    ~r/(\d+ )?(?<adjective>\w+) (?<color>\w+) bags?/
     |> Regex.named_captures(str)
   end
 
   def parse_sentence(sentence, graph) do
     [parent, children_str] = String.split(sentence, " contain ")
 
-    children =
-      children_str
-      |> String.replace(".", "")
-      |> String.split(", ", trim: true)
-      |> Enum.map(&parse_bag/1)
+    if String.match?(children_str, ~r/no other/) do
+      graph
+    else
+      children =
+        children_str
+        |> String.replace(".", "")
+        |> String.split(", ", trim: true)
+        |> Enum.map(&parse_bag/1)
+        |> Enum.reject(&is_nil/1)
 
-    graph
-    |> Graph.add_edges(
-      for child <- children, do: {parse_bag(parent), child}
-    )
+      graph
+      |> Graph.add_edges(
+        for child <- children, do: {child, parse_bag(parent)}
+      )
+    end
   end
 
+  @doc """
+      iex> "light red bags contain 1 bright white bag, 2 muted yellow bags.
+      ...>dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+      ...>bright white bags contain 1 shiny gold bag.
+      ...>muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+      ...>shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+      ...>dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+      ...>vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+      ...>faded blue bags contain no other bags.
+      ...>dotted black bags contain no other bags."
+      ...> |> setup_str
+      ...> |> p1
+      4
+  """
   def p1(inventory) do
+    target = %{"adjective" => "shiny", "color" => "gold"}
+
     inventory
-    require IEx; IEx.pry
-    # |> Enum.find(fn({parent, children}) ->
-    # end)
+    |> Graph.reachable([target])
+    |> List.delete(target)
+    |> IO.inspect
+    |> Enum.map(&Map.get(&1, "color"))
+    |> Enum.uniq
+    |> Enum.count
   end
 
   def p2(_i), do: nil
