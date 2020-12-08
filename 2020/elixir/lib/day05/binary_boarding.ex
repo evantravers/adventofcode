076939b2ddb4a8cad8c54b59d235d832ae009704
Evan Travers <evantravers@gmail.com>
2020-12-08 16:24:59 -0600

    mix format

--- 2020/elixir/lib/day05/binary_boarding.ex
+++ 2020/elixir/lib/day05/binary_boarding.ex
@@ -1,7 +1,9 @@
 defmodule Advent2020.Day5 do
   @moduledoc "https://adventofcode.com/2020/day/5"
-  @rows 127 # + 1 = 128
-  @columns 7 # + 1 = 8
+  # + 1 = 128
+  @rows 127
+  # + 1 = 8
+  @columns 7
 
   def setup do
     with {:ok, file} <- File.read("#{__DIR__}/input.txt") do
@@ -30,22 +32,23 @@ defmodule Advent2020.Day5 do
   """
   def parse_seat(str) do
     str
-    |> String.graphemes
+    |> String.graphemes()
     |> Enum.reduce(%{row: {0, @rows}, column: {0, @columns}}, &eval/2)
     |> Map.update!(:row, &elem(&1, 0))
     |> Map.update!(:column, &elem(&1, 0))
   end
 
-  def midpoint(low, high), do: high - Integer.floor_div((high - low), 2)
+  def midpoint(low, high), do: high - Integer.floor_div(high - low, 2)
 
   def binary(map, dimension, :high) do
-    Map.update!(map, dimension, fn({low, high}) ->
+    Map.update!(map, dimension, fn {low, high} ->
       {midpoint(low, high), high}
     end)
   end
+
   def binary(map, dimension, :low) do
-    Map.update!(map, dimension, fn({low, high}) ->
-      {low, midpoint(low, high)-1}
+    Map.update!(map, dimension, fn {low, high} ->
+      {low, midpoint(low, high) - 1}
     end)
   end
 
@@ -59,17 +62,18 @@ defmodule Advent2020.Day5 do
   def p1(boarding_passes) do
     boarding_passes
     |> Enum.map(&seat_id/1)
-    |> Enum.max
+    |> Enum.max()
   end
+
   def p2(boarding_passes) do
     for x <- 0..@rows,
         y <- 0..@columns,
         !Enum.member?(boarding_passes, %{row: x, column: y}) do
       %{row: x, column: y}
     end
-    |> Enum.find(fn(%{row: row, column: column}) ->
-      Enum.member?(boarding_passes, %{row: row+1, column: column}) &&
-      Enum.member?(boarding_passes, %{row: row-1, column: column})
+    |> Enum.find(fn %{row: row, column: column} ->
+      Enum.member?(boarding_passes, %{row: row + 1, column: column}) &&
+        Enum.member?(boarding_passes, %{row: row - 1, column: column})
     end)
     |> seat_id
   end
