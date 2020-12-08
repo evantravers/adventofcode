4fcdac53c18b9d94f93e4a34f2e699b7168477b9
Evan Travers <evantravers@gmail.com>
2020-12-08 12:45:34 -0600

    mix format

--- 2020/elixir/lib/day06/custom_customs.ex
+++ 2020/elixir/lib/day06/custom_customs.ex
@@ -11,37 +11,37 @@ defmodule Advent2020.Day6 do
 
   def count_any(group) do
     group
-    |> Enum.reduce(MapSet.new, fn(str, set) ->
+    |> Enum.reduce(MapSet.new(), fn str, set ->
       str
-      |> String.graphemes
-      |> MapSet.new
+      |> String.graphemes()
+      |> MapSet.new()
       |> MapSet.union(set)
     end)
-    |> Enum.count
+    |> Enum.count()
   end
 
   def count_all(group) do
     group
-    |> Enum.reduce(%{}, fn(str, map) ->
+    |> Enum.reduce(%{}, fn str, map ->
       str
-      |> String.graphemes
-      |> Enum.reduce(map, fn(char, m) ->
-        Map.update(m, char, 1, & &1 + 1)
+      |> String.graphemes()
+      |> Enum.reduce(map, fn char, m ->
+        Map.update(m, char, 1, &(&1 + 1))
       end)
     end)
-    |> Enum.filter(fn({_k, v}) -> v == Enum.count(group) end)
-    |> Enum.count
+    |> Enum.filter(fn {_k, v} -> v == Enum.count(group) end)
+    |> Enum.count()
   end
 
   def p1(groups) do
     groups
     |> Enum.map(&count_any/1)
-    |> Enum.sum
+    |> Enum.sum()
   end
 
   def p2(groups) do
     groups
     |> Enum.map(&count_all/1)
-    |> Enum.sum
+    |> Enum.sum()
   end
 end
