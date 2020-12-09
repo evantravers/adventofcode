392ccdc32f646b47928a58870c8fffefac56e98f
Evan Travers <evantravers@gmail.com>
2020-12-09 16:23:08 -0600

    Completes, but wrong answers only
    I think I'm cons-ing something that I shouldn't be.

--- 2020/elixir/lib/day09/encoding_error.ex
+++ 2020/elixir/lib/day09/encoding_error.ex
@@ -21,11 +21,30 @@ defmodule Advent2020.Day9 do
 
   def invalid?(list), do: !valid?(list)
 
+  def find_contiguous(integers, target, visited \\ [])
+  def find_contiguous([consider|remaining] = entire, target, visited) do
+    cond do
+      Enum.sum(visited) + consider == target -> entire # win
+      Enum.sum(visited) + consider > target -> # try again with remaining.
+        find_contiguous(tl(Enum.reverse(visited) ++ entire), target)
+      Enum.sum(visited) + consider < target ->
+        find_contiguous(remaining, target, [consider|visited]) # hit the dealer
+    end
+  end
+
   def p1(integers) do
     integers
     |> Enum.chunk_every(26, 1)
     |> Enum.find(&invalid?/1)
     |> List.last
   end
-  def p2(_i), do: nil
+
+  def p2(integers) do
+    target = 258585477
+
+    contiguous = find_contiguous(integers, target)
+                 |> IO.inspect
+
+    hd(contiguous) + List.last(contiguous)
+  end
 end
