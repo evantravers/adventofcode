baf92c17e3c9fb5cd79e40a0d349940b8acd393d
Evan Travers <evantravers@gmail.com>
2020-12-09 13:00:19 -0600

    2020.9.1
    That... was a lot faster than I thought. I also got it on my first try.
    woot!

--- 2020/elixir/lib/day09/encoding_error.ex
+++ 2020/elixir/lib/day09/encoding_error.ex
@@ -2,8 +2,31 @@ defmodule Advent2020.Day9 do
   @moduledoc "https://adventofcode.com/2020/day/9"
 
   def setup do
+    with {:ok, file} <- File.read("#{__DIR__}/input.txt") do
+      file
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+    end
   end
 
-  def p1(_i), do: nil
+  def valid?(list) when length(list) < 25, do: true
+  def valid?(twentysix) do
+    [sum|twentyfive] = Enum.reverse(twentysix)
+
+    Enum.any?(twentyfive, fn(int1) ->
+      Enum.any?(List.delete(twentyfive, int1), fn(int2) ->
+        sum == int1 + int2
+      end)
+    end)
+  end
+
+  def invalid?(list), do: !valid?(list)
+
+  def p1(integers) do
+    integers
+    |> Enum.chunk_every(26, 1)
+    |> Enum.find(&invalid?/1)
+    |> List.last
+  end
   def p2(_i), do: nil
 end
