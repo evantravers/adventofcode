88ebed911aa61969c162f4fdcd7227bc441597e6
Evan Travers <evantravers@gmail.com>
2020-12-10 11:40:06 -0600

    Revamp using... trees!
    Always the path forward. Just need to add the device and the charger.

--- 2020/elixir/lib/day10/adapter_array.ex
+++ 2020/elixir/lib/day10/adapter_array.ex
@@ -5,13 +5,22 @@ defmodule Advent2020.Day10 do
     with {:ok, file} <- File.read("#{__DIR__}/input.txt") do
       file
       |> String.split("\n", trim: true)
-      |> Enum.map(&parse_adapter/1)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sort
+      |> Enum.reduce(Graph.new, &parse_adapter/2)
     end
   end
 
-  def parse_adapter(str) do
-    num = String.to_integer(str)
-    %{output: num, input: (num - 3)..num}
+  def possible_adapters(graph, joltage) do
+    for jolt <- (joltage - 3)..joltage, Graph.has_vertex?(graph, jolt) do
+      {joltage, jolt}
+    end
+  end
+
+  def parse_adapter(joltage, graph) do
+    graph
+    |> Graph.add_vertex(joltage)
+    |> Graph.add_edges(possible_adapters(graph, joltage))
   end
 
   def p1(adapters) do
