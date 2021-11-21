ca3bd68bec6594405658aece09cdecd45f9d5fb9
Evan Travers <evantravers@gmail.com>
2021-11-20 19:47:53 -0600

    Quick refactor for 2020.13.2
    I'm going to need to know the index

--- 2020/elixir/lib/day13/shuttle_search.ex
+++ 2020/elixir/lib/day13/shuttle_search.ex
@@ -3,34 +3,40 @@ defmodule Advent2020.Day13 do
 
   def setup do
     with {:ok, file} <- File.read("#{__DIR__}/input") do
-      [stamp, buses] = String.split(file, "\n", trim: true)
-
-      %{
-        timestamp: String.to_integer(stamp),
-        buses: buses
-               |> String.split(",")
-               |> Enum.map(&process_departure/1)
-               |> Enum.reject(& is_nil(&1))
-      }
+      setup_string(file)
     end
   end
 
-  def process_departure("x"), do: nil
-  def process_departure(bus), do: String.to_integer(bus)
+  def setup_string(str) do
+    [stamp, buses] = String.split(str, "\n", trim: true)
+
+    %{
+      timestamp: String.to_integer(stamp),
+      buses: buses
+             |> String.split(",", trim: true)
+             |> Enum.with_index
+    }
+  end
 
   @doc """
-      iex> %{timestamp: 939, buses: [7,13,59,31,19]}
+      iex> "939
+      ...>7,13,x,x,59,x,31,19"
+      ...> |> setup_string
       ...> |> p1
       295
   """
   def p1(%{timestamp: timestamp, buses: buses}) do
     winner =
       buses
-      |> Enum.map(fn(bus) ->
-        min_rotations = ceil(timestamp / bus)
+      |> Enum.map(fn
+        {"x", _index} -> nil
+        {bus, _index} ->
+          bus = String.to_integer(bus)
+          min_rotations = ceil(timestamp / bus)
 
-        %{bus: bus, first: bus * min_rotations}
+          %{bus: bus, first: bus * min_rotations}
       end)
+      |> Enum.reject(&is_nil/1)
       |> Enum.min_by(fn(%{first: first}) ->
         first - timestamp
       end)
