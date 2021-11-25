798855cb5be9249ecb7554e08fa46425485109ee
Evan Travers <evantravers@gmail.com>
2021-11-24 22:45:43 -0600

    Quick solution for 2020.16.1
    As usual, didn't read the instructions correctly the first time.

--- 2020/elixir/lib/day16/ticket_translation.ex
+++ 2020/elixir/lib/day16/ticket_translation.ex
@@ -9,7 +9,6 @@ defmodule Advent2020.Day16 do
       %{fields: %{}, nearby: []}
       |> eval_fields(fields)
       |> eval_tickets(nearby_tickets)
-      |> IO.inspect
     end
   end
 
@@ -46,6 +45,18 @@ defmodule Advent2020.Day16 do
     end)
   end
 
-  def p1(_input), do: nil
+  def p1(%{nearby: nearby, fields: fields}) do
+    rules = Map.values(fields) |> List.flatten
+
+    nearby
+    |> List.flatten
+    |> Enum.filter(fn(number) ->
+      !Enum.any?(rules, fn(rule) ->
+        Enum.member?(rule, number)
+      end)
+    end)
+    |> Enum.sum
+  end
+
   def p2(_input), do: nil
 end
