47e0198a666d808a281fb2a8e94bd04222de0006
Evan Travers <evantravers@gmail.com>
2021-11-29 13:43:14 -0600

    Tests

--- 2020/elixir/lib/day16/ticket_translation.ex
+++ 2020/elixir/lib/day16/ticket_translation.ex
@@ -73,6 +73,23 @@ defmodule Advent2020.Day16 do
     Enum.member?(r1, num) || Enum.member?(r2, num)
   end
 
+  @doc """
+      iex> "class: 1-3 or 5-7
+      ...>row: 6-11 or 33-44
+      ...>seat: 13-40 or 45-50
+      ...>
+      ...>your ticket:
+      ...>7,1,14
+      ...>
+      ...>nearby tickets:
+      ...>7,3,47
+      ...>40,4,50
+      ...>55,2,20
+      ...>38,6,12"
+      ...> |> setup_string
+      ...> |> p1
+      71
+  """
   def p1(%{nearby: nearby} = state) do
     nearby
     |> Enum.filter(&invalid_ticket?(&1, state))
