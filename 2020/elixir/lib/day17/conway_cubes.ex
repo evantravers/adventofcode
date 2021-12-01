6b3bb2e66dcaf950d5064dea2307418fdefa9fef
Evan Travers <evantravers@gmail.com>
2021-12-01 07:33:31 -0600

    2020.17.2
    I'm sure there's a way to elegantly dry this up, but working first,
    pretty second.

--- 2020/elixir/lib/day17/conway_cubes.ex
+++ 2020/elixir/lib/day17/conway_cubes.ex
@@ -3,10 +3,10 @@ defmodule Advent2020.Day17 do
   @behaviour Advent
 
   def setup do
-    with {:ok, file} <- File.read("#{__DIR__}/input"), do: setup_string(file)
+    with {:ok, file} <- File.read("#{__DIR__}/input"), do: file
   end
 
-  def setup_string(str) do
+  def setup_world(str, dimensions \\ 3) do
     str
     |> String.split("\n", trim: true)
     |> Enum.with_index
@@ -15,7 +15,11 @@ defmodule Advent2020.Day17 do
       |> String.split("", trim: true)
       |> Enum.with_index
       |> Enum.reduce(state, fn({char, x}, state) ->
-        Map.put(state, {x, y, 0}, eval_char(char))
+        if dimensions == 4 do
+          Map.put(state, {x, y, 0, 0}, eval_char(char))
+        else
+          Map.put(state, {x, y, 0}, eval_char(char))
+        end
       end)
     end)
   end
@@ -33,12 +37,10 @@ defmodule Advent2020.Day17 do
     {min - 1, max + 1}
   end
 
-  def bounding_box(state) do
-    {
-      min_max(state, 0),
-      min_max(state, 1),
-      min_max(state, 2)
-    }
+  def bounding_box(state, dimensions \\ 3) do
+    0..dimensions-1
+    |> Enum.map(&min_max(state, &1))
+    |> List.to_tuple
   end
 
   def neighbors({x, y, z}) do
@@ -46,6 +48,13 @@ defmodule Advent2020.Day17 do
       {x, y, z}
     end
     |> List.delete({x, y, z})
+  end
+
+  def neighbors({x, y, z, w}) do
+    for x <- x-1..x+1, y <- y-1..y+1, z <- z-1..z+1, w <- w-1..w+1 do
+      {x, y, z, w}
+    end
+    |> List.delete({x, y, z, w})
   end
 
   def neighbors_count(coord, state) do
@@ -123,18 +132,72 @@ defmodule Advent2020.Day17 do
     |> play(count - 1)
   end
 
+  def play_4d(state, count \\ 6)
+  def play_4d(state, 0), do: state
+  def play_4d(state, count) do
+    {
+      {min_x, max_x},
+      {min_y, max_y},
+      {min_z, max_z},
+      {min_w, max_w}
+    } = bounding_box(state, 4)
+
+    # print(state)
+
+    for x <- min_x..max_x, y <- min_y..max_y, z <- min_z..max_z, w <- min_w..max_w, into: %{} do
+      coord  = {x, y, z, w}
+      active = Map.get(state, coord)
+      count  = neighbors_count(coord, state)
+
+      if active do
+        # If a cube is active and exactly 2 or 3 of its neighbors are also
+        # active, the cube remains active. Otherwise, the cube becomes
+        # inactive.
+
+        if count == 2 || count == 3 do
+          {coord, true}
+        else
+          {coord, false}
+        end
+      else
+        # If a cube is inactive but exactly 3 of its neighbors are active, the
+        # cube becomes active. Otherwise, the cube remains inactive.
+
+        if count == 3 do
+          {coord, true}
+        else
+          {coord, false}
+        end
+      end
+    end
+    |> play_4d(count - 1)
+  end
+
   @doc """
-      iex> setup_string(".#.
+      iex> ".#.
       ...>..#
-      ...>###")
+      ...>###"
       ...> |> p1
       112
   """
   def p1(state) do
     state
+    |> setup_world(3)
     |> play
     |> Enum.count(&elem(&1, 1))
   end
 
-  def p2(_state), do: nil
+  @doc """
+      iex> ".#.
+      ...>..#
+      ...>###"
+      ...> |> p2
+      848
+  """
+  def p2(state) do
+    state
+    |> setup_world(4)
+    |> play_4d
+    |> Enum.count(&elem(&1, 1))
+  end
 end
