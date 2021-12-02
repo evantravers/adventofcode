5576214ffd816c271d1b38ac2d97bc835adca01e
Evan Travers <evantravers@gmail.com>
2021-12-02 08:57:11 -0600

    2020.2.2
    Pretty straightforward. (see what I did there!)
    
    My first attempt, I forgot to remove `:aim` from the values before
    computing the product.

--- 2021/elixir/lib/day02/dive.ex
+++ 2021/elixir/lib/day02/dive.ex
@@ -9,16 +9,28 @@ defmodule Advent2021.Day2 do
     end
   end
 
+  def i(n), do: String.to_integer(n)
+
   def p1(input) do
     input
     |> Enum.reduce(%{x: 0, y: 0}, fn
-      "forward " <> num, %{x: x} = sub -> %{sub | x: x + String.to_integer(num)}
-      "down " <> num, %{y: y} = sub -> %{sub | y: y + String.to_integer(num)}
-      "up " <> num, %{y: y} = sub -> %{sub | y: y - String.to_integer(num)}
+      "forward " <> num, %{x: x} = sub -> %{sub | x: x + i(num)}
+      "down " <> num, %{y: y} = sub -> %{sub | y: y + i(num)}
+      "up " <> num, %{y: y} = sub -> %{sub | y: y - i(num)}
     end)
     |> Map.values
     |> Enum.product
   end
 
-  def p2(_i), do: nil
+  def p2(input) do
+    input
+    |> Enum.reduce(%{x: 0, y: 0, aim: 0}, fn
+      "forward " <> num, %{x: x, y: y, aim: aim} = sub -> %{sub | x: x + i(num), y: aim * i(num) + y}
+      "down " <> num, %{aim: aim} = sub -> %{sub | aim: aim + i(num)}
+      "up " <> num, %{aim: aim} = sub -> %{sub | aim: aim - i(num)}
+    end)
+    |> Map.delete(:aim)
+    |> Map.values
+    |> Enum.product
+  end
 end
