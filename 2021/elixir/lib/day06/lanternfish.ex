14922d6c28b5bf62afbb8b524a8c9f5840b02153
Evan Travers <evantravers@gmail.com>
2021-12-07 17:52:07 -0600

    refactor
    it looks like 2021.6.2 is going to be a heatdeath problem, looks like I
    need to do some math.

--- 2021/elixir/lib/day06/lanternfish.ex
+++ 2021/elixir/lib/day06/lanternfish.ex
@@ -14,14 +14,15 @@ defmodule Advent2021.Day6 do
   def live(0), do: [@lifespan, @lifespan + 2]
   def live(int), do: int - 1
 
-  def p1(input, count \\ 80)
-  def p1(input, 0), do: Enum.count(input)
-  def p1(input, count) do
+  def sim(input, count \\ 80)
+  def sim(input, 0), do: Enum.count(input)
+  def sim(input, count) do
     input
     |> Enum.map(&live/1)
     |> List.flatten
-    |> p1(count - 1)
+    |> sim(count - 1)
   end
 
-  def p2(_i), do: nil
+  def p1(input), do: sim(input, 80)
+  def p2(input), do: nil # sim(input, 256)
 end
