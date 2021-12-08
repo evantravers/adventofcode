ec923b366160684ef0b11e2d30e19febd539f5b4
Evan Travers <evantravers@gmail.com>
2021-12-08 12:49:12 -0600

    WIP: 2021.8.2
    I can see there's a heuristic for determining the remaining numbers as a
    sort of sieve... just haven't sorted it out yet

--- 2021/elixir/lib/day08/seven_segment_search.ex
+++ 2021/elixir/lib/day08/seven_segment_search.ex
@@ -16,8 +16,6 @@ defmodule Advent2021.Day8 do
     }
   end
 
-  def display(%{a: true, b: true, c: true, d: false, e: true, f: true, g: true}), do: 0
-
   def p1(input) do
     input
     |> Enum.map(fn(%{output: output}) ->
@@ -34,5 +32,63 @@ defmodule Advent2021.Day8 do
     |> Enum.sum
   end
 
-  def p2(_i), do: nil
+  def p2(input) do
+    input
+    |> Enum.map(fn(%{signals: signals, output: output}) ->
+      possible = %{
+        a: MapSet.new(["a", "b", "c", "d", "e", "f", "g"]),
+        b: MapSet.new(["a", "b", "c", "d", "e", "f", "g"]),
+        c: MapSet.new(["a", "b", "c", "d", "e", "f", "g"]),
+        d: MapSet.new(["a", "b", "c", "d", "e", "f", "g"]),
+        e: MapSet.new(["a", "b", "c", "d", "e", "f", "g"]),
+        f: MapSet.new(["a", "b", "c", "d", "e", "f", "g"]),
+        g: MapSet.new(["a", "b", "c", "d", "e", "f", "g"])
+      }
+
+      wiring =
+        signals
+        |> Enum.map(fn(str) ->
+          str
+          |> String.codepoints
+          |> MapSet.new
+        end)
+        |> Enum.sort_by(&Enum.count/1)
+        |> IO.inspect
+        |> Enum.reduce(possible, fn(signal, possible) ->
+          case Enum.count(signal) do
+            2 -> # found 1!
+              possible
+              |> Map.update!(:c, &MapSet.intersection(&1, signal))
+              |> Map.update!(:f, &MapSet.intersection(&1, signal))
+            3 -> # found 7!
+              possible
+              |> Map.update!(:a, &MapSet.intersection(&1, signal))
+              |> Map.update!(:c, &MapSet.intersection(&1, signal))
+              |> Map.update!(:f, &MapSet.intersection(&1, signal))
+            4 -> # found 4!
+              possible
+              |> Map.update!(:b, &MapSet.intersection(&1, signal))
+              |> Map.update!(:c, &MapSet.intersection(&1, signal))
+              |> Map.update!(:d, &MapSet.intersection(&1, signal))
+              |> Map.update!(:f, &MapSet.intersection(&1, signal))
+            7 -> # found 8!
+              possible
+              |> Map.update!(:a, &MapSet.intersection(&1, signal))
+              |> Map.update!(:b, &MapSet.intersection(&1, signal))
+              |> Map.update!(:c, &MapSet.intersection(&1, signal))
+              |> Map.update!(:d, &MapSet.intersection(&1, signal))
+              |> Map.update!(:e, &MapSet.intersection(&1, signal))
+              |> Map.update!(:f, &MapSet.intersection(&1, signal))
+              |> Map.update!(:g, &MapSet.intersection(&1, signal))
+            _ -> possible
+          end
+        end)
+        |> IO.inspect
+
+      # Enum.map(output, &Map.get(wiring, &1))
+
+    end)
+    # |> List.flatten
+    # |> Enum.sum
+  end
 end
