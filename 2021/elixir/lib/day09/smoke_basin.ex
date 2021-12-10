bc4c0e0774c7f53f32a543abd6d30178b629be0e
Evan Travers <evantravers@gmail.com>
2021-12-10 07:48:22 -0600

    2021.9.2
    I solved this partly yesterday and a little bit today with some
    `IO.inspect/1` hackery.
    
    I realized that I wasn't filtering for being in bounds, the queue was
    growing with infinite negative x values, filtering by `basin?/1` helped
    keep the coord requests in bounds

--- 2021/elixir/lib/day09/smoke_basin.ex
+++ 2021/elixir/lib/day09/smoke_basin.ex
@@ -19,16 +19,32 @@ defmodule Advent2021.Day9 do
     end)
   end
 
-  def low_point?({{x, y}, height}, map) do
-    [
-      {x - 1, y},
-      {x + 1, y},
-      {x, y - 1},
-      {x, y + 1}
-    ]
+  def adjacent({x, y}), do: [ {x - 1, y}, {x + 1, y}, {x, y - 1}, {x, y + 1} ]
+
+  def low_point?({point, height}, map) do
+    point
+    |> adjacent
     |> Enum.all?(fn(coord) ->
       Map.get(map, coord) > height
     end)
+  end
+
+  def basin?(9), do: false
+  def basin?(i) when is_integer(i), do: true
+  def basin?(_i), do: false
+
+  def find_basin(queue, map, visited \\ MapSet.new())
+  def find_basin([], _map, visited), do: visited
+  def find_basin([coord|queue], map, visited) do
+    find_basin(
+      coord
+      |> adjacent
+      |> Enum.filter(&Map.get(map, &1) |> basin?)
+      |> Enum.reject(&Enum.member?(visited, &1))
+      |> Kernel.++(queue),
+      map,
+      MapSet.put(visited, coord)
+    )
   end
 
   @doc """
@@ -48,5 +64,30 @@ defmodule Advent2021.Day9 do
     |> Enum.sum
   end
 
-  def p2(_i), do: nil
+  @doc """
+      iex> "2199943210
+      ...>3987894921
+      ...>9856789892
+      ...>8767896789
+      ...>9899965678"
+      ...> |> setup_string
+      ...> |> p2
+      1134
+  """
+  def p2(map) do
+    low_points =
+    map
+    |> Enum.filter(&low_point?(&1, map))
+    |> Enum.map(fn(low_point) ->
+      low_point
+      |> elem(0)
+      |> List.wrap
+      |> find_basin(map)
+      |> Enum.count
+    end)
+    |> Enum.sort
+    |> Enum.reverse
+    |> Enum.take(3)
+    |> Enum.product
+  end
 end
