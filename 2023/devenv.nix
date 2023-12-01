15e76e01eb914ad4dda43a704655e74c4e360109
Evan Travers <evantravers@gmail.com>
2023-12-01 10:02:26 -0600

    Aww... let's start 2023!

---
+++ 2023/devenv.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  enterShell = ''
+  '';
+
+  languages.elixir.enable = true;
+}
