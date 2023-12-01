bf0bf477ef4f3eb23db738dac4bba7077ec9e116
Evan Travers <evantravers@gmail.com>
2023-12-01 14:04:38 -0600

    Solving p1
    I'm pretty sure this is nearly right on p2, but I'm getting the wrong
    answer for some reason.

--- 2023/elixir/lib/01-trebuchet.ex
+++ 2023/elixir/lib/01-trebuchet.ex
@@ -1,4 +1,6 @@
 defmodule Advent2023.Day1 do
+  require IEx
+
   @moduledoc "https://adventofcode.com/2023/day/1"
 
   def setup do
@@ -8,11 +10,61 @@ defmodule Advent2023.Day1 do
     end
   end
 
+  def p("one"),   do: 1
+  def p("two"),   do: 2
+  def p("three"), do: 3
+  def p("four"),  do: 4
+  def p("five"),  do: 5
+  def p("six"),   do: 6
+  def p("seven"), do: 7
+  def p("eight"), do: 8
+  def p("nine"),  do: 9
+  def p(num),     do: String.to_integer(num)
+
+  @doc """
+  iex> ["1abc2",
+  ...> "pqr3stu8vwx",
+  ...> "a1b2c3d4e5f",
+  ...> "treb7uchet"]
+  ...> |> p1()
+  142
+  """
   def p1(input) do
     input
+    |> Enum.map(fn line ->
+      numbers =
+        line
+        |> String.codepoints
+        |> Enum.filter(fn c -> String.match?(c, ~r/\d/) end)
+        |> Enum.map(&String.to_integer/1)
+
+      List.first(numbers)*10 + List.last(numbers)
+    end)
+    |> Enum.sum
   end
 
+  @doc """
+  iex> ["two1nine",
+  ...> "eightwothree",
+  ...> "abcone2threexyz",
+  ...> "xtwone3four",
+  ...> "4nineeightseven2",
+  ...> "zoneight234",
+  ...> "7pqrstsixteen"]
+  ...> |> p2()
+  281
+  """
   def p2(input) do
-    nil
+    input
+    |> Enum.map(fn line ->
+      numbers =
+        ~r/\d|one|two|three|four|five|six|seven|eight|nine/
+        |> Regex.scan(line)
+        |> List.flatten
+
+      p(List.first(numbers))*10 + p(List.last(numbers))
+      |> IO.inspect
+    end)
+    |> Enum.sum
   end
 end
--- 2023/elixir/test/advent2023_test.exs
+++ 2023/elixir/test/advent2023_test.exs
@@ -2,4 +2,5 @@ defmodule Advent2023Test do
   use ExUnit.Case
   doctest Advent2023
 
+  doctest Advent2023.Day1, import: true
 end
