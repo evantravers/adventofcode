ad9e1cf634a7fa2a768ba2c4a756ba616e396ae8
Evan Travers <evantravers@gmail.com>
2023-12-04 02:32:56 +0000

    Unfinished 2023.3.1
    I feel pretty good about my approach, it's just that process isn't
    detecting the integers from 0-9... something screwy with my pattern
    matching. Therefore I haven't tested the `clear/1` functionality yet.

--- 2023/elixir/lib/03-gear-ratios.ex
+++ 2023/elixir/lib/03-gear-ratios.ex
@@ -1,6 +1,7 @@
 defmodule Advent2023.Day3 do
   @moduledoc "https://adventofcode.com/2023/day/3"
 
+  require IEx
   def setup_from_string(str) do
     str
     |> String.split("\n", trim: true)
@@ -10,16 +11,44 @@ defmodule Advent2023.Day3 do
       |> String.codepoints
       |> Enum.with_index
       |> Enum.reduce(schema, fn({char, x}, schema) ->
-        Map.put(schema, {x, y}, char)
-        # process(schema, {x, y}, char)
+        schema
+        |> clear
+        |> process({x, y}, char)
       end)
     end)
+    |> clear
   end
 
   def setup do
     with {:ok, file} <- File.read("../input/3") do
       file |> setup_from_string
     end
+  end
+
+  @doc """
+  Since we are no longer building a number, clear the memoized number.
+  """
+  def clear(%{number: number} = schema) do
+    schema
+    |> Map.update(:parts, String.to_integer(number), fn parts -> Map.merge(number, parts) end)
+    |> Map.delete(:number)
+  end
+  def clear(schema), do: schema
+
+  # '0'->48 :: '9'->57
+  # this is a number
+  def process(schema, coord, <<c>> = char) when c <= 48 and c >= 57 do
+    IO.puts("found a number: #{char}, #{coord}")
+    schema
+    |> Map.update(:number, char, fn current_expr -> current_expr <> char end)
+  end
+  # blank space "."
+  def process(schema, _coord, "."), do: schema |> clear
+  # symbol
+  def process(schema, coord, _char) do
+    schema
+    |> clear
+    |> Map.update(:symbols, [coord], fn coords -> [coord|coords] end)
   end
 
   @doc """
@@ -39,6 +68,7 @@ defmodule Advent2023.Day3 do
   """
   def p1(schematic) do
     schematic
+    |> IO.inspect
   end
 
   def p2(_i), do: nil
