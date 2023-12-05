f79b8a54c080d8fff8097400fa08639a3847f8ca
Evan Travers <evantravers@gmail.com>
2023-12-04 23:21:28 -0600

    Adjusting devenv

--- 2023/elixir/devenv.nix
+++ 2023/elixir/devenv.nix
@@ -2,27 +2,15 @@
 
 {
   # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  env.GREET = "";
 
   # https://devenv.sh/packages/
   packages = [ pkgs.git pkgs.elixir-ls ];
 
   # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
-
   enterShell = ''
-    hello
-    git --version
   '';
 
   # https://devenv.sh/languages/
   languages.elixir.enable = true;
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
-
-  # See full reference at https://devenv.sh/reference/options/
 }
