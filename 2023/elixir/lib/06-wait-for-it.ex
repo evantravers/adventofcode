d005358d2467a464ca8e52950eed8ca8e32ef170
Evan Travers <evantravers@gmail.com>
2023-12-06 16:18:41 -0600

    2023.6.2
    Just brute forced it. It takes about 5 seconds, there's probably a
    faster way.

--- 2023/elixir/lib/06-wait-for-it.ex
+++ 2023/elixir/lib/06-wait-for-it.ex
@@ -3,24 +3,8 @@ defmodule Advent2023.Day6 do
 
   def setup do
     with {:ok, file} <- File.read("../input/6") do
-      setup_from_string(file)
+      file
     end
-  end
-
-  def setup_from_string(str) do
-    [time, distance] =
-      str
-      |> String.split("\n", trim: true)
-      |> Enum.map(fn row ->
-        [_label, values] = String.split(row, ":")
-
-        values
-        |> String.trim
-        |> String.split(~r/\s+/)
-        |> Enum.map(&String.to_integer/1)
-      end)
-
-    Enum.zip(time, distance)
   end
 
   def ways_to_beat({time, distance}) do
@@ -34,15 +18,46 @@ defmodule Advent2023.Day6 do
   @doc """
   iex> "Time:      7  15   30
   ...>Distance:  9  40  200"
-  ...> |> setup_from_string
   ...> |> p1
   288
   """
-  def p1(races) do
+  def p1(input) do
+    [time, distance] =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn row ->
+        [_label, values] = String.split(row, ":")
+
+        values
+        |> String.trim
+        |> String.split(~r/\s+/)
+        |> Enum.map(&String.to_integer/1)
+      end)
+
+    races = Enum.zip(time, distance)
+
     races
     |> Enum.map(&ways_to_beat/1)
     |> Enum.product
   end
 
-  def p2(_i), do: nil
+  @doc """
+  iex> "Time:      7  15   30
+  ...>Distance:  9  40  200"
+  ...> |> p2
+  71503
+  """
+  def p2(races_bad_kerning) do
+    [time, distance] =
+      races_bad_kerning
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn str ->
+        Regex.scan(~r/\d+/, str)
+        |> List.flatten
+        |> Enum.join
+        |> String.to_integer
+      end)
+
+    ways_to_beat({time, distance}) # TODO: This is really slow
+  end
 end
