937315b8ca4a8f45a8b9df3698fec53bf980070d
Evan Travers <evantravers@gmail.com>
2023-12-06 16:09:54 -0600

    2023.6.1
    That was pretty easy.

--- 2023/elixir/lib/06-wait-for-it.ex
+++ 2023/elixir/lib/06-wait-for-it.ex
@@ -3,25 +3,45 @@ defmodule Advent2023.Day6 do
 
   def setup do
     with {:ok, file} <- File.read("../input/6") do
-      [time, distance] =
-        file
-        |> String.split("\n", trim: true)
-        |> Enum.map(fn row ->
-          [_label, values] = String.split(row, ":")
-
-          values
-          |> String.trim
-          |> String.split(~r/\s+/)
-          |> Enum.map(&String.to_integer/1)
-        end)
-
-      Enum.zip(time, distance)
+      setup_from_string(file)
     end
   end
 
+  def setup_from_string(str) do
+    [time, distance] =
+      str
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn row ->
+        [_label, values] = String.split(row, ":")
+
+        values
+        |> String.trim
+        |> String.split(~r/\s+/)
+        |> Enum.map(&String.to_integer/1)
+      end)
+
+    Enum.zip(time, distance)
+  end
+
+  def ways_to_beat({time, distance}) do
+    0..time
+    |> Enum.map(fn(speed) ->
+      speed*(time-speed)
+    end)
+    |> Enum.count(& &1 > distance)
+  end
+
+  @doc """
+  iex> "Time:      7  15   30
+  ...>Distance:  9  40  200"
+  ...> |> setup_from_string
+  ...> |> p1
+  288
+  """
   def p1(races) do
     races
-    |> IO.inspect
+    |> Enum.map(&ways_to_beat/1)
+    |> Enum.product
   end
 
   def p2(_i), do: nil
