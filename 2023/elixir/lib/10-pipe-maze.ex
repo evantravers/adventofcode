894024d5e3bf8bffccc0e53aaa87e15ac69e45c1
Evan Travers <evantravers@gmail.com>
2023-12-10 17:03:43 -0600

    Starting my approach:
    The theory... I've got a graph object. I can use it's stdlib to find the
    cycle and count the edges.

--- 2023/elixir/lib/10-pipe-maze.ex
+++ 2023/elixir/lib/10-pipe-maze.ex
@@ -1,22 +1,24 @@
 defmodule Advent2023.Day10 do
   @moduledoc "https://adventofcode.com/2023/day/10"
 
-  require LibGraph
-
   def setup do
     with {:ok, file} <- File.read("../input/10") do
-      file
-      |> String.split("\n", trim: true)
-      |> Enum.with_index
-      |> Enum.reduce(%{}, fn ({row, y}, map) ->
-        row
-        |> String.codepoints
-        |> Enum.with_index
-        |> Enum.reduce(map, fn ({char, x}, map) ->
-          Map.put(map, {x, y}, char)
-        end)
-      end)
+      file |> setup_with_string
     end
+  end
+
+  def setup_with_string(str) do
+    str
+    |> String.split("\n", trim: true)
+    |> Enum.with_index
+    |> Enum.reduce(Graph.new(), fn ({row, y}, graph) ->
+      row
+      |> String.codepoints
+      |> Enum.with_index
+      |> Enum.reduce(graph, fn ({char, x}, graph) ->
+        read_pipe(char, {x, y}, graph)
+      end)
+    end)
   end
 
   @doc """
@@ -29,10 +31,59 @@ defmodule Advent2023.Day10 do
   . is ground; there is no pipe in this tile.
   S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
   """
-  def pipe
+  def read_pipe("|", {x, y}, graph) do
+    graph
+    |> Graph.add_edge({x, y}, {x, y+1})
+    |> Graph.add_edge({x, y}, {x, y-1})
+  end
+  def read_pipe("-", {x, y}, graph) do
+    graph
+    |> Graph.add_edge({x, y}, {x-1, y})
+    |> Graph.add_edge({x, y}, {x+1, y})
+  end
+  def read_pipe("L", {x, y}, graph) do
+    graph
+    |> Graph.add_edge({x, y}, {x, y-1})
+    |> Graph.add_edge({x, y}, {x+1, y})
+  end
+  def read_pipe("J", {x, y}, graph) do
+    graph
+    |> Graph.add_edge({x, y}, {x, y-1})
+    |> Graph.add_edge({x, y}, {x-1, y})
+  end
+  def read_pipe("7", {x, y}, graph) do
+    graph
+    |> Graph.add_edge({x, y}, {x, y+1})
+    |> Graph.add_edge({x, y}, {x-1, y})
+  end
+  def read_pipe("F", {x, y}, graph) do
+    graph
+    |> Graph.add_edge({x, y}, {x, y+1})
+    |> Graph.add_edge({x, y}, {x+1, y})
+  end
+  def read_pipe(".", _coords, graph), do: graph
+  def read_pipe("S", {x, y}, graph) do
+    graph
+    |> Graph.add_vertex("S", {x, y})
+  end
 
-  def p1(map) do
-    # find S
+  @doc """
+  iex> "7-F7-
+  ...>.FJ|7
+  ...>SJLL7
+  ...>|F--J
+  ...>LJ.LJ"
+  ...> |> setup_with_string
+  ...> |> p1
+  8
+
+  How many steps along the loop does it take to get from the starting position
+  to the point farthest from the starting position?
+  """
+  def p1(graph) do
+    graph
+    |> Graph.components
+    |> IO.inspect
   end
 
   def p2(_i), do: nil
