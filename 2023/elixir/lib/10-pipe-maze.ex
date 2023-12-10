5c17fabf9fca441fa3e99a3772f4b2e390261131
Evan Travers <evantravers@gmail.com>
2023-12-10 16:45:21 -0600

    Add libgraph

--- 2023/elixir/lib/10-pipe-maze.ex
+++ 2023/elixir/lib/10-pipe-maze.ex
@@ -1,6 +1,8 @@
 defmodule Advent2023.Day10 do
   @moduledoc "https://adventofcode.com/2023/day/10"
 
+  require LibGraph
+
   def setup do
     with {:ok, file} <- File.read("../input/10") do
       file
@@ -17,6 +19,21 @@ defmodule Advent2023.Day10 do
     end
   end
 
-  def p1(_i), do: nil
+  @doc """
+  | is a vertical pipe connecting north and south.
+  - is a horizontal pipe connecting east and west.
+  L is a 90-degree bend connecting north and east.
+  J is a 90-degree bend connecting north and west.
+  7 is a 90-degree bend connecting south and west.
+  F is a 90-degree bend connecting south and east.
+  . is ground; there is no pipe in this tile.
+  S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
+  """
+  def pipe
+
+  def p1(map) do
+    # find S
+  end
+
   def p2(_i), do: nil
 end
--- 2023/elixir/mix.exs
+++ 2023/elixir/mix.exs
@@ -21,7 +21,8 @@ defmodule Advent2023.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:math, "~> 0.6.0"}
+      {:math, "~> 0.6.0"},
+      {:libgraph, "~> 0.16.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
