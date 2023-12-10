762b69828cb913a8e3b54724b3e026b698977cd5
Evan Travers <evantravers@gmail.com>
2023-12-10 08:34:41 -0600

    Some structure

--- 2023/elixir/lib/09-mirage-maintenance.ex
+++ 2023/elixir/lib/09-mirage-maintenance.ex
@@ -17,6 +17,12 @@ defmodule Advent2023.Day9 do
     end)
   end
 
+  def compute_prediction(list_of_integers) do
+    list_of_integers
+    |> Stream.chunk_every(2, 1)
+    |> IO.inspect
+  end
+
   @doc """
   iex> "0 3 6 9 12 15
   ...>1 3 6 10 15 21
@@ -25,8 +31,11 @@ defmodule Advent2023.Day9 do
   ...> |> p1
   114
   """
-  def p1(i) do
-    # isn't this basically a riemann sum / integral kinda thing?
+  def p1(list_of_numbers) do
+    # isn't this basically a Riemann sum / integral kinda thing?
+    list_of_numbers
+    |> Enum.map(&compute_prediction/1)
+    |> Enum.sum
   end
 
   def p2(_i), do: nil
