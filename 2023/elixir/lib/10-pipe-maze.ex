d13d5b5343481fc9c42e68771048e62d77ff7204
Evan Travers <evantravers@gmail.com>
2023-12-10 23:04:06 -0600

    Add a bunch of failing tests

--- 2023/elixir/lib/10-pipe-maze.ex
+++ 2023/elixir/lib/10-pipe-maze.ex
@@ -160,5 +160,51 @@ defmodule Advent2023.Day10 do
     |> div(2)
   end
 
-  def p2(_i), do: nil
+  @doc """
+  How many tiles are enclosed by the loop?
+
+  iex> "...........
+  ...>.S-------7.
+  ...>.|F-----7|.
+  ...>.||.....||.
+  ...>.||.....||.
+  ...>.|L-7.F-J|.
+  ...>.|..|.|..|.
+  ...>.L--J.L--J.
+  ...>..........."
+  ...> |> setup_from_string
+  ...> |> p2
+  4
+
+  iex> ".F----7F7F7F7F-7....
+  ...>.|F--7||||||||FJ....
+  ...>.||.FJ||||||||L7....
+  ...>FJL7L7LJLJ||LJ.L-7..
+  ...>L--J.L7...LJS7F-7L7.
+  ...>....F-J..F7FJ|L7L7L7
+  ...>....L7.F7||L7|.L7L7|
+  ...>.....|FJLJ|FJ|F7|.LJ
+  ...>....FJL-7.||.||||...
+  ...>....L---J.LJ.LJLJ..."
+  ...> |> setup_from_string
+  ...> |> p2
+  8
+
+  iex> "FF7FSF7F7F7F7F7F---7
+  ...>L|LJ||||||||||||F--J
+  ...>FL-7LJLJ||||||LJL-77
+  ...>F--JF--7||LJLJ7F7FJ-
+  ...>L---JF-JLJ.||-FJLJJ7
+  ...>|F|F-JF---7F7-L7L|7|
+  ...>|FFJF7L7F-JF7|JL---7
+  ...>7-L-JL7||F7|L7F-7F7|
+  ...>L.L7LFJ|||||FJL7||LJ
+  ...>L7JLJL-JLJLJL--JLJ.L"
+  ...> |> setup_from_string
+  ...> |> p2
+  10
+  """
+  def p2(graph) do
+    graph
+  end
 end
