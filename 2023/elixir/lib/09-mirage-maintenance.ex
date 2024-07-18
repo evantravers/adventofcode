f33e3533d14ee6cabfcbbd1c47556efb7126c854
Evan Travers <evantravers@gmail.com>
2024-07-18 08:45:33 -0500

    Gross, but passes tests
    I spent a long time trying to be clever, because I'm pretty sure that
    this is an integral or derivative, (a Riemann sum?)

--- 2023/elixir/lib/09-mirage-maintenance.ex
+++ 2023/elixir/lib/09-mirage-maintenance.ex
@@ -17,10 +17,40 @@ defmodule Advent2023.Day9 do
     end)
   end
 
-  def compute_prediction(list_of_integers) do
+  def acceleration_vector(list_of_integers) do
     list_of_integers
-    |> Stream.chunk_every(2, 1)
-    |> IO.inspect
+    |> Enum.chunk_every(2, 1)
+    |> Enum.reject(&length(&1) == 1)
+    |> Enum.map(fn ([a, b]) ->
+      b - a
+    end)
+  end
+
+  def generate_vectors(list_of_integers, history \\ []) do
+    acc_vector = acceleration_vector(list_of_integers)
+    if Enum.all?(acc_vector, & &1 == 0) do
+      [list_of_integers|history]
+    else
+      generate_vectors(acc_vector, [list_of_integers|history])
+    end
+  end
+
+  def recursive_compute(list_of_lists) do
+    # reverse everything so I can use hd/1 which is way more performant than
+    # List.last/1
+    list_of_lists
+    |> Enum.map(&Enum.reverse/1)
+    |> recursive_compute(0)
+  end
+  def recursive_compute([], accel), do: accel
+  def recursive_compute(list_of_lists, acceleration) do
+    recursive_compute(tl(list_of_lists), acceleration + hd(hd(list_of_lists)))
+  end
+
+  def extend_sequence(list_of_numbers) do
+    list_of_numbers
+    |> generate_vectors()
+    |> recursive_compute()
   end
 
   @doc """
@@ -34,7 +64,7 @@ defmodule Advent2023.Day9 do
   def p1(list_of_numbers) do
     # isn't this basically a Riemann sum / integral kinda thing?
     list_of_numbers
-    |> Enum.map(&compute_prediction/1)
+    |> Enum.map(&extend_sequence/1)
     |> Enum.sum
   end
 
