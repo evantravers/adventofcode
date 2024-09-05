469650ff9f2dfebcf18d928932c868749e6d877f
Evan Travers <evantravers@gmail.com>
2024-09-05 17:09:50 -0500

    Capture location of all galaxies

--- 2023/elixir/lib/11-cosmic-expansion.ex
+++ 2023/elixir/lib/11-cosmic-expansion.ex
@@ -5,10 +5,22 @@ defmodule Advent2023.Day11 do
   def setup do
     with {:ok, file} <- File.read("../input/11") do
       file
+      |> String.split("\n", trim: true)
+      |> Enum.with_index
+      |> Enum.reduce([], fn {line, y}, world ->
+        line
+        |> String.codepoints
+        |> Enum.with_index
+        |> Enum.reduce(world, fn
+          {"#", x}, world -> [{x, y}|world]
+                _, world  -> world
+        end)
+      end)
     end
   end
 
   def p1(world) do
+    world
   end
   def p2(_world), do: nil
 end
