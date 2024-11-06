bef7e00e0cc2670f0c63aafebbe533160dfedde0
Evan Travers <evantravers@gmail.com>
2024-11-06 16:14:46 -0600

    Setup tests

--- 2023/elixir/lib/11-cosmic-expansion.ex
+++ 2023/elixir/lib/11-cosmic-expansion.ex
@@ -5,22 +5,45 @@ defmodule Advent2023.Day11 do
   def setup do
     with {:ok, file} <- File.read("../input/11") do
       file
-      |> String.split("\n", trim: true)
-      |> Enum.with_index
-      |> Enum.reduce([], fn {line, y}, world ->
-        line
-        |> String.codepoints
-        |> Enum.with_index
-        |> Enum.reduce(world, fn
-          {"#", x}, world -> [{x, y}|world]
-                _ , world -> world
-        end)
-      end)
+      |> setup_with_string
     end
   end
 
+  def setup_with_string(str) do
+    str
+    |> String.split("\n", trim: true)
+    |> Enum.with_index
+    |> Enum.reduce([], fn {line, y}, world ->
+      line
+      |> String.codepoints
+      |> Enum.with_index
+      |> Enum.reduce(world, fn
+        {"#", x}, world -> [{x, y}|world]
+              _ , world -> world
+      end)
+    end)
+  end
+
+  @doc """
+  iex> "...#......
+  ...>.......#..
+  ...>#.........
+  ...>..........
+  ...>......#...
+  ...>.#........
+  ...>.........#
+  ...>..........
+  ...>.......#..
+  ...>#...#....."
+  ...> |> setup_with_string
+  ...> |> p1
+  374
+  """
   def p1(world) do
     world
+    # Let's think about this.
+    # I don't want to store every empty spot, because while it works in tests,
+    # it's going to fail on the input.
   end
 
   def p2(_world), do: nil
