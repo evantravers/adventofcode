f67fe1deed92bd3e9b4d6484d2e0f0910b874dc7
Evan Travers <evantravers@gmail.com>
2024-12-01 13:13:30 -0600

    24.1.1
    My list comprehension was going through both lists instead of combining
    them, I think it was doing every combination. 🤦

--- 2024/elixir/lib/01-historian-hysteria.ex
+++ 2024/elixir/lib/01-historian-hysteria.ex
@@ -3,28 +3,42 @@ defmodule Advent2024.Day1 do
 
   def setup do
     with {:ok, file} <- File.read("../input/01") do
-      file
-      |> String.split("\n", trim: true)
-      |> Enum.map(fn(str) ->
-        str
-        |> String.split(" ", trim: true)
-        |> Enum.map(&String.to_integer/1)
-      end)
-      |> Enum.zip
-      |> Enum.map(fn(tuple) ->
-        tuple
-        |> Tuple.to_list
-        |> Enum.sort
-      end)
+      file |> setup_string
     end
   end
 
+  def setup_string(str) do
+    str
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn(str) ->
+      str
+      |> String.split(" ", trim: true)
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> Enum.zip
+    |> Enum.map(fn(tuple) ->
+      tuple
+      |> Tuple.to_list
+      |> Enum.sort
+    end)
+  end
+
   @doc """
-  iex> p1()
-  23
+  iex> "3   4
+  ...>4   3
+  ...>2   5
+  ...>1   3
+  ...>3   9
+  ...>3   3"
+  ...> |> setup_string
+  ...> |> p1
+  11
   """
   def p1([list_1, list_2] = _input) do
-    for left <- list_1, right <- list_2 do abs(left - right) end
+    Enum.zip(list_1, list_2)
+    |> Enum.map(fn {a, b} ->
+      abs(a - b)
+    end)
     |> Enum.sum
   end
   def p2(_i), do: nil
--- 2024/elixir/test/advent2024_test.exs
+++ 2024/elixir/test/advent2024_test.exs
@@ -1,6 +1,5 @@
 defmodule Advent2024Test do
   use ExUnit.Case
-  doctest Advent2024
-
+  doctest Advent
   doctest Advent2024.Day1, import: true
 end
