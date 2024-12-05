ff5e57036eb3ff9b1a295608b890866a0ccac1d9
Evan Travers <evantravers@gmail.com>
2024-12-04 18:06:07 -0600

    Naive attempt at 24.4.1
    This is never completing.

---
+++ 2024/elixir/lib/04-ceres-search.ex
@@ -0,0 +1,52 @@
+defmodule Advent2024.Day4 do
+  @moduledoc "https://adventofcode.com/2024/day/4"
+  def setup do
+    with {:ok, file} <- File.read("../input/4") do
+      file
+      |> String.split("\n", trim: true)
+      |> Enum.with_index
+      |> Enum.reduce(%{}, fn ({str, y}, map) ->
+        str
+        |> String.graphemes
+        |> Enum.with_index
+        |> Enum.reduce(map, fn({char, x}, map) ->
+          Map.put(map, {x, y}, String.to_atom(char))
+        end)
+      end)
+    end
+  end
+
+  def xmas?([x, m, a, s], map) do
+    Map.get(map, x) == :X &&
+    Map.get(map, m) == :M &&
+    Map.get(map, a) == :A &&
+    Map.get(map, s) == :S ||
+    xmas?([s, a, m, x], map)
+  end
+
+  def generate_directions({x, y}) do
+    [
+      [{x, y}, {x+1, y}, {x+2, y}, {x+3, y}], # right
+      [{x, y}, {x-1, y}, {x-2, y}, {x-3, y}], # left
+      [{x, y}, {x, y+1}, {x, y+2}, {x, y+3}], # up
+      [{x, y}, {x, y-1}, {x, y-2}, {x, y-3}], # down
+      [{x, y}, {x+1, y+1}, {x+2, y+2}, {x+3, y+3}], # up right
+      [{x, y}, {x-1, y+1}, {x-2, y+2}, {x-3, y+3}], # up left
+      [{x, y}, {x+1, y-1}, {x+2, y-2}, {x+3, y-3}], # down right
+      [{x, y}, {x-1, y-1}, {x-2, y-2}, {x-3, y-3}], # down left
+    ]
+  end
+
+  def check_all_directions(coord, map) do
+    coord
+    |> generate_directions
+    |> Enum.count(&xmas?(&1, map))
+  end
+
+  def p1(map) do
+    map
+    |> Map.keys
+    |> Enum.map(&check_all_directions(&1, map))
+  end
+  def p2(_i), do: nil
+end
