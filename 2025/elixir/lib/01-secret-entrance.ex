8f538bcac64d3828089398d8400c3daa36c7a41e
Evan Travers <evantravers@gmail.com>
2025-12-01 08:18:01 -0600

    Setup failing tests

--- 2025/elixir/lib/01-secret-entrance.ex
+++ 2025/elixir/lib/01-secret-entrance.ex
@@ -4,13 +4,18 @@ defmodule Advent2025.Day1 do
   def setup do
     with {:ok, file} <- File.read("../input/01") do
       file
-      |> String.split("\n", trim: true)
-      |> Enum.map(&read_instructions/1)
+      |> setup_from_string
     end
   end
 
   def read_instructions("L" <> count), do: {:L, String.to_integer(count)}
   def read_instructions("R" <> count), do: {:R, String.to_integer(count)}
+  def setup_from_string(str) do
+    str
+    |> String.split("\n", trim: true)
+    |> Enum.map(&translate_instructions/1)
+  end
+
 
   def turn({:L, count}, [pointer|_rest] = locs) do
     [Integer.mod(pointer - count, 100)|locs]
@@ -27,5 +32,30 @@ defmodule Advent2025.Day1 do
     |> Enum.count(& &1 == 0)
   end
 
-  def p2(_setup), do: nil
+  @doc """
+  Count how many times the dial _passes_ 0.
+
+  We are going to just keep track of sign changes.
+
+  iex> "L68
+  ...>L30
+  ...>R48
+  ...>L5
+  ...>R60
+  ...>L55
+  ...>L1
+  ...>L99
+  ...>R14
+  ...>L82"
+  ...> |> setup_from_string
+  ...> |> p2
+  3
+  """
+  def p2(instructions) do
+    start = {50, 0} # {last_position, sign_swap_count}
+
+    {_last_pos, count} = Enum.reduce(instructions, start, &track/2)
+
+    count
+  end
 end
